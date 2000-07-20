f9158ce3284ee4164aa3792c89aafe03abadc394
algebra <algebra@8563fe0b-bd16-0410-b45f-99c24e105077>
2000-07-20 02:02:10 +0000

    int32, int64, integer types for general use -bds

---
+++ linbox-old/src/library/objects/Integers/lin_integer.h
@@ -0,0 +1,13 @@
+// -bds 7/00
+#ifndef __lin_integer__
+#define __lin_integer__
+#include <stdlib.h>
+#include <givinteger.h>
+namespace linbox{
+
+typedef int int_32; // should guarantee 32 bits though
+typedef long long int_64; // should guarantee 64 bits though
+typedef Integer integer;
+
+} // namespace linbox
+#endif
