e4ca9ac8468b9970e68d58b52fa21ee6a2a93c41
hovinen <hovinen@8563fe0b-bd16-0410-b45f-99c24e105077>
2001-05-29 00:29:13 +0000

    Fixed field element output (field.write) so that it does not put a /1 when outputting an integer

--- linbox-old/src/library/objects/field/unparametric/gmp-rational-field.h
+++ linbox-old/src/library/objects/field/unparametric/gmp-rational-field.h
@@ -438,13 +438,15 @@ namespace LinBox
 
 	 str = new char[mpz_sizeinbase (mpq_numref (x.rep), 10) + 2];
 	 mpz_get_str (str, 10, mpq_numref (x.rep));
-	 os << str << '/';
-	 delete str;
-
-	 str = new char[mpz_sizeinbase (mpq_denref (x.rep), 10) + 2];
-	 mpz_get_str (str, 10, mpq_denref (x.rep));
 	 os << str;
 	 delete str;
+
+	 if (mpz_cmp_ui (mpq_denref (x.rep), 1L) != 0) {
+		 str = new char[mpz_sizeinbase (mpq_denref (x.rep), 10) + 2];
+		 mpz_get_str (str, 10, mpq_denref (x.rep));
+		 os << '/' << str;
+		 delete str;
+	 }
 
 	 return os;
     }
