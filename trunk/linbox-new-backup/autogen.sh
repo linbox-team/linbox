cb0e5a71824f32a143ef8533f86c09620f0f376b
hovinen <hovinen@8563fe0b-bd16-0410-b45f-99c24e105077>
2001-06-14 16:58:12 +0000

    2001-06-14  Bradford Hovinen  <hovinen@ximian.com>
    	* autogen.sh: Fixed sanity test to reflect new directory structure

--- trunk/linbox-new-backup/autogen.sh
+++ trunk/linbox-new-backup/autogen.sh
@@ -7,8 +7,7 @@ test -z "$srcdir" && srcdir=.
 PKG_NAME="Linbox Library"
 
 (test -f $srcdir/configure.in \
-  && test -f $srcdir/src/linbox.dxx \
-  && test -d $srcdir/src/library) || {
+  && test -f $srcdir/src/linbox.dxx) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level "\`$PKG_NAME\'" directory"
     exit 1
