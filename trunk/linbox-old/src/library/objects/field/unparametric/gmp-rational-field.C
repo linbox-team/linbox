b7d5a27bd1ecd3b7e9b49ce5846d3a05bba6c80f
dumas <dumas@8563fe0b-bd16-0410-b45f-99c24e105077>
2001-06-15 11:07:26 +0000

    Static initializers

---
+++ trunk/linbox-old/src/library/objects/field/unparametric/gmp-rational-field.C
@@ -0,0 +1,8 @@
+#include "gmp-rational-field.h"
+
+const integer GMP_Rational_Field::_cardinality = -1;
+const integer GMP_Rational_Field::_characteristic = 0;
+const integer GMP_Rational_Field::_zero = 0;
+const integer GMP_Rational_Field::_one = 1;
+const integer GMP_Rational_Field::_neg_one = -1;
+
