2258ea6994222da1ab08cf710b4c812bcd0875b1
turner <turner@8563fe0b-bd16-0410-b45f-99c24e105077>
2001-06-23 16:04:08 +0000

    New diagonal blackbox matrix; to be used with compose for diagonal preconditioner.

---
+++ trunk/linbox-old/src/library/objects/blackbox/diagonal.h
@@ -0,0 +1,332 @@
+/* File: src/library/objects/blackbox/diagonal.h
+ * Authro: William J Turner for the LinBox group
+ */
+
+#ifndef _DIAGONAL_
+#define _DIAGONAL_
+
+#include "LinBox/blackbox_archetype.h"
+#include "LinBox/vector_traits.h"
+
+// Namespace in which all LinBox library code resides
+namespace LinBox
+{
+
+  /** Blackbox Diagonal matrix.
+   * This is a class of n by n diagonal matrices templatized by the 
+   * {@link Fields field} in 
+   * which the elements reside.  The class conforms to the 
+   * {@link Archetypes archetype} for \Ref{BlackBox Matrices}.
+   *
+   * The matrix itself is not stored in memory.  Rather, its apply
+   * methods use a vector of {@link Fields field} elements, which are 
+   * used to "multiply" the matrix to a vector.
+   * 
+   * This class has three template parameters.  The first is the field in 
+   * which the arithmetic is to be done.  The second is the type of 
+   * \Ref{LinBox} vector to which to apply the matrix.  The 
+   * third is chosen be defualt to be the \Ref{LinBox} vector trait
+   * of the vector.  This class is then specialized for dense and sparse 
+   * vectors.
+   * 
+   * The default class is not implemented.  It's functions should never
+   * be called because partial template specialization should always be
+   * done on the vector traits.
+   * @param Field \Ref{LinBox} field
+   * @param Vector \Ref{LinBox} dense or sparse vector of field elements
+   * @param Trait  Marker whether to use dense or sparse LinBox vector 
+   *               implementation.  This is chosen by a default parameter 
+   *               and partial template specialization.
+   */
+  template <class Field, class Vector,
+	    class Trait = vector_traits<Vector>::vector_category>
+  class diagonal : public Blackbox_archetype<Vector>
+  {
+  public:
+
+    /// Element definition
+    typedef typename Field::element element;
+
+    /** Constructor from field and dense vector of field elements.
+     * @param F	LinBox field in which ot do arithmetic
+     * @param v LinBox dense vector of field elements to be used 
+     * 		as the diagonal of the matrix.
+     */
+    diagonal(const Field F, const std::vector<typename Field::element>& v);
+
+    /** Virtual constructor.
+     * Required because constructors cannot be virtual.
+     * Make a copy of the Blackbox_archetype object.
+     * Required by abstract base class.
+     * @return pointer to new blackbox object
+     */
+    Blackbox_archetype<Vector>* clone() const;
+
+    /** Application of BlackBox matrix.
+     * y= A*x.
+     * Requires one vector conforming to the \Ref{LinBox}
+     * vector {@link Archetypes archetype}.
+     * Required by abstract base class.
+     * @return reference to vector y containing output.
+     * @param  x constant reference to vector to contain input
+     */
+    Vector& apply(const Vector& x) const;
+
+    /** Application of BlackBox matrix transpose.
+     * y= transpose(A)*x.
+     * Requires one vector conforming to the \Ref{LinBox}
+     * vector {@link Archetypes archetype}.
+     * Required by abstract base class.
+     * Because the diagonal matrix is symmetric, this is the same as calling 
+     * the apply function.
+     * @return reference to vector y containing output.
+     * @param  x constant reference to vector to contain input
+     */
+    Vector& applyTranspose(const Vector& x) const;
+
+   /** Retreive row dimensions of BlackBox matrix.
+     * This may be needed for applying preconditioners.
+     * Required by abstract base class.
+     * @return integer number of rows of black box matrix.
+     */
+    size_t rowdim(void) const;
+    
+    /** Retreive column dimensions of BlackBox matrix.
+     * Required by abstract base class.
+     * @return integer number of columns of black box matrix.
+     */
+    size_t coldim(void) const;
+
+  }; // template <Field, Vector> class diagonal
+ 
+  // Specialization of diagonal for LinBox dense vectors
+  template <class Field, class Vector>
+  class diagonal<Field, Vector, vector_categories::dense_vector_tag>
+    : public Blackbox_archetype<Vector>
+  {
+  public:
+
+    typedef typename Field::element element;
+    diagonal(const Field F, const std::vector<typename Field::element>& v);
+    Blackbox_archetype<Vector>* clone() const 
+      { return new diagonal(*this); }
+    Vector& apply(const Vector& x) const;
+    Vector& applyTranspose(const Vector& x) const { return apply(x); }
+    size_t rowdim(void) const { return _n; } 
+    size_t coldim(void) const { return _n; } 
+
+  private:
+
+    // Field for arithmetic
+    Field _F;
+
+    // Number of rows and columns of square matrix.
+    size_t _n;
+
+    // STL vector of field elements used in applying matrix.
+    std::vector<element> _v;
+    
+  }; // template <Field, Vector> class diagonal<dense_vector_tag>
+   
+  // Specialization of diagonal for LinBox sparse sequence vectors
+  template <class Field, class Vector>
+  class diagonal<Field, Vector, vector_categories::sparse_sequence_vector_tag>
+    : public Blackbox_archetype<Vector>
+  {
+  public:
+
+    typedef typename Field::element element;
+    diagonal(const Field F, const std::vector<typename Field::element>& v);
+    Blackbox_archetype<Vector>* clone() const 
+      { return new diagonal(*this); }
+    Vector& apply(const Vector& x) const;
+    Vector& applyTranspose(const Vector& x) const { return apply(x); }
+    size_t rowdim(void) const { return _n; } 
+    size_t coldim(void) const { return _n; } 
+
+  private:
+
+    // Field for arithmetic
+    Field _F;
+
+    // Number of rows and columns of square matrix.
+    size_t _n;
+
+    // STL vector of field elements used in applying matrix.
+    std::vector<element> _v;
+    
+  }; // template <Field, Vector> class diagonal<sparse_sequence_vector_tag>
+
+  // Specialization of diagonal for LinBox sparse associative vectors
+  template <class Field, class Vector>
+  class diagonal<Field, Vector, vector_categories::sparse_associative_vector_tag>
+    : public Blackbox_archetype<Vector>
+  {
+  public:
+
+    typedef typename Field::element element;
+    diagonal(const Field F, const std::vector<typename Field::element>& v);
+    Blackbox_archetype<Vector>* clone() const 
+      { return new diagonal(*this); }
+    Vector& apply(const Vector& x) const;
+    Vector& applyTranspose(const Vector& x) const { return apply(x); }
+    size_t rowdim(void) const { return _n; } 
+    size_t coldim(void) const { return _n; } 
+
+  private:
+
+    // Field for arithmetic
+    Field _F;
+
+    // Number of rows and columns of square matrix.
+    size_t _n;
+
+    // STL vector of field elements used in applying matrix.
+    std::vector<element> _v;
+    
+  }; // template <Field, Vector> class diagonal<sparse_associative_vector_tag>
+
+  // Method implementations for dense vectors
+ 
+  template <class Field, class Vector>
+  inline diagonal<Field, Vector, vector_categories::dense_vector_tag>
+  ::diagonal(const Field F, const std::vector<typename Field::element>& v)
+  	: _F(F), _n(v.size()), _v(v) {}
+
+  template <class Field, class Vector>
+  inline Vector& diagonal<Field, Vector, vector_categories::dense_vector_tag>
+  ::apply(const Vector& x) const
+  {
+    // Create zero vector to hold output
+    element temp;
+    _F.init(temp, 0);
+    Vector* y_ptr = new Vector(_n, temp);
+ 
+    if (_n != x.size())
+    {
+      cerr << endl << "ERROR:  Input vector not of right size." << endl
+  	   << endl;
+      return *y_ptr;
+    }
+ 
+    // Create iterators for input, output, and stored vectors
+    std::vector<element>::const_iterator v_iter;
+    typename Vector::const_iterator x_iter;
+    typename Vector::iterator y_iter;
+ 
+    // Start at beginning of _v and x vectors
+    v_iter = _v.begin();
+    x_iter = x.begin();
+
+    // Iterate through all three vectors, multiplying input and stored
+    // vector elements to create output vector element.
+    for (y_iter = y_ptr->begin();
+  	 y_iter != y_ptr->end();
+  	 y_iter++, v_iter++, x_iter++)
+      _F.mul(*y_iter, *v_iter, *x_iter);
+ 
+    return *y_ptr;
+  } // Vector& diagonal<dense_vector_tag>::apply(const Vector&) const
+  
+  // Method implementations for sparse sequence vectors
+ 
+  template <class Field, class Vector>
+  inline diagonal<Field, Vector, vector_categories::sparse_sequence_vector_tag>
+  ::diagonal(const Field F, const std::vector<typename Field::element>& v)
+  	: _F(F), _n(v.size()), _v(v) {}
+
+  template <class Field, class Vector>
+  inline Vector& 
+  diagonal<Field, Vector, vector_categories::sparse_sequence_vector_tag>
+  ::apply(const Vector& x) const
+  {
+    // Create zero vector to hold output
+    Vector* y_ptr = new Vector();
+ 
+    if ( (!x.empty()) && (_n < x.back().first) )
+    {
+      cerr << endl << "ERROR:  Input vector not of right size." << endl
+  	   << endl;
+      return *y_ptr;
+    }
+
+    // create field elements and size_t to be used in calculations
+    size_t i;
+    element zero, entry;
+    _F.init(zero, 0);
+    _F.init(entry, 0);
+
+    // Create iterators for input and stored vectors
+    std::vector<element>::const_iterator v_iter;
+    typename Vector::const_iterator x_iter;
+ 
+    // Start at beginning of _v vector
+    v_iter = _v.begin();
+ 
+    // Iterator over indices of input vector.
+    // For each element, multiply input element with corresponding element
+    // of stored vector and insert non-zero elements into output vector
+    for (x_iter = x.begin(); x_iter != x.end(); x_iter++)
+    {
+      i = (*x_iter).first;
+      _F.mul(entry, *(v_iter + i), (*x_iter).second);
+      if (!_F.isZero(entry)) y_ptr->push_back(make_pair(i, entry));
+    } // for (x_iter = x.begin(); x_iter != x.end(); x_iter++)
+
+    return *y_ptr;
+
+  } // Vector& diagonal<sparse_sequence_vector_tag>::apply(const Vector&) const
+
+  // Method implementations for sparse associative vectors
+ 
+  template <class Field, class Vector>
+  inline diagonal<Field, Vector, vector_categories::sparse_associative_vector_tag>
+  ::diagonal(const Field F, const std::vector<typename Field::element>& v)
+  	: _F(F), _n(v.size()), _v(v) {}
+
+  template <class Field, class Vector>
+  inline Vector& diagonal<Field, 
+                         Vector, 
+			 vector_categories::sparse_associative_vector_tag>
+  ::apply(const Vector& x) const
+  {
+    // Create zero vector to hold output
+    Vector* y_ptr = new Vector();
+ 
+    if ( (!x.empty()) && (_n < x.rbegin()->first) )
+    {
+      cerr << endl << "ERROR:  Input vector not of right size." << endl
+  	   << endl;
+      return *y_ptr;
+    }
+
+    // create field elements and size_t to be used in calculations
+    size_t i;
+    element zero, entry;
+    _F.init(zero, 0);
+    _F.init(entry, 0);
+
+    // Create iterators for input and stored vectors
+    std::vector<element>::const_iterator v_iter;
+    typename Vector::const_iterator x_iter;
+ 
+    // Start at beginning of _v vector
+    v_iter = _v.begin();
+ 
+    // Iterator over indices of input vector.
+    // For each element, multiply input element with corresponding element
+    // of stored vector and insert non-zero elements into output vector
+    for (x_iter = x.begin(); x_iter != x.end(); x_iter++)
+    {
+      i = x_iter->first;
+      _F.mul(entry, *(v_iter + i), (*x_iter).second);
+      if (!_F.isZero(entry)) y_ptr->insert(y_ptr->end(), make_pair(i, entry));
+    } // for (x_iter = x.begin(); x_iter != x.end(); x_iter++)
+
+    return *y_ptr;
+
+  } // Vector& diagonal<sparse_associative_vector_tag>::apply(...) const
+
+} // namespace LinBox
+
+#endif // _DIAGONAL_
