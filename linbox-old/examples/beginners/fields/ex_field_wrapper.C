958540fc91428c9956794482680313d70f732108
villard <villard@8563fe0b-bd16-0410-b45f-99c24e105077>
2001-09-27 16:58:21 +0000

    new examples for fields

---
+++ linbox-old/examples/beginners/fields/ex_field_wrapper.C
@@ -0,0 +1,46 @@
+// =========================================================
+// (C) The Linbox Group 1999
+// Examples for using fields 
+// Thu Sep 27 11:33:58 MEST 2001 / Gilles Villard
+// =========================================================
+
+
+// ---------------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream.h>
+// ---------------------------------------------
+
+#include "LinBox/field_archetype.h"
+#include "LinBox/gmp-rational-field.C"
+
+using namespace LinBox;
+ 
+// ---------------------------------------------
+
+template <class Field> 
+int in_a_field(const Field&  K) {
+ 
+  typedef typename Field::element K_elt;
+
+  K_elt a,b,r; 
+
+  K.read(cin,a);
+  K.read(cin,b);
+
+  K.div(r,a,b);
+
+  K.write(cout,r);
+
+}
+
+// ---------------------------------------------
+
+int main() {
+
+  GMP_Rational_Field  K;
+
+  in_a_field<  GMP_Rational_Field > (K);
+
+  return 0;
+};
