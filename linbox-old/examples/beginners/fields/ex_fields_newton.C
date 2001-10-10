e43fbfbb50379397f2cdcff7b94b1de6bef13879
villard <villard@8563fe0b-bd16-0410-b45f-99c24e105077>
2001-10-10 14:39:31 +0000

    *** empty log message ***

--- linbox-old/examples/beginners/fields/ex_fields_newton.C
+++ linbox-old/examples/beginners/fields/ex_fields_newton.C
@@ -12,39 +12,65 @@
 /* LinBox is connected to external libraries through 
  * wrappers. Here, wrappers for Givaro and NTL are included */
 
-#include "LinBox/unparam_field.h"
 #include "LinBox/lin_zpz_giv.h"
 #include "LinBox/ntl.h"
-
+#include "LinBox/unparam_field.h"
+#include "LinBox/gmp-rational-field.C"
 
 using namespace LinBox;
  // ---------------------------------------------
+/* Newton iterations to invert a number */
 
 template <class Field> 
 int fct(const Field K) {
  
   typedef typename Field::element K_elt;
-  K_elt a,b,r; 
-  K.init(a); K.init(b); K.init(r);
+
+  K_elt a,w,r,two; 
+  K.init(a); K.init(w); K.init(r); K.init(two,2);
+
+  long nb_iter=0,i;
+
+  cout << "Number to invert > "; 
   K.read(cin,a);
-  K.read(cin,b);
-  K.div(r,a,b);
-  K.write(cout,r) << "\n";
+  cout << "\n";
+
+  cout << "Initialization point > "; 
+  K.read(cin,w);
+  cout << "\n";
+
+  cout << "Number of iterations > "; 
+  cin >> nb_iter;
+  cout << "\n";
+
+  for (i=0; i<nb_iter; i++){
+     K.mul(r,a,w);
+     K.sub(r, two, r);
+     K.mulin(w,r);
+  } 
+
+
+  K.write(cout,w) << "\n";
 }
 
 // ---------------------------------------------
 
 int main() {
 
+  // Rational numbers from Gmp 
+  typedef GMP_Rational_Field  Field;
 
-  // Givaro parameterized modulo domain  
-  //ZpzDom<Std16> K(4);
+  // Or double numbers wrapped to LinBox 
+  //typedef unparam_field<double> Field;
 
   // NTL arbitrary precision real field
   // (Could be parameterized by the precision)  
-  unparam_field<NTL::RR> K;
-  NTL::RR::SetPrecision(500);
-  NTL::RR::SetOutputPrecision(50);
+
+  //typedef unparam_field<NTL::RR> Field;
+  //NTL::RR::SetPrecision(500);
+  //NTL::RR::SetOutputPrecision(50);
+
+  Field K; 
 
   fct(K);
 
