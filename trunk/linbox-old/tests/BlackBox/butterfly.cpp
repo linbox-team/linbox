f4dd9738f51c43d76f9eb4963d3be87433f1bdf5
turner <turner@8563fe0b-bd16-0410-b45f-99c24e105077>
2002-02-04 16:26:45 +0000

    Removing unneeded code

--- trunk/linbox-old/tests/BlackBox/butterfly.cpp
+++ trunk/linbox-old/tests/BlackBox/butterfly.cpp
@@ -6,44 +6,23 @@
 
 #include <iostream>
 #include <vector>
-#include <list>
-#include <utility>
-#include <map>
-#include <algorithm>
 #include "LinBox/giverror.C"
-#include "LinBox/unparam_field.h"
 #include "LinBox/param_modular.h"
 #include "LinBox/butterfly.h"
 #include "LinBox/boolean_switch.h"
 #include "LinBox/cekstv_switch.h"
 
-template <class Field> struct comp_w_ind
-{ 
-	bool operator() (const std::pair< size_t, typename Field::element >& entry, 
-			size_t col_in)
-	{ return entry.first < col_in; }
-};
-
 int main(void)
 {
-//	typedef LinBox::unparam_field<double> Field;
 	typedef LinBox::param_modular Field;
 	typedef Field::randIter RandIter;
 	typedef Field::element Element;
 	typedef std::vector<Element> Vector;  // Only dense vectors allowed
 
-/*
- * There's something wrong with applyTranspose when using param_modular field
- * and cekstv switch.  It appears to be in F.mul, but I can't figure out
- * what's wrong.  This needs to be fixed.
- */
 	LinBox::integer modulus; 	// prime modulus
 	cout << endl << "Enter a prime number for the modulus of the field: ";
 	cin >> modulus;
 	Field F(modulus);
-/*/
-	Field F;
-*/
 
 	cout << "Enter an integer size for the set of random numbers: ";
 	LinBox::integer size;
@@ -58,34 +37,6 @@ int main(void)
 	cout << "Enter the size of the vector to precondition: ";
 	size_t n;
 	cin >> n;
-
-/*
-	std::vector<bool> bools(n, false);
-
-	cout << "Enter the numbers corresponding to the linearly independent rows" << endl
-		<< "with the rows numbered from 0 to " << n - 1 << "." << endl
-		<< "End with a row number of '-1'." << endl;
-
-	size_t value;
-	while (cin >> value)
-	{
-		if (value == size_t(-1)) break;
-		if (value < bools.size()) bools[value] = true;
-	}
-	
-	cout << "The test vector is:" << endl;
-	for (size_t i = 0; i < bools.size(); i++)
-		cout << "\tbools[" << i << "] = " << bools[i] << endl;
-
-	cout << "Enter non-negative offset for contiguous block: ";
-
-	cin >> value;
-
-	cout << "The offset is " << value << endl;
-
-	std::vector<bool> s1 = LinBox::set_butterfly(bools, value, cout);
-	LinBox::boolean_switch switch1(s1);
-*/
 
 	cout << "Enter a vector to be switched by the butterfly matrix." << endl
 		<< "Input the vector by entering index and value." << endl
