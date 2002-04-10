2f1c8cec1a66f2b43483833023cbaf63eec5c48b
saunders <bdsaunders@users.noreply.github.com>
2002-04-10 23:26:50 +0000

    test-scalar-matrix.C:  New test for new blackbox.

---
+++ linbox/tests/test-scalar-matrix.C
@@ -0,0 +1,164 @@
+/* -*- mode: c; style: linux -*- */
+
+/* tests/test-scalar-matrix.C
+ * Evolved from test-diagonal.C  -bds
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#include <iostream.h>
+#include <fstream>
+#include <vector>
+
+//#include "linbox/field/archetype.h"
+#include "linbox/field/large-modular.h"
+#include "linbox/blackbox/scalar-matrix.h"
+#include "linbox/solutions/minpoly.h"
+
+#include "test-common.h"
+#include "test-generic.h"
+
+using namespace LinBox;
+
+/* Test 1: Application of identity matrix onto random vectors
+ *
+ * Construct the identity matrix and a series of randomly-generated
+ * vectors. Apply the identity to each vector and test whether the input and
+ * output are equal.
+ *
+ * F - Field over which to perform computations
+ * n - Dimension to which to make matrix
+ * report - Stream to which to output detailed report of failures, if any
+ *
+ * Return true on success and false on failure
+ */
+
+template <class Field>
+static bool testScalarApply (Field &F, size_t n, ostream &report) 
+{
+	typedef typename Field::element element;
+	typedef vector <typename Field::element> Vector;
+	typedef vector <pair <size_t, typename Field::element> > Row;
+	typedef ScalarMatrix <Field, Vector> Blackbox;
+
+	cout << "Testing scalar matrix apply with 1, 0, random ...";
+	cout.flush ();
+	report << "Testing identity apply:" << endl;
+
+	bool ret = true;
+
+	Vector u(n), v(n), w(n);
+	typename Field::RandIter r (F);
+	for (int j = 0; j < n; j++) r.random (v[j]);
+
+	element d;
+	/*  indentity matrix */
+	F.init (d, 1);
+	Blackbox I (F, n, d);
+
+	if (I.rowdim() != n || n != I.coldim())
+	  { ret = false; report << "    Bad dimension(s), should be " << n << endl; }
+
+	report << "    Input vector:  ";
+	printVector<Field> (F, report, v);
+
+	I.apply (w, v);
+
+	report << "    Output vector (should equal input): ";
+	printVector<Field> (F, report, w);
+
+	for (int j = 0; j < n; j++)
+		if (!F.areEqual (w[j], v[j])) {ret = false; break;}
+
+	/*  square zero matrix */
+	F.init (d, 0);
+	Blackbox Z (F, n, d);
+
+	report << "    Input vector:  ";
+	printVector<Field> (F, report, v);
+
+	Z.apply (w, v);
+
+	report << "    Output vector (should be zero): ";
+	printVector<Field> (F, report, w);
+
+	for (int j = 0; j < n; j++)
+		if (!F.isZero(w[j]) ) {ret = false; break;}
+
+	/* random scalar matrix */
+	do {r.random (d);} while ( F.isZero(d) );
+	Blackbox R (F, n, d);
+
+	report << "    Input vector:  ";
+	printVector<Field> (F, report, v);
+
+	R.apply (w, v);
+
+	report << "    apply Output vector (random): ";
+	printVector<Field> (F, report, w);
+
+	R.applyTranspose (u, v);
+
+	report << "    applyTranspose output vector (should equal apply output): ";
+	printVector<Field> (F, report, u);
+
+	for (int j = 0; j < n; j++)
+		if (!F.areEqual (u[j], w[j])) {ret = false; break;}
+
+	F.invin(d);
+	Blackbox Rinv (F, n, d);
+
+	Rinv.apply (u, w);
+
+	report << "    Inverse output vector (should equal input): ";
+	printVector<Field> (F, report, u);
+
+	for (int j = 0; j < n; j++)
+		if (!F.areEqual (u[j], v[j])) {ret = false; break;}
+
+	if (ret) {
+		cout << "passed" << endl;
+		report << "Test passed" << endl << endl;
+	} else {
+		cout << "FAILED" << endl;
+		report << "Test FAILED" << endl << endl;
+	}
+
+	cout.flush ();
+	return ret;
+}
+
+int main (int argc, char **argv)
+{
+	ofstream report;
+
+	bool pass = true;
+
+	static size_t n = 10;
+	static integer q = 4294967291U;
+	static int iterations = 1;
+	static int k = 3;
+	static int N = 20;
+
+	static Argument args[] = {
+		{ 'n', "-n N", "Set dimension of test matrices to NxN (default 10)",        TYPE_INT,     &n },
+		{ 'q', "-q Q", "Operate over the \"field\" GF(Q) [1] (default 4294967291)", TYPE_INTEGER, &q },
+		{ 'i', "-i I", "Perform each test for I iterations (default 100)",          TYPE_INT,     &iterations },
+	};
+
+	parseArguments (argc, argv, args);
+	LargeModular F (q);
+
+	srand (time (NULL));
+
+	cout << "Scalar matrix black box test suite" << endl << endl;
+
+	pass = pass && testScalarApply<LargeModular> (F, n, report);
+
+	//if (!testRandomMinpoly<LargeModular>    (F, n, report, iterations)) pass = false;
+	//if (!testRandomTranspose<LargeModular>  (F, n, report, iterations)) pass = false;
+
+	return pass ? 0 : -1;
+}
