7487f6d942cfd975fbbd41c4b3ab68435341c2ee
giorgi <giorgi@8563fe0b-bd16-0410-b45f-99c24e105077>
2002-04-26 17:49:42 +0000

    2002-04-26 Pascal Giorgi   <pascal.giorgi@ens-lyon.fr>
    	* lidia-gfq.h lidia.h : adding wrapper of extensions fields for Lidia

---
+++ linbox/linbox/field/lidia.h
@@ -0,0 +1,16 @@
+/* -*- mode: c; style: linux -*- */
+
+/* linbox/field/lidia.h
+ * Copyright (C) 2002 Pascal Giorgi
+ *
+ * Written by Pascal Giorgi <pascal.giorgi@ens-lyon.fr>
+ *
+ * See COPYING for license information.
+ */
+
+#ifndef __FIELD_LIDIA_H
+#define __FIELD_LIDIA_H
+
+#include "linbox/field/lidia-gfq.h"
+
+#endif // __FIELD_LIDIA_H
