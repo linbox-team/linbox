08e0cebbbbb46f1a3c9a4b2ea14ffd15b7c49195
hovinen <hovinen@8563fe0b-bd16-0410-b45f-99c24e105077>
2002-08-09 22:12:17 +0000

    2002-08-09  Bradford Hovinen  <hovinen@cis.udel.edu>
            * linbox/blackbox/dense.h (class DenseMatrix): Make _F a const
            reference
            (DenseMatrix::DenseMatrix): Make F const
    
            * linbox/blackbox/dense.h:
    
            * linbox/blackbox/dense.inl: Reindent and remove compiler warnings

--- trunk/linbox/tests/test-dense.C
+++ trunk/linbox/tests/test-dense.C
@@ -1,24 +1,14 @@
 /* -*- mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 
-/* tests/test-dense-matrix.C
+/* tests/test-dense.C
  * Copyright (C) 2001, 2002 Bradford Hovinen
  *
  * Written by Bradford Hovinen <hovinen@cis.udel.edu>
  * Modified by Zhendong Wan <wan@cis.udel.edu>
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
- * Lesser General Public License for more details.
+ * --------------------------------------------------------
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * See COPYING for license information
  */
 
 #include "linbox-config.h"
@@ -30,7 +20,7 @@
 
 #include "linbox/util/commentator.h"
 #include "linbox/field/modular.h"
-#include "linbox/blackbox/dense-matrix1.h"
+#include "linbox/blackbox/dense.h"
 #include "linbox/solutions/minpoly.h"
 
 #include "test-common.h"
