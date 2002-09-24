25a6bc8357ead1bb60fc6fb43e34718bbee71c0d
hovinen <hovinen@8563fe0b-bd16-0410-b45f-99c24e105077>
2002-09-24 01:31:21 +0000

    2002-09-23  Bradford Hovinen  <bghovinen@math.uwaterloo.ca>
    	* linbox/algorithms/wiedemann.h (solveWiedemannSingular): Don't
    	use DenseVector
    	(solveWiedemann): Compute m_A rather than m_A,b
    	(solveWiedemannSingular): Fix generator for random solution
    	(solveWiedemannSingular): Add butterfly preconditioner support
    	(solveWiedemannSingular): Cancel this loop iteration if the
    	leading principal minor turned out to be singular
    
    	* tests/test-solve.C (testSingularConsistentSolve): Implement
    	(testNonsingularSolve): Rename from testDiagonalSolve
    	(testSingularInconsistentSolve): Implement

--- linbox/tests/test-solve.C
+++ linbox/tests/test-solve.C
@@ -101,125 +101,7 @@ static bool testIdentitySolve (const Field &F, VectorStream<Vector> &stream)
 	return ret;
 }
 
-#if 0
-
-/* Test 3: Inverse of Vandermonde matrix
- *
- * Computes a random Vandermonde matrix and its inverse. This inverse is a
- * linear operator that interpolates the values given in the input vector to
- * produce a polynomial whose coefficients are the elements of the output
- * vector. We then evaluate the polynomial in Horner fashion at each of the
- * evaluation points generated above to check whether the result is the original
- * input vector.
- * 
- * F - Field over which to perform computations
- * n - Dimension to which to make matrix
- * iterations - Number of random diagonal matrices to construct
- * N - Number of random vectors to which to apply random Vandermonde matrix
- *
- * Return true on success and false on failure
- */
-
-template <class Field, class Vector>
-static bool testVandermondeInverse (const Field           &F,
-				    VectorStream<Vector> &x_stream,
-				    VectorStream<Vector> &v_stream) 
-{
-	typedef DenseMatrix <Field> Blackbox;
-
-	commentator.start ("Testing Vandermonde inverse", "testVandermondeInverse", x_stream.m ());
-
-	bool ret = true;
-	bool inner_iter_passed;
-
-	VectorDomain<Field> VD (F);
-	size_t j, k;
-
-	Blackbox V (F, x_stream.n (), x_stream.n ());
-
-	Vector x, v, w, z;
-	typename Field::Element t;
-
-	VectorWrapper::ensureDim (x, x_stream.n ());
-	VectorWrapper::ensureDim (v, x_stream.n ());
-	VectorWrapper::ensureDim (w, x_stream.n ());
-	VectorWrapper::ensureDim (z, x_stream.n ());
-
-	while (x_stream) {
-		commentator.startIteration (x_stream.j ());
-
-		/* Evaluation points */
-		x_stream.next (x);
-
-		ostream &report = commentator.report (Commentator::LEVEL_IMPORTANT, INTERNAL_DESCRIPTION);
-		report << "Evaluation points: ";
-		VD.write (report, x);
-		report << endl;
-		report.flush ();
-
-		/* Build the Vandermonde matrix */
-		for (j = 0; j < x_stream.n (); j++) {
-			F.init (t, 1);
-
-			for (k = 0; k < x_stream.n (); k++) {
-				V.setEntry (j, k, t);
-				F.mulin (t, VectorWrapper::ref<Field> (x, j));
-			}
-		}
-
-		Inverse<Field, Vector> VT (F, &V);
-
-		v_stream.reset ();
-
-		while (v_stream) {
-			inner_iter_passed = true;
-
-			/* Random vector of evaluation results */
-			v_stream.next (v);
-
-			commentator.indent (report);
-			report << "Input vector: ";
-			VD.write (report, v);
-			report << endl;
-
-			/* w should now be the requisite polynomial */
-			VT.apply (w, v);
-
-			commentator.indent (report);
-			report << "Output vector: ";
-			VD.write (report, w);
-			report << endl;
-
-			/* Multipoint evaluation to check whether w is correct */
-			multiEvalPoly (F, z, w, x);
-
-			commentator.indent (report);
-			report << "Evaluation results: ";
-			VD.write (report, z);
-			report << endl;
-
-			if (!VD.areEqual (z, v))
-				ret = inner_iter_passed = false;
-
-			if (!inner_iter_passed)
-				commentator.report (Commentator::LEVEL_IMPORTANT, INTERNAL_ERROR)
-					<< "ERROR: Vectors are not equal" << endl;
-		}
-
-		commentator.stop ("done");
-		commentator.progress ();
-	}
-
-	x_stream.reset ();
-
-	commentator.stop (MSG_STATUS (ret), (const char *) 0, "testVandermondeInverse");
-
-	return ret;
-}
-
-#endif
-
-/* Test 3: Solution of diagonal system
+/* Test 2: Solution of diagonal system
  *
  * Constructs a random nonsingular diagonal matrix D and a random right-hand
  * side b, and computes the solution to the Dx=b, checking the result
@@ -232,13 +114,13 @@ static bool testVandermondeInverse (const Field           &F,
  */
 
 template <class Field, class Vector>
-static bool testDiagonalSolve (const Field &F,
-			       VectorStream<Vector> &stream1, 
-			       VectorStream<Vector> &stream2) 
+static bool testNonsingularSolve (const Field &F,
+				  VectorStream<Vector> &stream1, 
+				  VectorStream<Vector> &stream2) 
 {
 	typedef Diagonal <Field, Vector> Blackbox;
 
-	commentator.start ("Testing diagonal solve", "testDiagonalSolve", stream1.m ());
+	commentator.start ("Testing nonsingular solve", "testNonsingularSolve", stream1.m ());
 
 	VectorDomain<Field> VD (F);
 
@@ -300,7 +182,255 @@ static bool testDiagonalSolve (const Field &F,
 	stream1.reset ();
 	stream2.reset ();
 
-	commentator.stop (MSG_STATUS (ret), (const char *) 0, "testDiagonalSolve");
+	commentator.stop (MSG_STATUS (ret), (const char *) 0, "testNonsingularSolve");
+
+	return ret;
+}
+
+/* Test 3: Solution of a consistent singular diagonal system with nonsingular
+ * leading principal minor
+ *
+ * Constructs a random diagonal matrix D of rank r and a random right-hand
+ * side b, and computes the solution to the Dx=b, checking the result
+ * 
+ * F - Field over which to perform computations
+ * n - Dimension to which to make matrix
+ * iterations - Number of random diagonal matrices to construct
+ *
+ * Return true on success and false on failure
+ */
+
+template <class Field, class Vector>
+static bool testSingularConsistentSolve (const Field &F,
+					 unsigned int n,
+					 VectorStream<Vector> &stream1,
+					 VectorStream<Vector> &stream2) 
+{
+	typedef Diagonal <Field, Vector> Blackbox;
+
+	commentator.start ("Testing singular consistent solve", "testSingularConsistentSolve", stream1.m ());
+
+	VectorDomain<Field> VD (F);
+
+	bool ret = true;
+	bool iter_passed;
+
+	Vector d1, b1, d, b, x, y;
+
+	VectorWrapper::ensureDim (d, n);
+	VectorWrapper::ensureDim (b, n);
+	VectorWrapper::ensureDim (x, n);
+	VectorWrapper::ensureDim (y, n);
+
+	SolverTraits traits (SolverTraits::METHOD_WIEDEMANN, false);
+
+	while (stream1 && stream2) {
+		commentator.startIteration (stream1.j ());
+
+		iter_passed = true;
+
+		stream1.next (d1);
+		stream2.next (b1);
+
+		VD.copy (d, d1);
+		VD.copy (b, b1);
+
+		ostream &report = commentator.report (Commentator::LEVEL_IMPORTANT, INTERNAL_DESCRIPTION);
+		report << "Diagonal entries: ";
+		VD.write (report, d);
+		report << endl;
+
+		commentator.indent (report);
+		report << "Right-hand side:  ";
+		VD.write (report, b);
+		report << endl;
+
+		Blackbox D (F, d);
+
+		solve (D, x, b, F, traits);
+
+		commentator.indent (report);
+		report << "System solution:  ";
+		VD.write (report, x);
+		report << endl;
+
+		D.apply (y, x);
+
+		commentator.indent (report);
+		report << "Output:           ";
+		VD.write (report, y);
+		report << endl;
+
+		if (!VD.areEqual (y, b))
+			ret = iter_passed = false;
+
+		if (!iter_passed)
+			commentator.report (Commentator::LEVEL_IMPORTANT, INTERNAL_ERROR)
+				<< "ERROR: Computed solution is incorrect" << endl;
+
+		commentator.stop ("done");
+		commentator.progress ();
+	}
+
+	stream1.reset ();
+	stream2.reset ();
+
+	commentator.stop (MSG_STATUS (ret), (const char *) 0, "testSingularConsistentSolve");
+
+	return ret;
+}
+
+/* Test 4: Solution of an inconsistent singular diagonal system with nonsingular
+ * leading principal minor
+ *
+ * Constructs a random diagonal matrix D of rank r and a random right-hand
+ * side b, and computes the solution to the Dx=b, checking the result
+ * 
+ * F - Field over which to perform computations
+ * n - Dimension to which to make matrix
+ * iterations - Number of random diagonal matrices to construct
+ *
+ * Return true on success and false on failure
+ */
+
+template <class Field, class Vector>
+static bool testSingularInconsistentSolve (const Field &F,
+					   VectorStream<Vector> &stream1,
+					   VectorStream<Vector> &stream2) 
+{
+	typedef Diagonal <Field, Vector> Blackbox;
+
+	commentator.start ("Testing singular inconsistent solve", "testSingularInconsistentSolve", stream1.m ());
+
+	VectorDomain<Field> VD (F);
+
+	bool ret = true;
+	bool iter_passed;
+
+	Vector d1, d, b, x, y;
+
+	VectorWrapper::ensureDim (d, stream2.dim ());
+	VectorWrapper::ensureDim (b, stream2.dim ());
+	VectorWrapper::ensureDim (x, stream2.dim ());
+	VectorWrapper::ensureDim (y, stream2.dim ());
+
+	while (stream1 && stream2) {
+		commentator.startIteration (stream1.j ());
+
+		iter_passed = true;
+
+		stream1.next (d1);
+		stream2.next (b);
+
+		VD.copy (d, d1);
+
+		ostream &report = commentator.report (Commentator::LEVEL_IMPORTANT, INTERNAL_DESCRIPTION);
+		report << "Diagonal entries: ";
+		VD.write (report, d);
+		report << endl;
+
+		commentator.indent (report);
+		report << "Right-hand side:  ";
+		VD.write (report, b);
+		report << endl;
+
+		Blackbox D (F, d);
+
+		solve (D, x, b, F);
+
+		commentator.indent (report);
+		report << "System solution:  ";
+		VD.write (report, x);
+		report << endl;
+
+		D.apply (y, x);
+
+		commentator.stop ("done");
+		commentator.progress ();
+	}
+
+	stream1.reset ();
+	stream2.reset ();
+
+	commentator.stop (MSG_STATUS (ret), (const char *) 0, "testSingularInconsistentSolve");
+
+	return ret;
+}
+
+/* Test 5: Solution of an inconsistent singular diagonal system with singular
+ * leading principal minor
+ *
+ * Constructs a random diagonal matrix D of rank r and a random right-hand
+ * side b, and computes the solution to the Dx=b, checking the result
+ * 
+ * F - Field over which to perform computations
+ * n - Dimension to which to make matrix
+ * iterations - Number of random diagonal matrices to construct
+ *
+ * Return true on success and false on failure
+ */
+
+template <class Field, class Vector, class SparseVector>
+static bool testSingularPreconditionedSolve (const Field &F,
+					     VectorStream<SparseVector> &stream1,
+					     VectorStream<Vector> &stream2) 
+{
+	typedef Diagonal <Field, Vector> Blackbox;
+
+	commentator.start ("Testing singular preconditioned solve", "testSingularPreconditionedSolve", stream1.m ());
+
+	VectorDomain<Field> VD (F);
+
+	bool ret = true;
+	bool iter_passed;
+
+	SparseVector d1;
+	Vector d, b, x, y;
+
+	VectorWrapper::ensureDim (d, stream2.dim ());
+	VectorWrapper::ensureDim (b, stream2.dim ());
+	VectorWrapper::ensureDim (x, stream2.dim ());
+	VectorWrapper::ensureDim (y, stream2.dim ());
+
+	while (stream1 && stream2) {
+		commentator.startIteration (stream1.j ());
+
+		iter_passed = true;
+
+		stream1.next (d1);
+		stream2.next (b);
+
+		VD.copy (d, d1);
+
+		ostream &report = commentator.report (Commentator::LEVEL_IMPORTANT, INTERNAL_DESCRIPTION);
+		report << "Diagonal entries: ";
+		VD.write (report, d);
+		report << endl;
+
+		commentator.indent (report);
+		report << "Right-hand side:  ";
+		VD.write (report, b);
+		report << endl;
+
+		Blackbox D (F, d);
+
+		solve (D, x, b, F);
+
+		commentator.indent (report);
+		report << "System solution:  ";
+		VD.write (report, x);
+		report << endl;
+
+		D.apply (y, x);
+
+		commentator.stop ("done");
+		commentator.progress ();
+	}
+
+	stream1.reset ();
+	stream2.reset ();
+
+	commentator.stop (MSG_STATUS (ret), (const char *) 0, "testSingularPreconditionedSolve");
 
 	return ret;
 }
@@ -309,35 +439,37 @@ int main (int argc, char **argv)
 {
 	bool pass = true;
 
-	static size_t n = 10;
+	static size_t n = 100;
+	static size_t r = 20;
 	static integer q = 2147483647U;
-	static int iterations = 100;
-	static int N = 1;
+	static int iterations = 10;
 
 	static Argument args[] = {
-		{ 'n', "-n N", "Set dimension of test matrices to NxN (default 10)",        TYPE_INT,     &n },
+		{ 'n', "-n N", "Set dimension of test matrices to NxN (default 100)",       TYPE_INT,     &n },
+		{ 'r', "-r R", "Set singular system rank to R (default 20)",                TYPE_INT,     &r },
 		{ 'q', "-q Q", "Operate over the \"field\" GF(Q) [1] (default 2147483647)", TYPE_INTEGER, &q },
-		{ 'i', "-i I", "Perform each test for I iterations (default 100)",          TYPE_INT,     &iterations },
-		{ 'N', "-N N", "Apply Vandermonde inverse to N vectors (default 1)",        TYPE_INT,     &N },
+		{ 'i', "-i I", "Perform each test for I iterations (default 10)",           TYPE_INT,     &iterations },
 	};
 
 	typedef Modular<uint32> Field;
-	typedef vector<Field::Element> DenseVector;
 
 	parseArguments (argc, argv, args);
 	Field F (q);
 
-	srand (time (NULL));
-
 	cout << "Solve test suite" << endl << endl;
 
-	commentator.getMessageClass (INTERNAL_DESCRIPTION).setMaxDepth (4);
+	commentator.getMessageClass (INTERNAL_DESCRIPTION).setMaxDepth (5);
 	commentator.getMessageClass (INTERNAL_DESCRIPTION).setMaxDetailLevel (Commentator::LEVEL_UNIMPORTANT);
 
-	RandomDenseStream<Field, DenseVector> stream1 (F, n, iterations), stream2 (F, n, iterations);
+	RandomDenseStream<Field> stream1 (F, n, iterations), stream2 (F, n, iterations);
+	RandomDenseStream<Field> stream3 (F, r, iterations), stream4 (F, r, iterations);
+	RandomSparseStream<Field> stream6 (F, n, (double) r / (double) n, iterations);
 
-	if (!testIdentitySolve (F, stream1)) pass = false;
-	if (!testDiagonalSolve (F, stream1, stream2)) pass = false;
+	if (!testIdentitySolve               (F, stream1)) pass = false;
+	if (!testNonsingularSolve            (F, stream1, stream2)) pass = false;
+	if (!testSingularConsistentSolve     (F, n, stream3, stream4)) pass = false;
+	if (!testSingularInconsistentSolve   (F, stream3, stream2)) pass = false;
+	if (!testSingularPreconditionedSolve (F, stream6, stream2)) pass = false;
 
 	return pass ? 0 : -1;
 }
