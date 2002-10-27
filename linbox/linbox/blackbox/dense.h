23ec9797c52a898085260b89a128578c9ab18a77
hovinen <hovinen@8563fe0b-bd16-0410-b45f-99c24e105077>
2002-10-27 22:22:12 +0000

    2002-10-27  Bradford Hovinen  <bghovinen@math.uwaterloo.ca>
    	* linbox/blackbox/dense.h (DenseMatrix::refEntry): Define
    	(class DenseMatrix): Reorder the declarations to match
    	SparseMatrix0Base; reindent; add Doc++ documentation to the
    	interfaces
    	(DenseMatrix::getEntry): Add a form that returns a read-only
    	reference to the entry
    	(class DenseMatrix): Make generic wrt vector type
    	(class DenseMatrix): Move some of the one-liners from dense.inl
    	into the class

--- linbox/linbox/blackbox/dense.h
+++ linbox/linbox/blackbox/dense.h
@@ -48,14 +48,13 @@ namespace LinBox
  * @param Field \Ref{LinBox} field
  */
   
-template <class Field>
-class DenseMatrix : public BlackboxArchetype<std::vector<typename Field::Element> >
+template <class Field, class Vector = typename LinBox::Vector<Field>::Dense>
+class DenseMatrix : public BlackboxArchetype<Vector>
 {
     public:
-	typedef typename Field::Element                 Element;
-	typedef typename std::vector<Element>           Vector;
-	typedef typename std::vector<Element>::iterator pointer;
-    
+	typedef typename Field::Element   Element;
+	typedef typename Vector::iterator pointer;
+
 	/** Constructor.
 	 * @param  F the field of entries; passed so that arithmetic may be done on elements. 
 	 * @param  m  row dimension
@@ -72,16 +71,18 @@ class DenseMatrix : public BlackboxArchetype<std::vector<typename Field::Element
 	 * @para iter, random iterator
 	 */
 	template<class RandIter>
-	DenseMatrix (const Field &F, size_t m, size_t n, RandIter& iter)
+	DenseMatrix (const Field &F, size_t m, size_t n, RandIter &iter)
 		: _F (F), _VD (F), _rep (m * n), _rows (m), _cols (n)
 	{
 		for (typename Vector::iterator p = _rep.begin (); p != _rep.end (); ++p)
 			iter.random (*p);
 	}
     
-	/** Constructor.
-	 * @param  F the field of entries; passed so that arithmetic may be done on elements. 
-	 * @param  stream A vector stream to use as a source of vectors for this matrix
+	/** Constructor using a vector stream
+	 * @param  F The field of entries; passed so that arithmetic may be done
+	 *           on elements. 
+	 * @param  stream A vector stream to use as a source of vectors for this
+	 *                matrix
 	 */
 	template <class StreamVector>
 	DenseMatrix (const Field &F, VectorStream<StreamVector> &stream)
@@ -102,99 +103,107 @@ class DenseMatrix : public BlackboxArchetype<std::vector<typename Field::Element
 	DenseMatrix (const DenseMatrix &M)
 		: _F (M._F), _VD (M._F), _rep (M._rep),_rows (M._rows), _cols (M._cols)
 	{}
-    
-	/// Blackbox interface
-    
+
+	/** Construct a copy of the matrix and return a pointer to it
+	 * @return Pointer to copy of the matrix
+	 */
 	BlackboxArchetype<Vector> *clone () const 
-		{ return new DenseMatrix<Field> (*this); }
-    
-	template<class Vect1, class Vect2>
-	Vect1& apply (Vect1& y, const Vect2& x) const;
-    
-	Vector& apply (Vector &y, const Vector &x) const
-		{ return apply<Vector,Vector> (y, x); }
-    
-	template<class Vect1>
-	Vect1& applyIn (Vect1& y) const
-	{
-		std::vector<Element> x (y.begin (),y.end ());
-		apply (y,x);
-		return y;
-	}
-      
-	Vector& applyIn (Vector& y) const
-	{ return applyIn<Vector> (y);}
+		{ return new DenseMatrix<Field, Vector> (*this); }
 
-	template<class Iterator1, class Iterator2 >
-	Iterator1& apply (Iterator1 out, 
-			  const Iterator2& inbegin, 
-			  const Iterator2& inend) const;
-   
-	template<class Vect1, class Vect2>
-	Vect1& applyTranspose (Vect1& y, const Vect2& x) const;
-    
-	Vector& applyTranspose (Vector& y, const Vector& x) const
-		{ return applyTranspose<Vector,Vector> (y, x); }
+	/** Get the number of rows in the matrix
+	 * @return Number of rows in matrix
+	 */
+	size_t rowdim () const
+		{ return _rows; }
 
-	template<class Vect>
-	Vect& applyTransposeIn (Vect& y) const
-	{
-		std::vector<Element> x (y.begin (), y.end ());
-		applyTranspose (y, x);
-		return y;
-	}
-  
-	Vector& applyTransposeIn (Vector& y) const
-		{ return applyTransposeIn<Vector> (y); }
-    
-	template<class Iterator1, class Iterator2>
-	Iterator1& applyTranspose (Iterator1 out, 
-				   const Iterator2& inbegin, 
-				   const Iterator2& inend) const;
+	/** Get the number of columns in the matrix
+	 * @return Number of columns in matrix
+	 */
+	size_t coldim () const
+		{ return _cols; }
 
-	size_t rowdim (void) const;
-	size_t coldim (void) const;
-    
-	/// entry access raw view.  Size m*x vector in C (row major) order.
-	typedef typename Vector::iterator RawIterator;
-	typedef typename Vector::const_iterator ConstRawIterator;
-    
-	RawIterator rawBegin ();		  
-	RawIterator rawEnd ();
-    
-	ConstRawIterator rawBegin () const;
-	ConstRawIterator rawEnd () const;
+	/** @name Input and output
+	 */
 
-	/** @name Index iterator
-         * The index iterator gives the row, column indices of all matrix
-         * elements in the same order as the raw iterator above.
-         */
-        class RawIndexIterator;
-        typedef const RawIndexIterator ConstRawIndexIterator;
+	//@{
 
-        RawIndexIterator rawIndexBegin();
-        RawIndexIterator rawIndexEnd();   
-	ConstRawIndexIterator rawIndexBegin() const;
-        ConstRawIndexIterator rawIndexEnd() const;   
-  
-	// col sequence of rows view
+	/** Read the matrix from an input stream
+	 * @param file Input stream from which to read
+	 */
+	void read (std::istream &file);
+    
+	/** Write the matrix to an output stream
+	 * @param os Output stream to which to write
+	 */
+	std::ostream &write (std::ostream &os = std::cout) const;
+ 
+	//@}
+
+	/** @name Access to matrix elements
+	 */
+
+	//@{
+
+	/** Set the entry at (i, j)
+	 * @param i Row number, 0...rowdim () - 1
+	 * @param j Column number 0...coldim () - 1
+	 * @param a_ij Element to set
+	 */
+	void setEntry (size_t i, size_t j, const Element &a_ij)
+		{ _rep[i*_cols+j] = a_ij; }
+
+	/** Get a writeable reference to an entry in the matrix
+	 * @param i Row index of entry
+	 * @param j Column index of entry
+	 * @return Reference to matrix entry
+	 */
+	Element &refEntry (size_t i, size_t j)
+		{ return _rep[i * _cols + j]; }
+
+	/** Get a read-only individual entry from the matrix
+	 * @param i Row index
+	 * @param j Column index
+	 * @return Const reference to matrix entry
+	 */
+	const Element &getEntry (size_t i, size_t j) const
+		{ return _rep[i * _cols + j]; }
+
+	/** Get an entry and store it in the given value
+	 * This form is more in the Linbox style and is provided for interface
+	 * compatibility with other parts of the library
+	 * @param x Element in which to store result
+	 * @param i Row index
+	 * @param j Column index
+	 * @return Reference to x
+	 */
+	Element &getEntry (Element &x, size_t i, size_t j) const
+		{ x = _rep[i * _cols + j]; return x; }
+
+	/** @name Columns of rows iterator
+	 * The columns of row iterator gives each of the rows of the
+	 * matrix in ascending order. Dereferencing the iterator yields
+	 * a row vector in dense format
+	 */
+
 	typedef typename Vector::iterator RowIterator;
 	typedef typename Vector::const_iterator ConstRowIterator;
-    
-	class ColOfRowsIterator;
-    
-	typedef Subvector<DenseMatrix<Field>::RowIterator> Row;  
-	typedef Subvector<DenseMatrix<Field>::ConstRowIterator> ConstRow;
-     
+
+	typedef Subvector<RowIterator> Row;  
+	typedef Subvector<ConstRowIterator> ConstRow;
+
 	class ColOfRowsIterator;    
 	class ConstColOfRowsIterator;
 
 	ColOfRowsIterator colOfRowsBegin ();  
 	ColOfRowsIterator colOfRowsEnd ();
- 
 	ConstColOfRowsIterator colOfRowsBegin () const;        
 	ConstColOfRowsIterator colOfRowsEnd () const;
-    
+
+	/** @name Row of columns iterator
+	 * The row of columns iterator gives each of the columns of the
+	 * matrix in ascending order. Dereferencing the iterator yields
+	 * a column vector in dense format
+	 */
 
 	typedef Subiterator<typename Vector::iterator> ColIterator;
 	typedef Subiterator<typename Vector::const_iterator> ConstColIterator;
@@ -207,37 +216,172 @@ class DenseMatrix : public BlackboxArchetype<std::vector<typename Field::Element
     
 	RowOfColsIterator rowOfColsBegin ();
 	RowOfColsIterator rowOfColsEnd ();
-    
 	ConstRowOfColsIterator rowOfColsBegin () const;    
 	ConstRowOfColsIterator rowOfColsEnd () const;
-    
-	/** Set the entry at (i, j)
-	 * @param i Row number, 0...rowdim () - 1
-	 * @param j Column number 0...coldim () - 1
-	 * @param a_ij Element to set
+
+	/** @name Raw iterator
+	 *
+	 * The raw iterator is a method for accessing all entries in the matrix
+	 * in some unspecified order. This can be used, e.g. to reduce all
+	 * matrix entries modulo a prime before passing the matrix into an
+	 * algorithm.
 	 */
-	void setEntry (size_t i, size_t j, const Element& a_ij) ;
+
+	typedef typename Vector::iterator RawIterator;
+	typedef typename Vector::const_iterator ConstRawIterator;
     
-	Element& getEntry (Element& a_ij, size_t i, size_t j) const;
-    
-	/** Read the matrix from an input stream
-	 * @param file Input stream from which to read
+	RawIterator rawBegin ();		  
+	RawIterator rawEnd ();
+	ConstRawIterator rawBegin () const;
+	ConstRawIterator rawEnd () const;
+
+	/** @name Index iterator
+	 * The index iterator gives the row, column indices of all matrix
+	 * elements in the same order as the raw iterator above. Its value type
+	 * is an STL pair with the row and column indices, starting at 0, in the
+	 * first and second positions, respectively.
 	 */
-	void read (std::istream &file);
+
+        class RawIndexIterator;
+        typedef const RawIndexIterator ConstRawIndexIterator;
+
+        RawIndexIterator rawIndexBegin();
+        RawIndexIterator rawIndexEnd();   
+	ConstRawIndexIterator rawIndexBegin() const;
+        ConstRawIndexIterator rawIndexEnd() const;   
     
-	/** Write the matrix to an output stream
-	 * @param os Output stream to which to write
+	/** Retrieve a reference to a row
+	 * @param i Row index
 	 */
-	std::ostream &write (std::ostream &os = std::cout) const;
- 
-	const Field& field () const
-		{ return _F;}
-    
 	Row operator[] (size_t i);
 	ConstRow operator[] (size_t i) const;
-   
-    protected:
+
+	//@}
+
+	/** @name Black box interface
+	 */
+
+	//@{
+
+	/** Generic matrix-vector apply
+	 * y = A * x
+	 * This version of apply allows use of arbitrary input and output vector
+	 * types
+	 * @param y Output vector
+	 * @param x Input vector
+	 * @return Reference to output vector
+	 */
+	template<class Vect1, class Vect2>
+	Vect1 &apply (Vect1 &y, const Vect2 &x) const;
+
+	/** Generic in-place apply
+	 * y = A * y
+	 * This version of in-place apply allows use of an arbitrary vector
+	 * type. Because it performs allocation and copying, it is not
+	 * recommended for general use.
+	 * @param y Input vector
+	 * @return Reference to output vector
+	 */
+	template<class Vect1>
+	Vect1 &applyIn (Vect1 &y) const
+	{
+		std::vector<Element> x (y.begin (),y.end ());
+		apply (y,x);
+		return y;
+	}
+
+	/** Matrix-vector apply
+	 * y = A * x
+	 * This implements the @ref{BlackboxArchetype} apply requirement
+	 * @param y Output vector
+	 * @param x Input vector
+	 * @return Reference to output vector
+	 */
+	Vector &apply (Vector &y, const Vector &x) const
+		{ return apply<Vector, Vector> (y, x); }
+
+	/** Iterator form of apply
+	 * This form of apply takes iterators specifying the beginning and end
+	 * of the vector to which to apply the matrix, and the beginning of the
+	 * vector at which to store the result of application. It is generic
+	 * with respect to iterator type, allowing different iterators to be
+	 * used for the input and output vectors.
+	 * @param out Beginning of output vector
+	 * @param inbegin Beginning of input vector
+	 * @param outbegin End of input vector
+	 * @return Reference to beginning of output vector
+	 */
+	template<class Iterator1, class Iterator2 >
+	Iterator1 &apply (Iterator1 out, 
+			  const Iterator2 &inbegin, 
+			  const Iterator2 &inend) const;
+
+	/** Generic matrix-vector transpose apply
+	 * y = A^T * x
+	 * This version of applyTranspose allows use of arbitrary input and
+	 * output vector types
+	 * @param y Output vector
+	 * @param x Input vector
+	 * @return Reference to output vector
+	 */
+	template<class Vect1, class Vect2>
+	Vect1 &applyTranspose (Vect1 &y, const Vect2 &x) const;
     
+	/** Generic in-place transpose apply
+	 * y = A^T * y
+	 * This version of in-place transpose apply allows use of an arbitrary
+	 * vector type. Because it performs allocation and copying, it is not
+	 * recommended for general use.
+	 * @param y Input vector
+	 * @return Reference to output vector
+	 */
+	template<class Vect>
+	Vect &applyTransposeIn (Vect &y) const
+	{
+		std::vector<Element> x (y.begin (), y.end ());
+		applyTranspose (y, x);
+		return y;
+	}
+  
+	/** Matrix-vector transpose apply
+	 * y = A^T * x
+	 * This implements the @ref{BlackboxArchetype} applyTranspose
+	 * requirement
+	 * @param y Output vector
+	 * @param x Input vector
+	 * @return Reference to output vector
+	 */
+	Vector &applyTranspose (Vector &y, const Vector &x) const
+		{ return applyTranspose<Vector,Vector> (y, x); }
+    
+	/** Iterator form of transpose apply
+	 *
+	 * This form of transpose apply takes iterators specifying the beginning
+	 * and end of the vector to which to apply the matrix, and the beginning
+	 * of the vector at which to store the result of application. It is
+	 * generic with respect to iterator type, allowing different iterators
+	 * to be used for the input and output vectors.
+	 *
+	 * @param out Beginning of output vector
+	 * @param inbegin Beginning of input vector
+	 * @param outbegin End of input vector
+	 * @return Reference to beginning of output vector
+	 */
+	template<class Iterator1, class Iterator2>
+	Iterator1 &applyTranspose (Iterator1 out, 
+				   const Iterator2 &inbegin, 
+				   const Iterator2 &inend) const;
+
+	//@}
+
+	/** Retrieve the field over which this matrix is defined
+	 * @return Reference to the underlying field
+	 */
+	const Field &field () const
+		{ return _F;}
+
+    protected:
+
 	const Field          &_F;
 	VectorDomain<Field>   _VD;
 	std::vector<Element>  _rep;
