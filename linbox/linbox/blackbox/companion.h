870f1c97e123abd26323273cd47a4715694e5853
wan <wan@8563fe0b-bd16-0410-b45f-99c24e105077>
2003-05-28 20:44:56 +0000

    revise the constructor for companion

--- linbox/linbox/blackbox/companion.h
+++ linbox/linbox/blackbox/companion.h
@@ -41,7 +41,17 @@ struct Companion: public TriplesBB<Field, Vector> {
 		std::vector<typename Field::Element> p(n+1);	       
 		for (typename std::vector<typename Field::Element>::iterator i = p.begin(); i != p.end(); ++i)
 			r.random(*i); // we'll pretend p[n] == 1, ok?
-		Companion(F, p);
+		
+		const size_t indexbase = 1;
+		typename Field::Element one; F.init(one, 1);
+	 	for (size_t i = 1; i < n; ++i) addEntry(one, i+indexbase, i-1+indexbase); 
+	 	for (size_t i = 0; i < n; ++i)
+		{	typename Field::Element x;
+			F.init(x, 0);
+			F.neg(x, p[i]); 
+			addEntry(x, i+indexbase, n-1+indexbase); 
+		}
+	
 	}
 
 	Companion(const Field& F, size_t n) :TriplesBB<Field,Vector>(F,n,n) 
@@ -50,8 +60,20 @@ struct Companion: public TriplesBB<Field, Vector> {
 		std::vector<typename Field::Element> p(n+1);	       
 		for (typename std::vector<typename Field::Element>::iterator i = p.begin(); i != p.end(); ++i)
 			r.random(*i); // we'll pretend p[n] == 1, ok?
-		Companion(F, p);
+
+		const size_t indexbase = 1;
+		typename Field::Element one; F.init(one, 1);
+	 	for (size_t i = 1; i < n; ++i) addEntry(one, i+indexbase, i-1+indexbase); 
+	 	for (size_t i = 0; i < n; ++i)
+		{	typename Field::Element x;
+			F.init(x, 0);
+			F.neg(x, p[i]); 
+			addEntry(x, i+indexbase, n-1+indexbase); 
+		}
+
 	}
+	
+	
 	
 
 // companion would be faster if built direct, using one axpy per entry: y_i = x_i-1 + p_i*x_n
