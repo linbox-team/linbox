5c20264d2802cab6f7867336fde36fefc71b3204
wan <wan@8563fe0b-bd16-0410-b45f-99c24e105077>
2003-05-30 16:59:50 +0000

    * linbox/blackbox/null-matrix.h: remove getInstance member function. Make default constructor public, remove static member nullMatrix * linbox/blackbox/direct-sum.h: update the include file for NullMatrix. * tests/Makefile.am: comment some tests lacking of source codes

--- linbox/linbox/blackbox/null-matrix.h
+++ linbox/linbox/blackbox/null-matrix.h
@@ -11,20 +11,12 @@ namespace LinBox{
   /// is a Blackbox class supporting a single instance of the 0 by 0 matrix.
   template<class Vector>
     class NullMatrix : public BlackboxArchetype<Vector> {
-    protected:
-    static NullMatrix* nullMatrix;
     public:
-    NullMatrix() {cout << "NullMatrix default cstor" << endl;}
+    NullMatrix() {}//cout << "NullMatrix default cstor" << endl;}
     NullMatrix(const NullMatrix& n) {}
 
-    virtual BlackboxArchetype<Vector>* clone() const {return new NullMatrix(getInstance());}
-
-    static NullMatrix& getInstance() { 
-      if(nullMatrix==0) 
-	nullMatrix=new NullMatrix();
-
-      return *nullMatrix;
-    }
+    public:
+    virtual BlackboxArchetype<Vector>* clone() const {return new NullMatrix();}
 
     virtual Vector& apply(Vector& y, const Vector& x) const {
       linbox_check(y.size()==0);
@@ -55,12 +47,8 @@ namespace LinBox{
     virtual inline size_t rowdim() const {return 0;}
 
     virtual inline size_t coldim() const {return 0;}
-    protected:
-    NullMatrix() {}
   };
 
-  template<class Vector>
-    NullMatrix<Vector>* NullMatrix<Vector>::nullMatrix=0;
 }
 
 #endif
