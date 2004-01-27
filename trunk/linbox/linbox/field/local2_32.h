823b59ecec1aa73e3229dc4fb742dd296e0b4b2c
saunders <saunders@8563fe0b-bd16-0410-b45f-99c24e105077>
2004-01-27 18:30:13 +0000

    2004-01-27 BDS <saunders@udel.edu> *add linbox/field/ntl-pid-zz_p.h, linbox/field/local2_32.h, linbox/algroithms/local-smith.h, code for local smith form computation.
    *add linbox/tests/test-local-smith.C, modify linbox/tests/Makefile.am, add test for local smith algorithm

---
+++ trunk/linbox/linbox/field/local2_32.h
@@ -0,0 +1,250 @@
+/* -*- mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+
+/* linbox/field/local2_32.h
+ * written by bds, wan
+ *
+ */
+
+#ifndef __LOCAL2_32_H
+#define __LOCAL2_32_H
+
+#include "linbox/field/unparametric.h"
+#include "linbox-config.h"
+
+namespace LinBox
+{
+  
+	/** @memo Fast arithmetic mod 2^32, including gcd.
+	 *
+	 * @doc extend UnparametricField<uint32> which is a representation 
+	 * of Z_2^32. It is especially fast because it uses hardware arithmetic 
+	 * directly.  This ring is a Local Principal Ideal Ring.
+	 *
+	 * These needed PIR functions are added:
+	 * gcdin(), isUnit(), also inv() is modified to work correctly.
+	 * The type Exponent is added: more effective rep of the powers of 2, 
+	 * which are important because gcds are powers of 2).  
+	 * This entails some new versions of divin(), mulin(), isUnit().
+	 *
+	 * Those are the function needed for the LocalSmith algorithm.
+	 * Further appropriate PIR functions may be added later.
+	 */
+
+	struct Local2_32: public UnparametricField<uint32>
+	{
+	public:
+
+		typedef UnparametricField<uint32>::Element Element;
+		typedef enum {_min=0,_max=32} Exponent; // enum?
+
+		// assume k is an exponent of 2.
+		static inline Exponent& gcdin(Exponent& k, const Element& b)
+		{   /*
+		      Element c = b >> k;
+		      c <<= k;
+		      Element d = b;
+		      std::cout << "c, b" << c << " " << b <<  "\n";
+		      if (c != b)  for(k = 0; ! (d & 1); ++k) d >>= 1; 
+		      std::cout << "g, b =" << (int)k << " " << b << "\n";
+		    */
+			Element d = b;
+			int i;
+			for ( i = 0; (i < k) && (!(d&1)); ++ i) d >>= 1;
+			return k = Exponent(i);
+		}
+
+		static inline bool isUnit(const Exponent& a)
+		{   return a == 0;   }
+
+		static inline bool isZero(const Exponent& a)
+		{   return a >= 32;   }
+
+		// not used ...
+		static inline bool isUnit(const Element& a)
+		{   return a & 1;   }
+
+		//Element& div(Element& c, const Element& a, const Element& b) const
+		//{   return c = NTL::rep(a)/NTL::GCD(NTL::rep(a),NTL::rep(b));   }
+		//
+		
+		static inline Element& mulin(Element& a, const Exponent& k) 
+		{  
+			if (k >= 32) return a = 0;
+			else return a <<= k;  
+		}
+
+		static inline Element& mulin(Element& a, const Element& b)  {
+			return a *= b;
+		}
+
+		static inline Element& axpyin(Element& r, const Element& x, const Element& y) {
+			return r += x * y;
+		}
+
+		// assume k is an exponent of 2 and the power of 2 exactly divides a
+		static inline Element& divin(Element& a, const Exponent& k)
+		{   return a >>= k;   }
+
+		static inline Element& inv(Element& a, const Element& b) {
+
+			if (!isUnit(b))
+				throw PreconditionFailed(__FUNCTION__,__LINE__,"inv: not a unit");
+			else {
+
+				Element g, s, t;
+				
+				xgcd(g, s, t, b, -b);
+				
+				return a = s - t;
+			}
+		}
+
+	protected:
+		
+		static  Element& xgcd(Element& d, Element& s, Element& t, const Element& a, const Element& b) 
+		{
+		       
+			Element  u, v, u0, v0, u1, v1, u2, v2, q, r;
+			
+			u1 = 1; v1 = 0;
+			u2 = 0; v2 = 1;
+			u = a; v = b;
+		    
+			while (v != 0) {
+				q = u / v;
+				r = u % v;
+				u = v;
+				v = r;
+				u0 = u2;
+				v0 = v2;
+				u2 =  u1 - q*u2;
+				v2 = v1- q*v2;
+				u1 = u0;
+				v1 = v0;
+			}
+		    
+		    
+			d = u;
+			s = u1;
+			t = v1;
+			//std::cout << "XGCD is called: d, s, t, a, b, sa + tb: " << d << ' '
+			//	<< s << ' ' << t << ' ' << a << ' ' << b << ' ' << s * a + t * b << '\n';
+			return d;
+
+			/*
+
+			//Element  u, v, u0, v0, u1, v1, u2, v2, q, r;
+			
+			Element u, v, q, r;
+			
+			int64  u0, u1, u2;
+			
+			u1 = 1; //v1 = 0;
+			u2 = 0; //v2 = 1;
+			u = a; v = b;
+		    
+			if ( b == 0) {
+				s = 1;
+				t = 0;
+				return d = a ;
+			}
+			
+			if (v != 0) {
+				q = u / v;
+				r = u % v;
+				u = v;
+				v = r;
+				u0 = u2;
+				//v0 = v2;
+				u2 =  u1 - q * u2;
+				//v2 = v1- q * v2;
+				u1 = u0;
+				//v1 = v0;
+				
+			}
+
+			while (v != 0) {
+				r = u;
+				while ( r >= v) {
+					r = u - v;
+					
+					u2 = u1 - u2;
+				}
+				u0 = u2;
+				u1 = u0;
+				u = v;
+				v = r;
+				
+				
+			}	
+				
+			
+			while (v != 0) {
+				q = u / v;
+				r = u % v;
+				u = v;
+				v = r;
+				
+				u0 = u2;
+				//v0 = v2;
+				u2 =  u1 - q * u2;
+				//v2 = v1- q * v2;
+				u1 = u0;
+				//v1 = v0;
+			}
+			
+		    
+			d = u;
+			s = u1;
+			
+			t = ((int64) d - u1 * (int64) a) / (int64)b;
+
+			//std::cout << "XGCD is called: d, s, t, a, b, sa + tb: " << d << ' '
+			//<< s << ' ' << t << ' ' << a << ' ' << b << ' ' << s * a + t * b << '\n';
+			return d;
+			
+			
+			*/
+		}
+		
+
+		/** @memo HGCD (g, s, a, b).
+		 * Half GCD
+		 * g = gcd (a, b).
+		 * exists t, such that: s * a + t * b = g.
+		 * return g.
+		 */
+		static Element& HGCD (Element& g, Element& s, const Element& a, const  Element& b) {
+			
+			Element  u, v, u0, u1, u2, q, r;
+			
+			u1 = 1;
+			u2 = 0;
+			u = a; v = b;
+		    
+			while (v != 0) {
+				q = u / v;
+				r = u % v;
+				u = v;
+				v = r;
+				
+				u0 = u2;
+				
+				u2 =  u1 - q*u2;
+				
+				u1 = u0;
+				
+			}
+		    
+		    
+			g = u;
+			s = u1;
+
+			return g;	
+	    
+		}
+	};
+		
+} // namespace LinBox
+
+#endif // __LOCAL2_32_H
---
+++ trunk/linbox/linbox/field/ntl-pid-zz_p.h
@@ -0,0 +1,48 @@
+/* -*- mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+
+/* linbox/field/ntl-pid-zz_p.h
+ * written by bds
+ *
+ */
+
+#ifndef __NTL_PID_zz_p_H
+#define __NTL_PID_zz_p_H
+
+#include "linbox/field/ntl-zz_p.h"
+#include "linbox-config.h"
+#include <NTL/ZZ.h>
+
+// Namespace in which all LinBox library code resides
+namespace LinBox
+{
+  
+    /** @memo extend Wrapper of zz_p from NTL.  Add PID functions
+     */
+    struct NTL_PID_zz_p: public NTL_zz_p
+    {
+	protected: long _modulus;
+	public:
+	NTL_PID_zz_p(long pp) 
+    	: NTL_zz_p(pp), _modulus(pp) {}
+
+        Element& gcd(Element& g, const Element& a, const Element& b) const
+	{   g = NTL::GCD(NTL::rep(a), NTL::rep(b));  
+	    g = NTL::GCD(NTL::rep(g), _modulus);
+	    return g;
+	}
+
+        Element& gcdin(Element& a, const Element& b) const
+	{   return gcd(a,a, b);  }
+
+	bool isUnit(const Element& a) const
+	{   return 1 == NTL::GCD(NTL::rep(a), _modulus);  }
+
+	Element& div(Element& c, const Element& a, const Element& b) const
+	{   return c = NTL::rep(a)/NTL::GCD(NTL::rep(a),NTL::rep(b));   }
+	Element& divin(Element& a, const Element& b) const
+	{   return div(a, a, b);   }
+    };
+	    
+} // namespace LinBox
+
+#endif // __NTL_PID_zz_p_H
