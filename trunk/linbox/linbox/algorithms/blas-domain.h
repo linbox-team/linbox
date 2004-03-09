c5ad25d23b04a6efd6cd7b2f647fe36bca67710f
giorgi <giorgi@8563fe0b-bd16-0410-b45f-99c24e105077>
2004-03-09 14:50:02 +0000

    *** empty log message ***

--- trunk/linbox/linbox/algorithms/blas-domain.h
+++ trunk/linbox/linbox/algorithms/blas-domain.h
@@ -57,69 +57,73 @@ namespace LinBox {
 		BlasMatrixDomain (const Field& F ) : _F(F) {}
 	    
 		// Copy constructor
-		BlasMatrixDomain (const BlasMatrixDomain & BMD) : _F(BMD._F) {}
+		BlasMatrixDomain (const BlasMatrixDomain<Field> & BMD) : _F(BMD._F) {}
 
 
 		// Field accessor
 		Field& field() {return _F;}
 
 		// LSP Factorization 
-		const unsigned int LSP (const BlasMatrix<Element>& A, BlasMatrix<Element>& L, BlasMatrix<Element>& S, BlasPermutation& P) const;					
-		// in-place LSP Factorization (L is in compressed format)
-		const unsigned int LSPin (BlasMatrix<Element>& A, BlasPermutation& P) const;
-
+		
+		const unsigned int LSP (const BlasMatrix<Matrix>& A, BlasMatrix<Matrix>& L, BlasMatrix<Matrix>& S, BlasPermutation& P) const;					
 		// LQUP Factorization
-		const unsigned int LQUP (const BlasMatrix<Element>& A, BlasMatrix<Element>& L, BlasPermutation& Q, BlasMatrix<Element>& U, BlasPermutation& P) const;
+		const unsigned int LQUP (const BlasMatrix<Matrix>& A, BlasMatrix<Matrix>& L, BlasPermutation& Q, BlasMatrix<Matrix>& U, BlasPermutation& P) const;
 		
 		// in-place LQUP Factorization (L is in compressed format)
-		const unsigned int LQUPin (BlasMatrix<Element>& A, BlasPermutation& Q, BlasPermutation& P) const;
+		const unsigned int LQUPin (BlasMatrix<Matrix>& A, BlasPermutation& Q, BlasPermutation& P) const;
 
 		// Inversion
-		const BlasMatrix<Element>& inv(const BlasMatrix<Element>& A, BlasMatrix<Element>& Ainv) const;
+		const BlasMatrix<Matrix>& inv(const BlasMatrix<Matrix>& A, BlasMatrix<Matrix>& Ainv) const;
 
 		// Rank
-		const unsigned int rank(const BlasMatrix<Element>& A) const;
+		const unsigned int rank(const BlasMatrix<Matrix>& A) const;
 
 		// in-place Rank (the matrix is modified)
-		const unsigned int rankin(BlasMatrix<Element>& A) const;
+		const unsigned int rankin(BlasMatrix<Matrix>& A) const;
 
 		// determinant
-		const Element& det(const BlasMatrix<Element>& A) const;
+		const Element& det(const BlasMatrix<Matrix>& A) const;
 
 		//in-place Determinant (the matrix is modified)
-		const Element& detin(BlasMatrix<Element>& A) const;
+		const Element& detin(BlasMatrix<Matrix>& A) const;
 		
-		// non-singular linear solve with matrix right hand side
-		bool solve (BlasMatrix<Element>& X, const BlasMatrix<Element>& A, const BlasMatrix<Element>& B) const;
+		// non-singular linear solve with matrix right hand side 
+		bool solve (BlasMatrix<Matrix>& X, const BlasMatrix<Matrix>& A, const BlasMatrix<Matrix>& B) const;
 
 		// non-singular linear solve with vector right hand side
-		bool solve (std::vector<Element>& x, const BlasMatrix<Element>& A, const std::vector<Element& b) const;
+		bool solve (std::vector<Matrix>& x, const BlasMatrix<Matrix>& A, const std::vector<Matrix& b) const;
+
+		// non-singular linear solve with matrix right hand side, the result is stored in-place in B
+		bool solve (const BlasMatrix<Matrix>& A, const BlasMatrix<Matrix>& B) const;
+
+		// non-singular linear solve with vector right hand side
+		bool solve (const BlasMatrix<Matrix>& A, const std::vector<Matrix& b) const;
 
 		// Apply a BlasPermutation matrix P to a dense matrix A: 
 		// B = A.P 
-		BlasMatrix<Element>& applyRight(  BlasMatrix<Element>& B, const BlasMatrix<Element>& A, const BlasPermutation& P);
+		BlasMatrix<Matrix>& applyRight(  BlasMatrix<Matrix>& B, const BlasMatrix<Matrix>& A, const BlasPermutation& P);
 
 		// B = A.P^t
-                BlasMatrix<Element>& applyRightTranspose(  BlasMatrix<Element>& B, const BlasMatrix<Element>& A, const BlasPermutation& P);
+                BlasMatrix<Matrix>& applyRightTranspose(  BlasMatrix<Matrix>& B, const BlasMatrix<Matrix>& A, const BlasPermutation& P);
 
 		// B = P.A 
-		BlasMatrix<Element>& applyLeft(  BlasMatrix<Element>& B, const BlasMatrix<Element>& A, const BlasPermutation& P);
+		BlasMatrix<Matrix>& applyLeft(  BlasMatrix<Matrix>& B, const BlasMatrix<Matrix>& A, const BlasPermutation& P);
 		
 		// B = A.P^t
-                BlasMatrix<Element>& applyLeftTranspose(  BlasMatrix<Element>& B, const BlasMatrix<Element>& A, const BlasPermutation& P);
+                BlasMatrix<Matrix>& applyLeftTranspose(  BlasMatrix<Matrix>& B, const BlasMatrix<Matrix>& A, const BlasPermutation& P);
 		
 		// In place apply.
 		// A = A.P 
-		BlasMatrix<Element>& applyinRight( BlasMatrix<Element>& A, const BlasPermutation& P);
+		BlasMatrix<Matrix>& applyinRight( BlasMatrix<Matrix>& A, const BlasPermutation& P);
 		
 		// A = A.P^t
-                BlasMatrix<Element>& applyinRightTranspose( BlasMatrix<Element>& A, const BlasPermutation& P);       
+                BlasMatrix<Matrix>& applyinRightTranspose( BlasMatrix<Matrix>& A, const BlasPermutation& P);       
 
 		// A = P.A 
-		BlasMatrix<Element>& applyinLeft( BlasMatrix<Element>& A, const BlasPermutation& P);
+		BlasMatrix<Matrix>& applyinLeft( BlasMatrix<Matrix>& A, const BlasPermutation& P);
 		
 		// A = A.P^t
-                BlasMatrix<Element>& applyinLeftTranspose( BlasMatrix<Element>& A, const BlasPermutation& P);
+                BlasMatrix<Matrix>& applyinLeftTranspose( BlasMatrix<Matrix>& A, const BlasPermutation& P);
 
 		// Conversion from BlasPermutation to BlackBoxPermutation 
 		Permutation& convert ( Permutation& P, const BlasPermutation& BP );
