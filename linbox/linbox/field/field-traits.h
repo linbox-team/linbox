c6984f5a4936c4ee2f8c37ed22484908d8471247
roche <roche@8563fe0b-bd16-0410-b45f-99c24e105077>
2004-06-09 13:20:55 +0000

    2004-June-9 Dan Roche 	* Actually added field/PIR-modular-int32.h and field/field-traits.h

---
+++ linbox/linbox/field/field-traits.h
@@ -0,0 +1,59 @@
+/* -*- mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+
+/* linbox/field/field-traits.h
+ * Copyright (C) June 2004 Dan Roche
+ * see COPYING for permissions etc.
+ */
+
+#ifndef __FIELD_TRAITS_H
+#define __FIELD_TRAITS_H
+
+// Namespace in which all LinBox library code resides
+namespace LinBox {
+
+/* FieldTraits will hold various methods for each field that do not need to be put
+ * in the field definition itself.
+ * maxModulus returns the greatest modulus that is usable for a given field, -1
+ * for infinite.
+ * goodModulus returns takes an integer and returns true if that integer is a valid
+ * modulus for the given field.
+ */
+
+template <class Field>
+struct FieldTraits
+{
+	static integer& maxModulus( integer& );
+	static bool goodModulus( const integer& i ) {
+		integer max;
+		return ( i >= 2 && i <= maxModulus(max) );
+	}
+};
+
+// Specializations for FieldTraits
+
+template< class E > class Modular;
+template< class E > class PIRModular;
+class NTL_zz_p;
+
+integer& FieldTraits< Modular< int32 > >::maxModulus( integer& i )
+	{ return i = 1073741789; }
+
+integer& FieldTraits< Modular< int16 > >::maxModulus( integer& i )
+	{ return i = 32749; }
+
+integer& FieldTraits< Modular< int8 > >::maxModulus( integer& i )
+	{ return i = 127; }
+
+integer& FieldTraits< Modular< double > >::maxModulus( integer& i )
+	{ return i = 94906249; }
+
+integer& FieldTraits< PIRModular<int32> >::maxModulus( integer& i )
+	{ return FieldTraits< Modular<int32> >::maxModulus( i ); }
+
+integer& FieldTraits< NTL_zz_p >::maxModulus( integer& i )
+	{ return i = NTL_SP_BOUND; }
+
+
+} // namespace Linbox
+
+#endif // __FIELD_TRAITS_H
