77579cca5ffaa9190d42ecbc23b1f360e5db1e6c
wan <wan@8563fe0b-bd16-0410-b45f-99c24e105077>
2004-06-17 20:06:52 +0000

    linbox/solutions/lu.h, tests/test-lu.C, fix the block lu and make test work.

--- trunk/linbox/linbox/solutions/lu.h
+++ trunk/linbox/linbox/solutions/lu.h
@@ -10,149 +10,189 @@
 
 #include <iostream>
 #include <linbox/blackbox/dense.h>
-#include <linbox/matrix/dense-submatrix.h>
+#include <linbox/blackbox/submatrix.h>
 
 namespace LinBox
 {
-  /**
-   * M <-- the LU decomposition of M.
-   *
-   *@param M is a dense matrix on input.  
-   * Every leading principal minor must be nonzero.
-   * M is modified to represent
-   * the LU decomposition of the original M.
-   * L is unit lower triangular and occupies the strictly lower triangular
-   * part of M.  The diagonal one's are implicit.
-   * U is upper triangular and occupies the rest of M.
-   */
-  template<class Field>
-    void LU(DenseMatrix<Field>& M);
+	/**
+	 * M <-- the LU decomposition of M.
+	 *
+	 *@param M is a dense matrix on input.  
+	 * require all principle minor are non-singular.
+	 * Every leading principal minor must be nonzero.
+	 * M is modified to represent
+	 * the LU decomposition of the original M.
+	 * L is unit lower triangular and occupies the strictly lower triangular
+	 * part of M.  The diagonal one's are implicit.
+	 * U is upper triangular and occupies the rest of M.
+	 */
+	template<class Field>
+	void LU(DenseMatrix<Field>& M);
   
-  // M <-- LU decomp of M (just as for DenseMatrix argument).
-  template<class Field>
-    void LU(DenseSubmatrix<typename Field::Element>& M);
+	// M <-- LU decomp of M (just as for DenseMatrix argument).
+	template<class Field>
+	void LU(Submatrix<DenseMatrix<Field> >& M);
   
-  // M <-- M L^{-1}, where L is unit lower triangular with implicit diagonal.
-  template<class Field>
-    void LL_MULIN(DenseSubmatrix<typename Field::Element>& M, const DenseSubmatrix<typename Field::Element>& L);
+	// M <-- M L^{-1}, where L is unit lower triangular with implicit diagonal.
+	template<class Matrix>
+	void LL_MULIN(Matrix& M, const Matrix& L);
   
-  // R <-- RU, where U is upper triangular.
-  template<class Field>
-    void RU_MULIN(DenseSubmatrix<typename Field::Element>& R, const DenseSubmatrix<typename Field::Element>& U);
+	// R <-- RU, where U is upper triangular.
+	template<class Matrix>
+	void RU_MULIN(Matrix& R, const Matrix& U);
   
-  template<class Field>
-    void  AXMYIN(DenseSubmatrix<typename Field::Element>&, const DenseSubmatrix<typename Field::Element>&, const DenseSubmatrix<typename Field::Element>&);
+	template<class Matrix>
+	void  AXMYIN(Matrix&, const Matrix&, const Matrix&);
   
-  template<class Field>
-    void LU(DenseMatrix<Field>& M) 
-    {
-      linbox_check(M.rowdim()==M.coldim());
-      if(M.rowdim()<=1)
-	return;
-      LU(DenseSubmatrix<typename Field::Element>(M,0,M.rowdim()-1,0,M.coldim()-1));      
-    }
-  
-  template<class Field>
-    void LU(DenseSubmatrix<typename Field::Element> M) 
-    {
-      int dim=M.rowdim();
-      if(dim<=1)
-	return;
-      --dim;
-      int HALF=dim/2;
-      DenseSubmatrix<typename Field::Element> M00(M,0,HALF,0,HALF);
-      DenseSubmatrix<typename Field::Element> M01(M,0,HALF,HALF+1,dim);
-      DenseSubmatrix<typename Field::Element> M10(M,HALF+1,dim,0,HALF);
-      DenseSubmatrix<typename Field::Element> M11(M,HALF+1,dim,HALF+1,dim);
-      LU(M00);
-      LL_MULIN(M01,M00);
-      RU_MULIN(M10,M00);
-      AXMYIN(M11,M10,M01);
-      LU(M11);     
-    }
-
-  /*
-   *@param L is a lower triangle matrix.
-   * All diagonal entries in L are one.
-   * $ M <- L^{-1} M$
-   */
-  template<class Field>
-    void LL_MULIN(DenseSubmatrix<typename Field::Element>& M, const DenseSubmatrix<typename Field::Element>& L) 
-    {
-      typedef DenseSubmatrix<typename Field::Element> Matrix;
-      typename Matrix::ColIterator colp;
-      typename Matrix::ColIterator ep, epo;
-
-      typename Matrix::ConstRowIterator crowp;
-      typename Matrix::ConstRowIterator cep;
-      
-      typename Matrix::Element e;
-
-      for(colp=M.rowOfColsBegin();colp!=M.rowOfColsEnd();++colp)
+	template<class Field>
+	void LU(DenseMatrix<Field>& M) 
 	{
-	  crowp=L.colOfRowsBegin();
-	  for(ep=colp->begin();ep!=colp->end();++ep,++crowp)
-	    for(cep= crowp->begin(),epo=colp->begin();epo!=ep;++cep,++epo)
-	      {
-		M.field().mul(e,*epo,*cep);
-		M.field().subin(*ep,e);
-	      }
+		linbox_check (M.rowdim() == M.coldim());
+
+		if (M. rowdim() <= 1)
+
+			return;
+
+		Submatrix<DenseMatrix<Field> > sub (M, 0, 0, M. rowdim(), M. coldim());      
+	
+		LU (sub);
+
 	}
-    }
   
-  /*
-   *param U is an upper triangle matrix.
-   *$M <- M U^{-1}$
-   */
-  template<class Field>
-    void RU_MULIN(DenseSubmatrix<typename Field::Element>& M, const DenseSubmatrix<typename Field::Element>& U)
-    {
-      typedef DenseSubmatrix<typename Field::Element> Matrix;
-      typename Matrix::ColOfRowsIterator rowp;
-      typename Matrix::ConstRowOfColsIterator ccolp;
-      typename Matrix::Element e;
-      typename Matrix::RowIterator ep,epo;
-      typename Matrix::ConstColIterator cep;
-      
-      for(rowp=M.colOfRowsBegin();rowp!=M.colOfRowsEnd();++rowp)
-	{
-	  ccolp=U.rowOfColsBegin();
-	  for(ep=rowp->begin();ep!=rowp->end();++ep,++ccolp)
-	    {
-	      for(cep= ccolp->begin(),epo=rowp->begin();epo!=ep;++cep,++epo)
-		{
-		  M.field().mul(e,*epo,*cep);
-		  M.field().subin(*ep,e);
+	template<class Field>
+	void LU(Submatrix<DenseMatrix<Field> >& M) {
+
+		int dim = M. rowdim ();
+
+		if ( dim <= 1)
+
+			return;
+
+		int HALF = dim / 2;
+
+		int rest = dim - HALF;
+
+		Submatrix<DenseMatrix<Field> > M00 (M, 0, 0, HALF, HALF);
+
+		Submatrix<DenseMatrix<Field> > M01 (M, 0, HALF, HALF, rest);
+
+		Submatrix<DenseMatrix<Field> > M10 (M, HALF, 0, rest, HALF);
+
+		Submatrix<DenseMatrix<Field> > M11 (M, HALF, HALF, rest, rest);
+
+		LU (M00);
+
+		LL_MULIN (M01,M00);
+
+		RU_MULIN (M10,M00);
+
+		AXMYIN (M11,M10,M01);
+
+		LU (M11);     
+
+	}
+
+	/*
+	 *@param L is a unit lower triangle matrix.
+	 * All diagonal entries in L are one.
+	 * $ M <- L^{-1} M$
+	 */
+	template<class Matrix>
+	void LL_MULIN(Matrix& M, const Matrix& L) {
+
+		typename Matrix::ColIterator colp;
+
+		typename Matrix::ConstRowIterator crowp;
+
+		typename Matrix::Col::iterator ep, epo;
+
+		typename Matrix::ConstRow::const_iterator cep;
+
+		typename Matrix::Element e;
+
+		for(colp = M. colBegin();colp != M. colEnd(); ++colp) {
+
+			crowp = L. rowBegin();
+
+			for(ep = colp -> begin(); ep != colp -> end(); ++ ep, ++ crowp)
+
+				for(cep = crowp -> begin(), epo = colp -> begin(); epo !=ep; ++ cep, ++ epo) {
+
+					M.field().mul(e,*epo,*cep);
+
+					M.field().subin(*ep,e);
+				}
 		}
-	      M.field().divin(*ep,*cep);
-	    }
 	}
-    }
+  
+	/*
+	 *param U is an upper triangle matrix.
+	 *$M <- M U^{-1}$
+	 */
+	template<class Matrix>
+	void RU_MULIN (Matrix& M, const Matrix& U) {
 
-  /*
-   *@M1, M2, dense matrix.
-   *M<-M-M1*M2
-   */
-  template<class Field>
-    void  AXMYIN(DenseSubmatrix<typename Field::Element>& M, const DenseSubmatrix<typename Field::Element>& M1, const DenseSubmatrix<typename Field::Element>& M2)
-    {
-      typedef DenseSubmatrix<typename Field::Element> Matrix;
-      typename Matrix::RowOfColsIterator colp;
-      typename Matrix::ConstColOfRowsIterator crowp1;
-      typename Matrix::ConstRowOfColsIterator ccolp2; 
-      typename Matrix::Element e;
-      typename Matrix::ColIterator ep;
-      typename Matrix::ConstRowIterator cep1;
-      typename Matrix::ConstColIterator cep2;
-      for(colp=M.rowOfColsBegin(),ccolp2=M2.rowOfColsBegin();colp!=M.rowOfColsEnd();++colp,++ccolp2)
-	for(ep=colp->begin(),crowp1=M1.colOfRowsBegin();ep!=colp->end();++ep,++crowp1)
-	  {
-	    M.field().init(e,0);
-	    for(cep1=crowp1->begin(),cep2=ccolp2->begin();cep1!=crowp1->end();++cep1,++cep2)
-	      M.field().axpyin(e,*cep1,*cep2);
-	    M.field().subin(*ep,e);
-	  }
-    }
+		typename Matrix::RowIterator rowp;
+
+		typename Matrix::ConstColIterator ccolp;
+
+		typename Matrix::Element e;
+
+		typename Matrix::Row::iterator ep,epo;
+
+		typename Matrix::ConstCol::const_iterator cep;
+      
+		for (rowp = M. rowBegin(); rowp != M. rowEnd(); ++ rowp) {
+
+			ccolp = U. colBegin();
+
+			for (ep = rowp -> begin (); ep != rowp -> end(); ++ ep, ++ ccolp) {
+
+				for ( cep= ccolp -> begin (), epo = rowp -> begin (); epo != ep; ++ cep,++ epo) {
+
+					M. field(). mul (e, *epo, *cep);
+
+					M. field(). subin (*ep, e);
+				}
+
+				M. field(). divin (*ep, *cep);
+
+			}
+		}
+	}
+
+	/*
+	 *@M1, M2, dense matrix.
+	 *M<-M-M1*M2
+	 */
+	template<class Matrix>
+	void  AXMYIN(Matrix& M, const Matrix& M1, const Matrix& M2) {
+
+		typename Matrix::ColIterator colp;
+
+		typename Matrix::ConstRowIterator crowp1;
+
+		typename Matrix::ConstColIterator ccolp2; 
+
+		typename Matrix::Element e;
+
+		typename Matrix::Col::iterator ep;
+
+		typename Matrix::ConstRow::const_iterator cep1;
+
+		typename Matrix::ConstCol::const_iterator cep2;
+
+		for (colp = M. colBegin(), ccolp2 = M2. colBegin(); colp != M. colEnd(); ++colp, ++ccolp2)
+
+			for (ep = colp -> begin(), crowp1 = M1. rowBegin(); ep != colp->end(); ++ep, ++crowp1) {
+				M. field(). init(e,0);
+
+				for(cep1 = crowp1 -> begin(), cep2 = ccolp2 -> begin(); cep1 != crowp1 -> end(); ++cep1, ++cep2)
+					M. field(). axpyin (e, *cep1, *cep2);
+	
+				M. field(). subin (*ep, e);
+			}
+	}
 }
 
 #endif
--- trunk/linbox/tests/test-lu.C
+++ trunk/linbox/tests/test-lu.C
@@ -2,101 +2,139 @@
 #include <fstream>
 #include <cstdlib>
 #include <time.h>
-#include <linbox/field/ntl-zz_p.h>
+#include <linbox/field/modular-int.h>
 #include <linbox/solutions/lu.h>
 #include <linbox/blackbox/dense.h>
 #include <linbox/util/commentator.h>
 #include <vector>
 #include <iomanip>
 
-typedef LinBox::UnparametricField<NTL::zz_p> Field;
+typedef LinBox::Modular<int> Field;
 
 namespace LinBox
 {
-  /*- test if M==L*U;
-   * where L is lower triangular matrix, whose diagonal entries are 1.
-   * U is an upper triangular matrix.
-   */
-  template<class Field>
-    bool LU_MUL_TEST(const DenseMatrix<Field>& M, const DenseMatrix<Field>& L,const DenseMatrix<Field>& U)
-    {
-      linbox_check((M.rowdim()==L.rowdim())&&(M.coldim()==U.coldim())&&
-                   (L.rowdim()==L.coldim())&&(U.rowdim()==U.coldim())&&
-                   (L.coldim()==U.rowdim()));
+	/*- test if M==L*U;
+	 * where L is lower triangular matrix, whose diagonal entries are 1.
+	 * U is an upper triangular matrix.
+	 */
+	template<class Field>
+	bool LU_MUL_TEST(const DenseMatrix<Field>& M, const DenseMatrix<Field>& L,const DenseMatrix<Field>& U) {
+
+		linbox_check( (M.rowdim() == L. rowdim()) && (M. coldim() == U. coldim())&&
+			      (L.rowdim() == L. coldim()) && (U. rowdim() == U. coldim())&&
+			      (L.coldim() == U. rowdim()));
       
-      typename DenseMatrix<Field>::ConstRowOfColsIterator ccolp;
-      typename DenseMatrix<Field>::Element e;
-      typename DenseMatrix<Field>::ConstColOfRowsIterator crowp1;   
-      typename DenseMatrix<Field>::ConstRowOfColsIterator ccolp2;       
-      typename DenseMatrix<Field>::ConstColIterator cep, cepo;
-      typename DenseMatrix<Field>::ConstRowIterator cep1,cep1e;
-      typename DenseMatrix<Field>::ConstColIterator cep2;
-      int i, j;
-      i=0;
-      for(int ccolp=M.rowOfColsBegin(),ccolp2=U.rowOfColsBegin();
-          ccolp!=M.rowOfColsEnd();
-          ++ccolp,++ccolp2,++i)
-        {
-          j=0;
-          for(cep=ccolp->begin(),crowp1=L.colOfRowsBegin();
-              cep!=ccolp->end();
-              ++cep,++crowp1,++j)
-            {
-              if(j<=i)
-                cep1e=crowp1->begin()+j;
-              else
-                cep1e=crowp1->begin()+i+1;
+		typename DenseMatrix<Field>::ConstColIterator ccolp;
+
+		typename DenseMatrix<Field>::Element e;
+
+		typename DenseMatrix<Field>::ConstRowIterator crowp1;   
+
+		typename DenseMatrix<Field>::ConstColIterator ccolp2;       
+
+		typename DenseMatrix<Field>::ConstCol::const_iterator cep;
+
+		typename DenseMatrix<Field>::ConstRow::const_iterator cep1,cep1e;
+
+		typename DenseMatrix<Field>::ConstCol::const_iterator cep2;
+
+		int i, j;
+
+		i=0;
+
+		for (ccolp = M. colBegin(), ccolp2 = U. colBegin();
+		     ccolp != M. colEnd();
+		     ++ ccolp, ++ ccolp2, ++ i) {
+
+			j=0;
+
+			for (cep = ccolp -> begin(), crowp1 = L. rowBegin();
+			     cep != ccolp -> end();
+			     ++ cep, ++ crowp1, ++j) {
+
+				if ( j <= i)
+
+					cep1e = crowp1 -> begin() + j;
+
+				else
+
+					cep1e = crowp1 -> begin() + (i + 1);
               
-              M.field().init(e,0);
-              for(cep1=crowp1->begin(),cep2=ccolp2->begin();
-                  cep1!=cep1e;
-                  ++cep1,++cep2)
-                M.field().axpyin(e,*cep1,*cep2);
+				M. field(). init (e, 0);
+
+				for (cep1 = crowp1 -> begin(), cep2 = ccolp2 -> begin();
+				     cep1 != cep1e;
+				     ++ cep1, ++ cep2)
+
+					M. field(). axpyin (e, *cep1, *cep2);
               
-              if(j<=i)
-                M.field().addin(e ,*cep2);
-              if(!M.field().areEqual(e,*cep))
-                return false;
-            }     
-        }
-      return true;
-    }
+				if (j <= i)
+
+					M. field(). addin (e, *cep2);
+
+				if (! M. field(). areEqual (e, *cep))
+
+					return false;
+			}     
+		}
+		return true;
+	}
 }
 
-bool test(int _SIZE)
-{
-  typedef Unparametrix<NTL::zz_p> Field;
-  Field field;
-  NTL::zz_p::init(1073741789);
-  typedef LinBox::DenseMatrix<Field>  Matrix;
-  Matrix M(field,_SIZE,_SIZE);
-  for(int i=0;i<M.rowdim();++i)
-      M.setEntry(i,i,NTL::to_zz_p(rand()%1000000+1000000));
+bool test (int _SIZE) {
 
+	Field field (65521);
 
-  for(int i=0;i<M.rowdim();++i)
-    for(int j=0;j<M.coldim();++j)
-      M.setEntry(i,j,NTL::to_zz_p(rand()%1000000+1));
+	Field::Element e;
+
+	typedef LinBox::DenseMatrix<Field>  Matrix;
+
+	Matrix M (field, _SIZE, _SIZE);
+
+	for (int i = 0; i < (int)M. rowdim(); ++i) {
+
+		field. init (e, random());
+
+		M. setEntry (i, i, e);
+	}
+
+	for (int i = 0; i < (int)M. rowdim(); ++i)
+
+		for (int j = 0; j < (int)M. coldim(); ++j) {
+
+			field. init (e,  random());
+
+			M. setEntry (i, j, e); 
+		}
  
-  typename LinBox::DenseMatrix<Field> M_C(M);
-  LinBox::LU(M);
-  return LinBox::LU_MUL_TEST(M_C,M,M);
+	LinBox::DenseMatrix<Field> M_C(M);
+
+	LinBox::LU (M);
+
+	return LinBox::LU_MUL_TEST(M_C,M,M);
 }
   
-int main()
-{
-  srand(time(0));
-  int iteration=120;
-  LinBox::commentator.start("Test LU over random matrix in ntl-zz_p field","",iteration-10);
-  for(int i=10;i<iteration;++i)
-    {
-      LinBox::commentator.progress();
-      if(!test(i))
-	{
-	  LinBox::commentator.stop("Failed","");
-	  return -1;
+int main() {
+
+	srand(time(0));
+
+	int iteration = 120;
+
+	LinBox::commentator.start("Test LU over random matrix in modular int field","",iteration-10);
+
+	for(int i = 10;i < iteration; ++i) {
+
+		LinBox::commentator.progress();
+
+		if (!test (i)) {
+
+			LinBox::commentator.stop("Failed","");
+
+			return -1;
+		}
 	}
-    }
-  LinBox::commentator.stop("passed","");
-  return 0;
+
+	LinBox::commentator.stop("passed","");
+
+	return 0;
 }
