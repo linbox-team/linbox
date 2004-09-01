9396e7b3810aff7a2f243103c17a551ecd084856
wan <wan@8563fe0b-bd16-0410-b45f-99c24e105077>
2004-09-01 19:56:30 +0000

    linbox/algorithms/rational-solver-adaptive.h, adaptive solver

--- trunk/linbox/linbox/algorithms/smith-form-adaptive.h
+++ trunk/linbox/linbox/algorithms/smith-form-adaptive.h
@@ -10,37 +10,40 @@
 
 namespace LinBox {
 
-	long prime[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+class SmithFormAdaptive {
+	public:
 
-	const int NPrime = 25;
+	static const long prime[];// = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+
+	static const int NPrime;// = 25;
 
 	/* Compute the local smith form at prime p, when modular (p^e) fits in long
 	*/
 	template <class Ring>
-	void compute_local_long (std::vector<integer>& s, const DenseMatrix<Ring>& A, long p, long e);
+	static void compute_local_long (std::vector<integer>& s, const DenseMatrix<Ring>& A, long p, long e);
 
 	/* Compute the local smith form at prime p, when modular (p^e) doesnot fit in long
 	*/
 	template <class Ring>
-	void compute_local_big (std::vector<integer>& s, const DenseMatrix<Ring>& A, long p, long e);
+	static void compute_local_big (std::vector<integer>& s, const DenseMatrix<Ring>& A, long p, long e);
 
 	/* Compute the local smith form at prime p
 	*/
 	template <class Ring>
-	void compute_local (std::vector<integer>& s, const DenseMatrix<Ring>& A, long p, long e);
+	static void compute_local (std::vector<integer>& s, const DenseMatrix<Ring>& A, long p, long e);
 
 	/* Compute the k-smooth part of the invariant factor, where k = 100.
 	 * @param sev is the exponent part ...
 	 * By local smith form and rank computation
 	 */
 	template <class Ring>
-	void SmithFormSmooth (std::vector<integer>& s, const DenseMatrix<Ring>& A, long r, const std::vector<long>& sev);
+	static void smithFormSmooth (std::vector<integer>& s, const DenseMatrix<Ring>& A, long r, const std::vector<long>& sev);
 			
 	/* Compute the k-rough part of the invariant factor, where k = 100.
 	 * By EGV+ algorithm or Iliopoulos' algorithm for Smith form.
 	*/
 	template <class Ring>
-	void SmithFormRough  (std::vector<integer>& s, const DenseMatrix<Ring>& A, integer m );
+	static void smithFormRough  (std::vector<integer>& s, const DenseMatrix<Ring>& A, integer m );
 
 	/** \brief Smith form of a dense matrix by adaptive algorithm.
 	 *
@@ -48,7 +51,10 @@ namespace LinBox {
 	 * compute the rough and smooth part, separately.
 	 */
 	template <class Ring>
-	void SmithForm (std::vector<integer>& s, const DenseMatrix<Ring>& A);
+	static void smithForm (std::vector<integer>& s, const DenseMatrix<Ring>& A);
+};
+	const long SmithFormAdaptive::prime[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+	const int SmithFormAdaptive::NPrime = 25;
 }
 
 #include <linbox/algorithms/smith-form-adaptive.inl>
