59d60be279b2bf060938c8ee9c085fb17757afde
saunders <bdsaunders@users.noreply.github.com>
2005-03-15 21:05:48 +0000

    2005-03-15 bds     *tests/test-rank.C linbox/solutions/rank.h methods.h Introduce rank 	by blas-elimination, together with check for too small prime in wiedemann rank.

--- linbox/tests/test-rank.C
+++ linbox/tests/test-rank.C
@@ -23,162 +23,14 @@
 #include "linbox/blackbox/diagonal.h"
 #include "linbox/blackbox/sparse.h"
 #include "linbox/blackbox/scalar-matrix.h"
+#include "linbox/blackbox/direct-sum.h"
 #include "linbox/solutions/rank.h"
 
 #include "test-common.h"
 
 using namespace LinBox;
 
-/* Test 1: Rank of diagonal matrix with n/2 distinct nonzero entries
- *
- * Construct a random diagonal matrix with n/2 distinct nonzero entries and see
- * that its computed determinant equals the product of diagonal entries
- *
- * F - Field over which to perform computations
- * n - Dimension to which to make matrix
- * iterations - Number of iterations to run
- *
- * Return true on success and false on failure
- */
-
-template <class Field>
-static bool testDiagonalRank1 (Field &F, size_t n, int iterations) 
-{
-	typedef vector <typename Field::Element> Vector;
-	typedef vector <typename Field::Element> Polynomial;
-	typedef vector <pair <size_t, typename Field::Element> > Row;
-	typedef Diagonal <Field> Blackbox;
-
-	commentator.start ("Testing diagonal rank (1)", "testDiagonalRank1", iterations);
-
-	bool ret = true;
-	bool done;
-	int i;
-	size_t j, k;
-
-	Vector d(n);
-	unsigned long _rank;
-	typename Field::RandIter r (F);
-
-	for (i = 0; i < iterations; i++) {
-		commentator.startIteration (i);
-
-		for (j = 0; j < n / 2; j++) {
-			do {
-				do r.random (d[j]); while (F.isZero (d[j]));
-
-				done = true;
-				for (k = 0; k < j; k++) {
-					if (F.areEqual (d[j], d[k])) {
-						done = false;
-						break;
-					}
-				}
-			} while (!done);
-		}
-
-		for (j = n / 2; j < n; j++)
-			F.init (d[j], 1);
-
-// 		ostream &report = commentator.report (Commentator::LEVEL_UNIMPORTANT, INTERNAL_DESCRIPTION);
-		ostream &report = commentator.report (Commentator::LEVEL_IMPORTANT, INTERNAL_DESCRIPTION);
-		report << "Diagonal entries: ";
-		printVector<Field> (F, report, d);
-
-		report << "True rank: " << n / 2 << endl;
-
-		Blackbox D (F, d);
-
-		rank (_rank, D, F, MethodTrait::Wiedemann ());
-
-		report << "Computed rank: " << _rank << endl;
-
-		if (_rank != n) {
-			ret = false;
-			commentator.report (Commentator::LEVEL_IMPORTANT, INTERNAL_ERROR)
-				<< "ERROR: Computed rank is incorrect" << endl;
-		}
-
-		commentator.stop ("done");
-		commentator.progress ();
-	}
-
-	commentator.stop (MSG_STATUS (ret), (const char *) 0, "testDiagonalRank1");
-
-	return ret;
-}
-
-/* Test 2: Rank of diagonal matrix with n / 2 nondistinct nonzero entries
- *
- * Construct a random diagonal matrix with n / 2 nonzero, nondistinct entries
- * and see that its computed rank equals n / 2
- *
- * F - Field over which to perform computations
- * n - Dimension to which to make matrix
- * iterations - Number of iterations to run
- *
- * Return true on success and false on failure
- */
-
-template <class Field>
-static bool testDiagonalRank2 (Field &F, size_t n, int iterations) 
-{
-	typedef vector <typename Field::Element> Vector;
-	typedef vector <typename Field::Element> Polynomial;
-	typedef vector <pair <size_t, typename Field::Element> > Row;
-	typedef Diagonal <Field> Blackbox;
-
-	commentator.start ("Testing diagonal rank (2)", "testDiagonalRank2", iterations);
-
-	bool ret = true;
-	int i;
-	size_t j, k;
-
-	Vector d(n);
-	unsigned long _rank;
-	typename Field::RandIter r (F);
-
-	for (i = 0; i < iterations; i++) {
-		commentator.startIteration (i);
-
-		for (j = 0; j < n / 4; j++)
-			do r.random (d[j]); while (F.isZero (d[j]));
-
-		for (j = n / 4; j < n / 2; j++) {
-			k = rand () % (n / 4);
-			d[j] = d[k];
-		}
-
-		for (j = n / 2; j < n; j++)
-			F.init (d[j], 0);
-
-		ostream &report = commentator.report (Commentator::LEVEL_UNIMPORTANT, INTERNAL_DESCRIPTION);
-		report << "Diagonal entries: ";
-		printVector<Field> (F, report, d);
-
-		report << "True rank: " << n / 2 << endl;
-
-		Blackbox D (F, d);
-
-		rank (_rank, D, F, MethodTrait::Wiedemann ());
-
-		report << "Computed rank: " << _rank << endl;
-
-		if (_rank != n / 2) {
-			ret = false;
-			commentator.report (Commentator::LEVEL_IMPORTANT, INTERNAL_ERROR)
-				<< "ERROR: Computed rank is incorrect" << endl;
-		}
-
-		commentator.stop ("done");
-		commentator.progress ();
-	}
-
-	commentator.stop (MSG_STATUS (ret), (const char *) 0, "testDiagonalRank2");
-
-	return ret;
-}
-
+// tests 1 and 2 were certain diagonals - now deemed unnecessary.  -bds 2005Mar15 (witnesses: ZW and PG)
 /* Test 3: Rank of a random sparse matrix
  *
  * Constructs a random sparse matrix and computes its rank using both Gaussian
@@ -195,24 +47,28 @@ bool testEliminationRank (const Field &F, size_t n, unsigned int iterations)
 	bool ret = true;
 	unsigned int i;
 
-	unsigned long rank_Wiedemann, rank_elimination;
+	unsigned long rank_Wiedemann, rank_elimination, rank_blas_elimination;
 
 	typename Field::RandIter ri (F);
 
 	for (i = 0; i < iterations; ++i) {
 		commentator.startIteration (i);
 
-		RandomSparseStream<Field, typename Vector<Field>::SparseSeq> stream (F, ri, 0.005, n, n);
+		RandomSparseStream<Field, typename Vector<Field>::SparseSeq> stream (F, ri, 0.05, n, n);
 		Blackbox A (F, stream);
 
-		rank (rank_Wiedemann, A, F, MethodTrait::Wiedemann ());
-		rank (rank_elimination, A, F, MethodTrait::Elimination (EliminationTraits::PIVOT_LINEAR));
+		A.write( commentator.report (Commentator::LEVEL_NORMAL, INTERNAL_DESCRIPTION)) << endl; 
+
+		rank (rank_Wiedemann, A, MethodTrait::Wiedemann ());
+		rank (rank_elimination, A, MethodTrait::Elimination (EliminationTraits::PIVOT_LINEAR));
+		rank (rank_blas_elimination, A, MethodTrait::BlasElimination ());
 
 		commentator.report (Commentator::LEVEL_NORMAL, INTERNAL_DESCRIPTION)
 			<< "Rank computed by Wiedemann: " << rank_Wiedemann << endl
-			<< "Rank computed by elimination: " << rank_elimination << endl;
+			<< "Rank computed by elimination: " << rank_elimination << endl
+			<< "Rank computed by blas_elimination: " << rank_blas_elimination << endl;
 
-		if (rank_Wiedemann != rank_elimination) {
+		if (rank_Wiedemann != rank_elimination || rank_elimination != rank_blas_elimination) {
 			commentator.report (Commentator::LEVEL_IMPORTANT, INTERNAL_ERROR)
 				<< "ERROR: Ranks are not equal" << endl;
 			ret = false;
@@ -236,7 +92,7 @@ bool testZeroAndIdentRank (const Field &F, size_t n, unsigned int iterations)
 {
 	typedef ScalarMatrix<Field> Blackbox;
 
-	commentator.start ("Testing rank of 0 and I", "testZeroAndIdentRank", iterations);
+	commentator.start ("Testing rank of zero and Identity and half/half matrices", "testZeroAndIdentRank", iterations);
 
 	bool ret = true;
 	unsigned int i;
@@ -247,26 +103,32 @@ bool testZeroAndIdentRank (const Field &F, size_t n, unsigned int iterations)
 		commentator.startIteration (i);
 
 		typename Field::Element zero, one;
+
 		F.init(zero, 0);
-		F.init(one, 1);
-
 		Blackbox A (F, n, zero);
-		Blackbox I (F, n, one);
-
-		rank (r, A, F, MethodTrait::Wiedemann ());
+		rank (r, A, MethodTrait::Wiedemann ());
 		if (r != 0) {
 			commentator.report (Commentator::LEVEL_IMPORTANT, INTERNAL_ERROR)
-				<< "ERROR: Rank of 0 is not 0 but " << r << endl;
+				<< "ERROR: Rank of 0 is not 0, but is " << r << endl;
 			ret = false;
 		}
 
-		rank (r, I, F, MethodTrait::Wiedemann ());
+		F.init(one, 1);
+		Blackbox I (F, n, one);
+		rank (r, I, MethodTrait::Wiedemann ());
 		if (r != n) {
 			commentator.report (Commentator::LEVEL_IMPORTANT, INTERNAL_ERROR)
-				<< "ERROR: Rank of I is not " << n << " but " << r << endl;
+				<< "ERROR: Rank of I is not " << n << ", but is " << r << endl;
 			ret = false;
 		}
 
+		DirectSum<Blackbox> B(A, I);
+		rank (r, B, MethodTrait::Wiedemann ());
+		if (r != n) {
+			commentator.report (Commentator::LEVEL_IMPORTANT, INTERNAL_ERROR)
+				<< "ERROR: Rank of I+0 is not " << n << ", but is " << r << endl;
+			ret = false;
+		}
 		commentator.stop ("done");
 		commentator.progress ();
 	}
@@ -284,9 +146,9 @@ int main (int argc, char **argv)
    
 	bool pass = true;
 
-	static size_t n = 256;
-	static integer q = 2147483647U;
-	static int iterations = 10;
+	static size_t n = 80;
+	static integer q = 10007;
+	static int iterations = 2;
 
 	static Argument args[] = {
 		{ 'n', "-n N", "Set dimension of test matrices to NxN (default 256)",       TYPE_INT,     &n },
@@ -295,7 +157,8 @@ int main (int argc, char **argv)
 	};
 
 	parseArguments (argc, argv, args);
-	Modular<uint32> F (q);
+	Modular<LinBox::uint32> F (q);
+	//Modular<int> F (q);
 
 	srand (time (NULL));
 
@@ -303,10 +166,11 @@ int main (int argc, char **argv)
 	commentator.getMessageClass (INTERNAL_DESCRIPTION).setMaxDepth (3);
 	commentator.getMessageClass (INTERNAL_DESCRIPTION).setMaxDetailLevel (Commentator::LEVEL_NORMAL);
 
-	if (!testDiagonalRank1 (F, n, iterations)) pass = false;
-	if (!testDiagonalRank2 (F, n, iterations)) pass = false;
 	if (!testEliminationRank (F, n, iterations)) pass = false;
 	if (!testZeroAndIdentRank (F, n, 1)) pass = false;
+	//Modular<int> G (3);
+	//if (!testEliminationRank (G, n, iterations)) pass = false;
+	//if (!testZeroAndIdentRank (G, n, 1)) pass = false;
 
 	return pass ? 0 : -1;
 }
