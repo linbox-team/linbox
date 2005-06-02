2a6c8c3d31105e0e0657c4c20d02b33a31dcaaba
Jean-Guillaume Dumas <jgdumas@users.noreply.github.com>
2005-06-02 09:48:06 +0000

    Changed to use mpz_abscmp

--- linbox/linbox/util/gmp++/gmp++_int_compare.C
+++ linbox/linbox/util/gmp++/gmp++_int_compare.C
@@ -13,15 +13,12 @@
 // returns 1 if a > b, 0 if a == b and -1 otherwise.  
 int compare(const Integer &a, const Integer& b) 
 {
-  if (&a == &b) return 0 ;
-  int cmp = mpz_cmp ( (mpz_ptr)&a.gmp_rep, (mpz_ptr)&b.gmp_rep );
-  return (cmp <0 ? -1 : (cmp >0 ? 1 : 0));
+  return mpz_cmp ( (mpz_ptr)&a.gmp_rep, (mpz_ptr)&b.gmp_rep );
 }
 
 int absCompare(const Integer &a, const Integer &b) 
 {
-  Integer c = ((a<0)?-a:a), d = ((b<0)?-b:b);
-  return mpz_cmp( (mpz_ptr)&(c.gmp_rep), (mpz_ptr)&(d.gmp_rep));
+  return mpz_cmpabs( (mpz_ptr)&(a.gmp_rep), (mpz_ptr)&(b.gmp_rep));
 }
 
 int Integer::operator != (const int l) const 
