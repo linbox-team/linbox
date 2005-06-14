01054c52596d68d0cd1a3c8aa4ad93119c82f01c
pernet <pernet@8563fe0b-bd16-0410-b45f-99c24e105077>
2005-06-14 17:00:02 +0000

    iszero -> isZero

--- trunk/linbox/linbox/util/gmp++/gmp++_int_add.C
+++ trunk/linbox/linbox/util/gmp++/gmp++_int_add.C
@@ -13,15 +13,15 @@
 //-------------------------------------------------- operator +
 Integer& Integer::addin(Integer& res, const Integer& n) 
 {
-  if (iszero(n)) return res;
-  if (iszero(res)) return res = n;
+  if (isZero(n)) return res;
+  if (isZero(res)) return res = n;
   mpz_add( (mpz_ptr)&res.gmp_rep, (mpz_ptr)&res.gmp_rep, (mpz_ptr)&n.gmp_rep );
   return res;
 }
 Integer& Integer::addin(Integer& res, const long n) 
 {
-  if (iszero(n)) return res;
-  if (iszero(res)) return res = n;
+  if (isZero(n)) return res;
+  if (isZero(res)) return res = n;
   int sgn = GMP__SGN(n); 
   if (sgn >0) mpz_add_ui( (mpz_ptr)&res.gmp_rep, (mpz_ptr)&res.gmp_rep, n);
   else mpz_sub_ui((mpz_ptr)&res.gmp_rep, (mpz_ptr)&res.gmp_rep, -n);
@@ -29,23 +29,23 @@ Integer& Integer::addin(Integer& res, const long n)
 }
 Integer& Integer::addin(Integer& res, const unsigned long n) 
 {
-  if (iszero(n)) return res;
-  if (iszero(res)) return res = n;
+  if (isZero(n)) return res;
+  if (isZero(res)) return res = n;
   mpz_add_ui( (mpz_ptr)&res.gmp_rep, (mpz_srcptr)&res.gmp_rep, n);
   return res;
 }
 
 Integer& Integer::add(Integer& res, const Integer& n1, const Integer& n2)
 {
-  if (iszero(n1)) return res = n2;
-  if (iszero(n2)) return res = n1;
+  if (isZero(n1)) return res = n2;
+  if (isZero(n2)) return res = n1;
   mpz_add( (mpz_ptr)&res.gmp_rep, (mpz_ptr)&n1.gmp_rep, (mpz_ptr)&n2.gmp_rep);
   return res;
 }
 Integer& Integer::add(Integer& res, const Integer& n1, const long n2)
 {
-  if (iszero(n1)) return res = n2;
-  if (iszero(n2)) return res = n1;
+  if (isZero(n1)) return res = n2;
+  if (isZero(n2)) return res = n1;
   int sgn = GMP__SGN(n2); 
   if (sgn >0) mpz_add_ui( (mpz_ptr)&res.gmp_rep, (mpz_ptr)&n1.gmp_rep, n2);
   else mpz_sub_ui((mpz_ptr)&res.gmp_rep, (mpz_ptr)&n1.gmp_rep, -n2);
@@ -53,8 +53,8 @@ Integer& Integer::add(Integer& res, const Integer& n1, const long n2)
 }
 Integer& Integer::add(Integer& res, const Integer& n1, const unsigned long n2)
 {
-  if (iszero(n1)) return res = n2;
-  if (iszero(n2)) return res = n1;
+  if (isZero(n1)) return res = n2;
+  if (isZero(n2)) return res = n1;
   mpz_add_ui( (mpz_ptr)&res.gmp_rep, (mpz_ptr)&n1.gmp_rep, n2);
   return res;
 }
@@ -62,8 +62,8 @@ Integer& Integer::add(Integer& res, const Integer& n1, const unsigned long n2)
 
 Integer& Integer::operator += (const Integer& n)
 {
-  if (iszero(n)) return *this;
-  if (iszero(*this)) return logcpy(n);
+  if (isZero(n)) return *this;
+  if (isZero(*this)) return logcpy(n);
   mpz_add( (mpz_ptr)&(gmp_rep), (mpz_ptr)&gmp_rep, (mpz_ptr)&n.gmp_rep) ;
   return *this;
 }
@@ -71,7 +71,7 @@ Integer& Integer::operator += (const Integer& n)
 Integer& Integer::operator += (const unsigned long l)
 {
   if (l==0) return *this;
-  if (iszero(*this)) return logcpy(Integer(l));
+  if (isZero(*this)) return logcpy(Integer(l));
   mpz_add_ui( (mpz_ptr)&(gmp_rep), (mpz_ptr)&gmp_rep, l);
   return *this;
 }
@@ -79,7 +79,7 @@ Integer& Integer::operator += (const unsigned long l)
 Integer& Integer::operator += (const long l)
 {
   if (l==0) return *this;
-  if (iszero(*this)) return logcpy(Integer(l));
+  if (isZero(*this)) return logcpy(Integer(l));
   int sgn = GMP__SGN(l);
   if (sgn >0) mpz_add_ui( (mpz_ptr)&(gmp_rep), (mpz_ptr)&gmp_rep, l);
   else mpz_sub_ui( (mpz_ptr)&(gmp_rep), (mpz_ptr)&gmp_rep, -l);
@@ -89,8 +89,8 @@ Integer& Integer::operator += (const long l)
 
 Integer Integer::operator + (const Integer& n) const
 {
-  if (iszero(n)) return *this;
-  if (iszero(*this)) return n;
+  if (isZero(n)) return *this;
+  if (isZero(*this)) return n;
   Integer res;   
   mpz_add( (mpz_ptr)&(res.gmp_rep), (mpz_ptr)&gmp_rep, (mpz_ptr)&n.gmp_rep) ;
   return res;
@@ -99,7 +99,7 @@ Integer Integer::operator + (const Integer& n) const
 Integer Integer::operator + (const unsigned long l) const 
 {
   if (l==0) return *this;
-  if (iszero(*this)) return Integer(l);
+  if (isZero(*this)) return Integer(l);
   Integer res;   
   mpz_add_ui( (mpz_ptr)&(res.gmp_rep), (mpz_ptr)&gmp_rep, l);
   return res;
@@ -108,7 +108,7 @@ Integer Integer::operator + (const unsigned long l) const
 Integer Integer::operator + (const long l) const 
 {
   if (l==0) return *this;
-  if (iszero(*this)) return Integer(l);
+  if (isZero(*this)) return Integer(l);
   Integer res;   
   int sgn = GMP__SGN(l);
   if (sgn >0) mpz_add_ui( (mpz_ptr)&(res.gmp_rep), (mpz_ptr)&gmp_rep, l);
--- trunk/linbox/linbox/util/gmp++/gmp++_int_sub.C
+++ trunk/linbox/linbox/util/gmp++/gmp++_int_sub.C
@@ -14,15 +14,15 @@
 //-------------------------------------------------- operator -
 Integer& Integer::subin(Integer& res, const Integer& n) 
 {
-  if (iszero(n)) return res;
-  if (iszero(res)) return res = - n;
+  if (isZero(n)) return res;
+  if (isZero(res)) return res = - n;
   mpz_sub( (mpz_ptr)&res.gmp_rep, (mpz_ptr)&res.gmp_rep, (mpz_ptr)&n.gmp_rep );
   return res;
 }
 Integer& Integer::subin(Integer& res, const long n) 
 {
-  if (iszero(n)) return res;
-  if (iszero(res)) return res = - n;
+  if (isZero(n)) return res;
+  if (isZero(res)) return res = - n;
   int sgn = GMP__SGN(n); 
   if (sgn >0) mpz_sub_ui( (mpz_ptr)&res.gmp_rep, (mpz_ptr)&res.gmp_rep, n);
   else mpz_add_ui((mpz_ptr)&res.gmp_rep, (mpz_ptr)&res.gmp_rep, -n);
@@ -30,23 +30,23 @@ Integer& Integer::subin(Integer& res, const long n)
 }
 Integer& Integer::subin(Integer& res, const unsigned long n) 
 {
-  if (iszero(n)) return res;
-  if (iszero(res)) return res = - n;
+  if (isZero(n)) return res;
+  if (isZero(res)) return res = - n;
   mpz_sub_ui( (mpz_ptr)&res.gmp_rep, (mpz_srcptr)&res.gmp_rep, n);
   return res;
 }
 
 Integer& Integer::sub(Integer& res, const Integer& n1, const Integer& n2)
 {
-  if (iszero(n1)) return res = - n2;
-  if (iszero(n2)) return res = n1;
+  if (isZero(n1)) return res = - n2;
+  if (isZero(n2)) return res = n1;
   mpz_sub( (mpz_ptr)&res.gmp_rep, (mpz_ptr)&n1.gmp_rep, (mpz_ptr)&n2.gmp_rep);
   return res;
 }
 Integer& Integer::sub(Integer& res, const Integer& n1, const long n2)
 {
-  if (iszero(n1)) return res = - n2;
-  if (iszero(n2)) return res = n1;
+  if (isZero(n1)) return res = - n2;
+  if (isZero(n2)) return res = n1;
   int sgn = GMP__SGN(n2); 
   if (sgn >0) mpz_sub_ui( (mpz_ptr)&res.gmp_rep, (mpz_ptr)&n1.gmp_rep, n2);
   else mpz_add_ui((mpz_ptr)&res.gmp_rep, (mpz_ptr)&n1.gmp_rep, -n2);
@@ -54,8 +54,8 @@ Integer& Integer::sub(Integer& res, const Integer& n1, const long n2)
 }
 Integer& Integer::sub(Integer& res, const Integer& n1, const unsigned long n2)
 {
-  if (iszero(n1)) return res = - n2;
-  if (iszero(n2)) return res = n1;
+  if (isZero(n1)) return res = - n2;
+  if (isZero(n2)) return res = n1;
   mpz_sub_ui( (mpz_ptr)&res.gmp_rep, (mpz_ptr)&n1.gmp_rep, n2);
   return res;
 }
@@ -75,8 +75,8 @@ Integer& Integer::negin(Integer& res)
 
 Integer& Integer::operator -= (const Integer& n)
 {
-  if (iszero(n)) return *this;
-  if (iszero(*this)) return logcpy(-n);
+  if (isZero(n)) return *this;
+  if (isZero(*this)) return logcpy(-n);
 //   Rep (res.gmp_rep)( MAX(SZ_REP(n.gmp_rep),SZ_REP(gmp_rep)) );  
   mpz_sub( (mpz_ptr)&(gmp_rep), (mpz_ptr)&gmp_rep, (mpz_ptr)&n.gmp_rep) ;
   return *this;
@@ -85,7 +85,7 @@ Integer& Integer::operator -= (const Integer& n)
 Integer& Integer::operator -= (const unsigned long l)
 {
   if (l==0) return *this;
-  if (iszero(*this)) return logcpy(Integer(-l));
+  if (isZero(*this)) return logcpy(Integer(-l));
 //   Rep (res.gmp_rep)( MAX(SZ_REP(gmp_rep),1) );   
   mpz_sub_ui( (mpz_ptr)&(gmp_rep), (mpz_ptr)&gmp_rep, l);
   return *this;
@@ -94,7 +94,7 @@ Integer& Integer::operator -= (const unsigned long l)
 Integer& Integer::operator -= (const long l)
 {
   if (l==0) return *this;
-  if (iszero(*this)) return logcpy(Integer(-l));
+  if (isZero(*this)) return logcpy(Integer(-l));
 //   Rep (res.gmp_rep)( MAX(SZ_REP(gmp_rep),1) );
   int sgn = GMP__SGN(l);
   if (sgn >0) mpz_sub_ui( (mpz_ptr)&(gmp_rep), (mpz_ptr)&gmp_rep, l);
@@ -105,8 +105,8 @@ Integer& Integer::operator -= (const long l)
 
 Integer Integer::operator - (const Integer& n) const
 {
-  if (iszero(n)) return *this;
-  if (iszero(*this)) return -n;
+  if (isZero(n)) return *this;
+  if (isZero(*this)) return -n;
 //   Rep (res.gmp_rep)( MAX(SZ_REP(n.gmp_rep),SZ_REP(gmp_rep)) );   
   Integer res;   
   mpz_sub( (mpz_ptr)&(res.gmp_rep), (mpz_ptr)&gmp_rep, (mpz_ptr)&n.gmp_rep) ;
@@ -116,7 +116,7 @@ Integer Integer::operator - (const Integer& n) const
 Integer Integer::operator - (const unsigned long l) const 
 {
   if (l==0) return *this;
-  if (iszero(*this)) return Integer(-l);
+  if (isZero(*this)) return Integer(-l);
 //   Rep (res.gmp_rep)( MAX(SZ_REP(gmp_rep),1) );   
   Integer res;   
   mpz_sub_ui( (mpz_ptr)&(res.gmp_rep), (mpz_ptr)&gmp_rep, l);
@@ -126,7 +126,7 @@ Integer Integer::operator - (const unsigned long l) const
 Integer Integer::operator - (const long l) const 
 {
   if (l==0) return *this;
-  if (iszero(*this)) return Integer(-l);
+  if (isZero(*this)) return Integer(-l);
 //   Rep (res.gmp_rep)( MAX(SZ_REP(gmp_rep),1) );   
   Integer res;   
   int sgn = GMP__SGN(l);
