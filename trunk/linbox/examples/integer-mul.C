d04a3372df821922bb8ad7b66d8f08fa1724a07c
saunders <saunders@8563fe0b-bd16-0410-b45f-99c24e105077>
2005-06-17 17:09:57 +0000

    moved file

---
+++ trunk/linbox/examples/integer-mul.C
@@ -0,0 +1,35 @@
+// ============================================================
+// (C) The Linbox Group 1999   Examples for using long integers
+// Fri Feb  8 14:00:35 MET 2002 Gilles Villard 
+// Wed Apr 17 15:41:35 MEST 2002
+// Sun Feb  9 23:15:15 MET 2003
+// ============================================================
+
+// ---------------------------------------------
+#include <iostream>
+#include <fstream> 
+// ---------------------------------------------
+
+#include "linbox-config.h"
+
+// Use of Gmp based LinBox integers 
+#include "linbox/integer.h"
+
+using namespace LinBox;
+using namespace std;
+ 
+// ---------------------------------------------
+
+int main() {
+
+  integer a,b;
+
+  cout << "1rst integer > ";
+  cin >> a;
+  cout << "2nd integer > ";
+  cin >> b;
+
+  cout << "The product " << a*b << "\n";
+
+  return 0;
+};
