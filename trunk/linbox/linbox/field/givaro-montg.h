65e3b5693c810f42e63a717ee62bd9b1de5dac6d
turner <turner@8563fe0b-bd16-0410-b45f-99c24e105077>
2005-06-24 22:44:16 +0000

    Removed using declarations.

--- trunk/linbox/linbox/field/givaro-montg.h
+++ trunk/linbox/linbox/field/givaro-montg.h
@@ -31,15 +31,8 @@
 #include "linbox/util/xml/linbox-reader.h"
 #include "linbox/util/xml/linbox-writer.h"
 
-using LinBox::Reader;
-using LinBox::Writer;
-
 #include <iostream>
 #include <string>
-
-using std::istream;
-using std::ostream;
-using std::string;
 
 #endif
 
@@ -103,8 +96,8 @@ namespace LinBox
 		}
     
 #ifdef __LINBOX_XMLENABLED
-		// XML Reader constructor
-		GivaroMontg(Reader &R)
+		// XML LinBox::Reader constructor
+		GivaroMontg(LinBox::Reader &R)
 		{
 			integer p, n;
 
@@ -214,24 +207,24 @@ namespace LinBox
 
 #ifdef __LINBOX_XMLENABLED
 
-		ostream &write(ostream &os) const
+		std::ostream &write(std::ostream &os) const
 		{
-			Writer W;
+			LinBox::Writer W;
 			if( toTag(W) )
 				W.write(os);
 
 			return os;
 		}
 
-		bool toTag(Writer &W) const
+		bool toTag(LinBox::Writer &W) const
 		{
-			string s;
+			std::string s;
 			long card = Montgomery<Std32>::size();
 			size_t i = 0;
 
 			W.setTagName("field");
 			W.setAttribute("implDetail", "givaro-gfq");
-			W.setAttribute("cardinality", Writer::numToString(s, card));
+			W.setAttribute("cardinality", LinBox::Writer::numToString(s, card));
 
 			W.addTagChild();
 			W.setTagName("finite");
@@ -261,39 +254,39 @@ namespace LinBox
 		// e = a0 + a1x + a2x^2 + ..., e is represented as:
 		// "<cn>n</cn>" where n = a0 + a1 * p + a2 * p^2 + ...
 		// 
-		ostream &write(ostream &os, const Element &e) const
+		std::ostream &write(std::ostream &os, const Element &e) const
 		{
-			Writer W;
+			LinBox::Writer W;
 			if( toTag(W, e))
 				W.write(os);
 
 			return os;
 		}
 
-		bool toTag(Writer &W, const Element &e) const
+		bool toTag(LinBox::Writer &W, const Element &e) const
 		{
-			string s;
+			std::string s;
 			long rep = _log2pol[ (unsigned long) e];
 
 			W.setTagName("cn");
-			W.addDataChild(Writer::numToString(s, rep));
+			W.addDataChild(LinBox::Writer::numToString(s, rep));
 		  
 			return true;
 		}
 
-		istream &read(istream &is, Element &e) const
+		std::istream &read(std::istream &is, Element &e) const
 		{
-			Reader R(is);
+			LinBox::Reader R(is);
 			if( !fromTag(R, e)) {
-				is.setstate(istream::failbit);
+				is.setstate(std::istream::failbit);
 				if(!R.initalized())
-					is.setstate(istream::badbit);
+					is.setstate(std::istream::badbit);
 			}
 
 			return is;
 		}
 
-		bool fromTag(Reader &R, Element &e) const
+		bool fromTag(LinBox::Reader &R, Element &e) const
 		{
 			unsigned long i;
 
