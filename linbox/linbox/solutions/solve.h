87de2a1f542be15225344a460fc1163f7c0155b5
giorgi <giorgi@8563fe0b-bd16-0410-b45f-99c24e105077>
2005-07-04 15:11:54 +0000

    2005-07-4 Pascal Giorgi 	* linbox/solutions/methods.h: extend dixon method 	* linbox/solutions/solve.h: add rational-solver and diophantine-solver support for BlasElimination method (need to fix denominator output). 				    also add support for BlasElimination over finite field (not fully implemented)

--- linbox/linbox/solutions/solve.h
+++ linbox/linbox/solutions/solve.h
@@ -26,14 +26,13 @@ namespace LinBox
 {
 
 	// for specialization with respect to the DomainCategory
-    template< class Vector, class Blackbox, class SolveMethod, class DomainCategory>
-    Vector & solve (
-		Vector & 				x,
-        const Blackbox &        A,
-		const Vector &			b,
-        const DomainCategory &  tag,
-        const SolveMethod &     M);
-//		SolveStatus * 			s = 0);
+	template< class Vector, class Blackbox, class SolveMethod, class DomainCategory>
+	Vector & solve (Vector & 			x,
+			const Blackbox &                A,
+			const Vector &			b,
+			const DomainCategory &        tag,
+			const SolveMethod &            M);
+	//		SolveStatus * 			s = 0);
 
 	/** \brief Solve Ax = b, for x.
 	 *
@@ -46,30 +45,29 @@ namespace LinBox
 	 
          \ingroup solutions
         */
-	 //and the SolveStatus, if non-null, is set to indicate inconsistency.
-    template< class Vector, class Blackbox, class SolveMethod>
-    Vector & solve (
-		Vector & 				x,
-        const Blackbox &        A,
-		const Vector &			b,
-        const SolveMethod &     M)
-//		SolveStatus * 			s = 0)
-	{ return solve(x, A, b, 
-			typename FieldTraits<typename Blackbox::Field>::categoryTag(), M);
+	//and the SolveStatus, if non-null, is set to indicate inconsistency.
+	template< class Vector, class Blackbox, class SolveMethod>
+	Vector & solve (Vector &        		x,
+			const Blackbox &                A,
+			const Vector &			b,
+			const SolveMethod &             M)
+	//		SolveStatus * 			s = 0)
+	{ 
+		return solve(x, A, b, typename FieldTraits<typename Blackbox::Field>::categoryTag(), M);
 	}
 
 	// the solve with default method
-    template< class Vector, class Blackbox>
+	template< class Vector, class Blackbox>
 	Vector& solve(Vector& x, const Blackbox& A, const Vector& b)
 	{ return solve(x, A, b, Method::Hybrid()); }
 
-// in methods.h FoobarMethod and Method::Foobar are the same class.
-// in methods.h template<BB> bool useBB(const BB& A) is defined.
+	// in methods.h FoobarMethod and Method::Foobar are the same class.
+	// in methods.h template<BB> bool useBB(const BB& A) is defined.
 
 	// specialize this on blackboxes which have local methods
 	template <class Vector, class BB> 
 	Vector& solve(Vector& x, const BB& A, const Vector& b, 
-					const Method::Hybrid& m)
+		      const Method::Hybrid& m)
 	{	
 		if (useBB(A)) return solve(x, A, b, Method::Blackbox(m)); 
 		else return solve(x, A, b, Method::Elimination(m));
@@ -77,28 +75,28 @@ namespace LinBox
 
 	template <class Vector, class BB> 
 	Vector& solve(Vector& x, const BB& A, const Vector& b, 
-					const Method::Blackbox& m)
+		      const Method::Blackbox& m)
 	{ 
-	// what is chosen here should be best and/or most reliable currently available choice
-	//	return solve(x, A, b, Method::Lanczos(m));
-	// maybe should be:
+		// what is chosen here should be best and/or most reliable currently available choice
+		//	return solve(x, A, b, Method::Lanczos(m));
+		// maybe should be:
 		return solve(x, A, b, Method::BlockLanczos(m));
 	}
 
-// temporary - fix this
+	// temporary - fix this
 #define inBlasRange(p) true
 
 	template <class Vector, class BB> 
 	Vector& solve(Vector& x, const BB& A, const Vector& b, 
-					const Method::Elimination& m)
+		      const Method::Elimination& m)
 	{ 
 		integer c, p;
 		A.field().cardinality(c);
 		A.field().characteristic(p);
 		//if ( p == 0 || (c == p && inBlasRange(p)) )
-			return solve(x, A, b, 
-					FieldTraits<typename BB::Field>::categoryTag(), 
-					Method::BlasElimination(m)); 
+		return solve(x, A, b, 
+			     FieldTraits<typename BB::Field>::categoryTag(), 
+			     Method::BlasElimination(m)); 
   		//else 
 		//	return solve(x, A, b, 
 		//			FieldTraits<typename BB::Field>::categoryTag(), 
@@ -107,7 +105,7 @@ namespace LinBox
 
 	template <class Vector, class Field> 
 	Vector& solve(Vector& x, const SparseMatrix<Field>& A, const Vector& b, 
-					const Method::Elimination& m)
+		      const Method::Elimination& m)
 	{	bool consistent = false;
 		// sparse elimination based solver can be called here ?
 
@@ -117,12 +115,12 @@ namespace LinBox
 		}
 		return x;
 	}
-// BlasElimination section ///////////////////
+	// BlasElimination section ///////////////////
 
 	template <class Vector, class BB> 
 	Vector& solve(Vector& x, const BB& A, const Vector& b, 
-           			const RingCategories::ModularTag & tag, 
-					const Method::BlasElimination& m)
+		      const RingCategories::ModularTag & tag, 
+		      const Method::BlasElimination& m)
 	{ 
 		BlasBlackbox<typename BB::Field> B(A); // copy A into a BlasBlackbox
 		return solve(x, B, b, tag, m);
@@ -130,23 +128,25 @@ namespace LinBox
 
 	template <class Vector, class Field> 
 	Vector& solve(Vector& x, const BlasBlackbox<Field>& A, const Vector& b, 
-           			const RingCategories::ModularTag & tag, 
-					const Method::BlasElimination& m)
+		      const RingCategories::ModularTag & tag, 
+		      const Method::BlasElimination& m)
 	{ 
 		bool consistent = false;
-		// Clement puts in call to dense solver mod p ?
+		FactorizedMatrix<Field> LQUP(A);
+		LQUP.left_solve(x, b);
 
-		if ( ! consistent ) {  // we will return the zero vector
-			typename Field::Element zero; A.field().init(zero, 0);
-			for (typename Vector::iterator i = x.begin(); i != x.end(); ++i) *i = zero;
-		}
+		// this should be implemented directly in left_solve 
+		//if ( ! consistent ) {  // we will return the zero vector
+		//	typename Field::Element zero; A.field().init(zero, 0);
+		//	for (typename Vector::iterator i = x.begin(); i != x.end(); ++i) *i = zero;
+		//}
 		return x;
 	} 
 
 	template <class Vector, class BB> 
 	Vector& solve(Vector& x, const BB& A, const Vector& b, 
-           			const RingCategories::IntegerTag & tag, 
-					const Method::BlasElimination& m)
+		      const RingCategories::IntegerTag & tag, 
+		      const Method::BlasElimination& m)
 	{ 
 		BlasBlackbox<typename BB::Field> B(A); // copy A into a BlasBlackbox
 		return solve(x, B, b, tag, m);
@@ -155,8 +155,108 @@ namespace LinBox
 	// specialization when no need to copy matrix
 	template <class Vector, class Field> 
 	Vector& solve(Vector& x, const BlasBlackbox<Field>& A, const Vector& b, 
-   			        const RingCategories::IntegerTag & tag, 
-					const Method::BlasElimination& m)
+		      const RingCategories::IntegerTag & tag, 
+		      const Method::BlasElimination& m)
+	{ 
+		Method::Dixon mDixon(m);
+		return solve (x, A, b, tag, mDixon);
+	} 
+
+
+	/** \brief solver specialization with DixonTraits over integer (no copying)
+	 */
+	template <class Vector, class Ring> 
+	Vector& solve(Vector& x, const BlasBlackbox<Ring>& A, const Vector& b, 
+		      const RingCategories::IntegerTag tag, const Method::Dixon& m)
+	{ 
+		// NOTE: righ now return only the numerator of the rational solution
+		//       NEED TO BE FIXED !!!
+
+		linbox_check ((x.size () == A.coldim ()) && (b.size () == A.rowdim ()));
+		commentator.start ("Padic Integer Blas-based Solving", "solving");
+		
+		typedef Modular<double> Field;
+		// 0.7213475205 is an upper approximation of 1/(2log(2))
+		RandomPrime genprime( 26-(int)ceil(log((double)A.rowdim())*0.7213475205)); 
+		RationalSolver<Ring, Field, RandomPrime, DixonTraits> rsolve(A.field(), genprime); 		
+		typename Ring::Element d;
+		SolverReturnStatus status;
+
+		// if singularity unknown and matrix is square, we try nonsingular solver
+		switch ( m.singular() ) {
+		case SINGULARITY_UNKNOWN:
+			switch (A.rowdim() == A.coldim() ? 
+				status=rsolve.solveNonsingular(x, d, A, b, false ,m.maxTries()) : SS_SINGULAR) {				
+			case SS_OK:
+				m.singular(NONSINGULAR);				
+				break;					
+			case SS_SINGULAR:
+				switch (m.solution()){
+				case DETERMINIST:
+					status= rsolve.monolithicSolve(x, d, A, b, false, false, m.maxTries(), 
+								       (m.certificate()? SL_LASVEGAS: SL_MONTECARLO));
+					break;					
+				case RANDOM:
+					status= rsolve.monolithicSolve(x, d, A, b, false, true, m.maxTries(), 
+								       (m.certificate()? SL_LASVEGAS: SL_MONTECARLO));
+					break;					
+				case DIOPHANTINE:
+					DiophantineSolver<RationalSolver<Ring,Field,RandomPrime, DixonTraits> > dsolve(rsolve);
+					status= dsolve.diophantineSolve(x, d, A, b, m.maxTries(),
+									(m.certificate()? SL_LASVEGAS: SL_MONTECARLO));
+					break;					
+				default:
+					break;
+				}			
+				break;
+			}
+			
+		case NONSINGULAR:
+			rsolve.solveNonsingular(x, d, A, b, false ,m.maxTries());
+			break;
+			    
+		case SINGULAR:
+			switch (m.solution()){
+			case DETERMINIST:
+				status= rsolve.monolithicSolve(x, d, A, b, 
+							       false, false, m.maxTries(), 
+							       (m.certificate()? SL_LASVEGAS: SL_MONTECARLO));
+				break;
+				
+			case RANDOM:
+				status= rsolve.monolithicSolve(x, d, A, b, 
+							       false, true, m.maxTries(), 
+							       (m.certificate()? SL_LASVEGAS: SL_MONTECARLO));
+				break;
+				
+			case DIOPHANTINE:
+				DiophantineSolver<RationalSolver<Ring,Field,RandomPrime, DixonTraits> > dsolve(rsolve);
+				status= dsolve.diophantineSolve(x, d, A, b, m.maxTries(),
+								(m.certificate()? SL_LASVEGAS: SL_MONTECARLO));
+				break;
+				
+			default:
+				break;
+			}		
+		default:			    
+			break;
+		}
+
+		
+		if ( status == SS_INCONSITENT ) {  // we will return the zero vector
+			typename Ring::Element zero; A.field().init(zero, 0);
+			for (typename Vector::iterator i = x.begin(); i != x.end(); ++i) *i = zero;
+		}
+		return x;
+	}	
+
+	// same thing for DenseMatrix ??
+
+	// specialization when no need to copy matrix
+	template <class Vector, class Field> 
+	Vector& solve(Vector& x, const DenseMatrix<Field>& A, const Vector& b, 
+		      const RingCategories::ModularTag & tag, 
+		      const Method::BlasElimination& m)
 	{ 
 		bool consistent = false;
 		// Pascal puts in call to dense rational solver (which choses prime...)
@@ -168,46 +268,28 @@ namespace LinBox
 		return x;
 	} 
 
-// same thing for DenseMatrix ??
+	/*
+	  struct BlasEliminationCRASpecifier;
+	  // Extra case put in (1) for timing comparison or (2) for parallelism or 
+	  // (3) as an example of how we might leave an abandoned choice around in a 
+	  // callable state for future reference 
+	  template <class Vector, class Field> 
+	  Vector& solve(Vector& x, const DenseMatrix<Field>& A, const Vector& b, 
+	  const RingCategories::IntegerTag & tag, 
+	  const BlasEliminationCRASpecifier & m)
+	  { // (low priority) J-G puts in code using CRA object CRA and solve(x, A, b, ModularTag, Method::BlasElimination) 
+	  typename Field::Element zero; A.field().init(zero, 0);
+	  for (typename Vector::iterator i = x.begin(); i != x.end(); ++i) *i = zero;
+	  return x;
+	  } 
+	*/
 
-	// specialization when no need to copy matrix
-	template <class Vector, class Field> 
-	Vector& solve(Vector& x, const DenseMatrix<Field>& A, const Vector& b, 
-           			const RingCategories::ModularTag & tag, 
-					const Method::BlasElimination& m)
-	{ 
-		bool consistent = false;
-		// Pascal puts in call to dense rational solver (which choses prime...)
-
-		if ( ! consistent ) {  // we will return the zero vector
-			typename Field::Element zero; A.field().init(zero, 0);
-			for (typename Vector::iterator i = x.begin(); i != x.end(); ++i) *i = zero;
-		}
-		return x;
-	} 
-
-/*
-	struct BlasEliminationCRASpecifier;
-	// Extra case put in (1) for timing comparison or (2) for parallelism or 
-	// (3) as an example of how we might leave an abandoned choice around in a 
-	// callable state for future reference 
-	template <class Vector, class Field> 
-	Vector& solve(Vector& x, const DenseMatrix<Field>& A, const Vector& b, 
-   			        const RingCategories::IntegerTag & tag, 
-					const BlasEliminationCRASpecifier & m)
-	{ // (low priority) J-G puts in code using CRA object CRA and solve(x, A, b, ModularTag, Method::BlasElimination) 
-		typename Field::Element zero; A.field().init(zero, 0);
-		for (typename Vector::iterator i = x.begin(); i != x.end(); ++i) *i = zero;
-		return x;
-	} 
-*/
-
-// NonBlasElimination section ////////////////
+	// NonBlasElimination section ////////////////
 
 	template <class Vector, class BB> 
 	Vector& solve(Vector& x, const BB& A, const Vector& b, 
-   					const RingCategories::ModularTag & tag, 
-					const Method::NonBlasElimination& m)
+		      const RingCategories::ModularTag & tag, 
+		      const Method::NonBlasElimination& m)
 	{	DenseMatrix<typename BB::Field> B(A); // copy
 		return solve(x, B, b, tag, m);
 	}
@@ -215,36 +297,36 @@ namespace LinBox
 	// specialization when no need to copy
 	template <class Vector, class Field> 
 	Vector& solve(Vector& x, const DenseMatrix<Field>& A, const Vector& b, 
-   			        const RingCategories::ModularTag & tag, 
-					const Method::NonBlasElimination& m)
+		      const RingCategories::ModularTag & tag, 
+		      const Method::NonBlasElimination& m)
 	{ //Do we have a non blas elimination?  There was not one in the original solve.h (now algorithms/bbsolve.h).
 		return x;
 	}
 
-// note: no need for NonBlasElimination when RingCategory is integer
+	// note: no need for NonBlasElimination when RingCategory is integer
 
-// Lanczos ////////////////
+	// Lanczos ////////////////
 	// may throw SolverFailed or InconsistentSystem
 	template <class Vector, class BB> 
 	Vector& solve(Vector& x, const BB& A, const Vector& b, 
-   			        const RingCategories::ModularTag & tag, 
-					const Method::BlockLanczos& m)
+		      const RingCategories::ModularTag & tag, 
+		      const Method::BlockLanczos& m)
 	{
 		// we should catch the SolveFailed exception here
 		try { solve(A, x, b, A.field(), m); }
 		catch (SolveFailed)
-		{
-			typename BB::Field::Element zero; A.field().init(zero, 0);
-			for (typename Vector::iterator i = x.begin(); i != x.end(); ++i) *i = zero;
-		}
+			{
+				typename BB::Field::Element zero; A.field().init(zero, 0);
+				for (typename Vector::iterator i = x.begin(); i != x.end(); ++i) *i = zero;
+			}
 		return x;
 	}
 
 	// may throw SolverFailed or InconsistentSystem
 	template <class Vector, class BB> 
 	Vector& solve(Vector& x, const BB& A, const Vector& b, 
-   			        const RingCategories::IntegerTag & tag, 
-					const Method::BlockLanczos& m)
+		      const RingCategories::IntegerTag & tag, 
+		      const Method::BlockLanczos& m)
 	{
 		// should catch SolveFailed exception
 		//CRA? //solve(A, x, b, A.field(), m);
@@ -252,13 +334,13 @@ namespace LinBox
 	}
 
 
-// Wiedemann section ////////////////
+	// Wiedemann section ////////////////
 
 	// may throw SolverFailed or InconsistentSystem
 	template <class Vector, class BB> 
 	Vector& solve(Vector& x, const BB& A, const Vector& b, 
-   			        const RingCategories::ModularTag & tag, 
-					const Method::Wiedemann& m)
+		      const RingCategories::ModularTag & tag, 
+		      const Method::Wiedemann& m)
 	{
 		// adapt to earlier signature of wiedemann solver
 		typename WiedemannSolver<typename BB::Field, Vector>::ReturnStatus s;
@@ -269,24 +351,24 @@ namespace LinBox
 
 	template <class Vector, class BB> 
 	Vector& solve(Vector& x, const BB& A, const Vector& b, 
-   			        const RingCategories::IntegerTag & tag, 
-					const Method::Wiedemann& m)
+		      const RingCategories::IntegerTag & tag, 
+		      const Method::Wiedemann& m)
 	{ 	// Must put in cra loop
 		/*struct solver 
-		{ Vector& operator ()(Vector& x, const Modular<double>& F) { 
-			// make modular Am bm from A, b, make sm, then
-		MatrixHom::mod(
-		solve (xm, Am, bm, m, sm)
-		return xm;
-		}
+		  { Vector& operator ()(Vector& x, const Modular<double>& F) { 
+		  // make modular Am bm from A, b, make sm, then
+		  MatrixHom::mod(
+		  solve (xm, Am, bm, m, sm)
+		  return xm;
+		  }
 		*/
 		return x;
 	}
 
-/* remark 1.  I used copy constructors when switching method types.
-But if the method types are (empty) child classes of a common  parent class containing
-all the information, then casts can be used in place of copies.
-*/ 
+	/* remark 1.  I used copy constructors when switching method types.
+	   But if the method types are (empty) child classes of a common  parent class containing
+	   all the information, then casts can be used in place of copies.
+	*/ 
 
 } // LinBox
 
