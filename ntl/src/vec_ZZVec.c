2f6dde919ec33fe6a2e4fbbdc6606da8ecb33d27
karel <karel@8563fe0b-bd16-0410-b45f-99c24e105077>
2006-06-21 15:34:32 +0000

    Also removing trunk/MapleLB and trunk/ntl, and renaming trunk/linbox-www/ to simply trunk/www/; same with trunk/linbox-server/

--- ntl/src/vec_ZZVec.c
+++
@@ -1,5 +0,0 @@
-
-#include <NTL/vec_ZZVec.h>
-
-
-NTL_vector_impl(ZZVec,vec_ZZVec)
--- ntl/src/vec_xdouble.c
+++
@@ -1,9 +0,0 @@
-
-#include <NTL/vec_xdouble.h>
-
-NTL_vector_impl(xdouble,vec_xdouble)
-
-NTL_io_vector_impl(xdouble,vec_xdouble)
-
-NTL_eq_vector_impl(xdouble,vec_xdouble)
-
