e1af1855d9d0ca8089751999bee9f22667db59f2
jmay <jmay@8563fe0b-bd16-0410-b45f-99c24e105077>
2006-09-20 21:34:52 +0000

    Properly isolated MPI dependant code.

--- trunk/linbox/linbox/solutions/det.h
+++ trunk/linbox/linbox/solutions/det.h
@@ -369,9 +369,14 @@ namespace LinBox {
 		ChineseRemainder< Modular<double> > cra(3UL);
 		IntegerModularDet<Blackbox,MyMethod> iteration(A, M);
 		integer dd; // use of integer due to non genericity of cra. PG 2005-08-04
+
+	#ifdef __LINBOX_HAVE_NTL
 		Communicator* C = M.communicatorp();
 		std::cout << "using cra_det with C = " << int(C) << " and dd = " << dd << std::endl;
-		cra(dd, iteration, genprime, C);
+                cra(dd, iteration, genprime, C);
+	#else
+		cra(dd, iteration, genprime);
+	#endif
 		A.field().init(d, dd); // convert the result from integer to original type
 		commentator.stop ("done", NULL, "det");
 		return d;
