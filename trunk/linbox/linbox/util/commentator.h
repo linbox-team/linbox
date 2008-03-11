91c60aa8419761bcf130be5570daf3bce9270c2f
pernet <pernet@8563fe0b-bd16-0410-b45f-99c24e105077>
2008-03-11 01:09:19 +0000

    updated after having fixed the bugs showing up with sage.

--- trunk/linbox/linbox/util/commentator.h
+++ trunk/linbox/linbox/util/commentator.h
@@ -742,9 +742,8 @@ namespace LinBox
 
 	class Commentator {
 	    public: 
-		inline Commentator () : cnull (new nullstreambuf()) {}
-		//inline ~Commentator () {}
-		//inline ~Commentator () { delete cnull; }
+		inline Commentator () : cnull (new nullstreambuf) {}
+		inline  ~Commentator () {}
 		inline void start (const char *description, const char *fn = (const char *) 0, unsigned long len = 0) {}
 		inline void startIteration (unsigned int iter, unsigned long len = 0) {}
 		inline void stop (const char *msg, const char *long_msg = (const char *) 0, const char *fn = (const char *) 0) {}
@@ -805,7 +804,7 @@ namespace LinBox
 	    private:
 		// Null std::ostream prints nothing
 		struct nullstreambuf : public std::streambuf {
-			nullstreambuf () {};	
+			nullstreambuf () {};
 			~nullstreambuf () {};
 			inline std::streampos seekoff (std::streambuf::off_type, std::ios::seekdir, std::ios::openmode) { return 0; }
 			inline std::streampos seekpos (std::streambuf::pos_type, std::ios::openmode) { return 0; }
