ca68aaf70cd96a2d33563f21be82ef88aa4e5a76
saunders <bdsaunders@users.noreply.github.com>
2008-08-18 15:31:06 +0000

    this should bring svn repository up to Aug 08

---
+++ LinBoxWebservice/src/samples/quickstart/clients/OperationTimeEstimator.java
@@ -0,0 +1,424 @@
+// OperationTimeEstimator.java
+// Matthew Fendt and David Saunders, Summer '08
+// Provides simple time estimates for the duration of certain matrix operations
+
+
+
+package samples.quickstart.clients;
+
+import java.lang.*;
+
+public class OperationTimeEstimator
+{
+    
+    /*
+      Argument meanings:
+      
+      m = number of rows.
+      n = number of cols.
+      s = number of entries specified in input data file (any other matrix 
+          entries are implicitly zero).
+      d = number of bits in largest entry, approximately 
+          max_v(1 + log_2(abs(v))).
+      r = rank, 0 <= r <= min(m, n).  Generally unknown to user, but 
+          occasionally known a priori.
+      
+
+      File formats:
+       SMS format example:
+       4 6 M
+       1 1 3
+       1 3 7
+       2 2 9
+       3 1 27
+       4 3 -100
+       0 0 0
+       m = 4, n = 6, s = 5, d = 1 + log(100), r = 4.
+       s is from number of lines strictly between '4 6 M' and '0 0 0'.
+       d from 3 7 9 27 -100
+       
+       Other basic sparse format:
+       4 6 5
+       1 1 3
+       1 3 7
+       2 2 9
+       3 1 27
+       4 6 -100
+       parameters as before, s is from third number on line 1.
+       
+       Dense format:
+       4 6
+       3  0 7 0 0  0
+       0  9 0 0 0  0
+       27 0 0 0 0  0
+       0  0 0 0 0 -100
+       m = 4, n = 6, s = 24, d = 1 + lg(100),
+       s is from m*n.
+    */
+
+
+
+
+
+    // ************************************************************************
+    // ****** Machine specific costs.
+
+    // true iff machine cost factors have been obtained.
+    static boolean GoodMachineCosts = false;
+    static double C; // cost of wordsize modular axpy.
+    static double D; // cost per entry of wordsize modular dot product.
+    // Thus D*n is cost of length n vector dot product, wordsize modular.
+
+    // average cost of wordsize modular axpy, when implicit within blas.
+    static double E;
+
+    int MAX_LENGTH = 100;
+    // ************************************************************************
+
+    static void initMachineCosts (/* url Service */) 
+	// to become a service call getting these values.
+    {   C = 1/25.42e6; D = 1/20.46e6; E = 0.5e-8; GoodMachineCosts = true;   }
+    // ************************************************************************
+
+
+    boolean isDense (int m, int n, double s) 
+	// a kludge for now.
+    {   return m*n < 3*s;   }
+
+
+
+    // *********************************************************************** 
+
+    // cost estimate in seconds to compute minimum polynomial of mxn integer 
+    // matrix of rank r and having s nonzero entries of size bounded by d bits.
+    double minpolyCost (int m, int n, double s, int d, int r)    
+    {
+	if ( ! GoodMachineCosts ) initMachineCosts();
+	if (m > n) 
+	    {
+		// Swamp m and n
+		int temp = m;
+		m = n;
+		n = temp;
+	    }
+
+	if (r == 0) r = m;
+	double F = 1; // minpoly fudge factor.
+
+	return isDense(m,n,s) ? F*E*d*m*n*r : F*d*(C*s + D*n)*r;
+    }
+
+    // ************************************************************************
+    double detCost (int m, int n, double s, int d)       
+    {
+	int r = 0;
+
+	if ( ! GoodMachineCosts ) initMachineCosts();
+
+	if (m > n)
+	    {
+		// Swap m and n
+		int temp = m;
+		m = n;
+		n = temp;
+	    }
+
+	if (r == 0) r = m;
+	double F = 1; // det fudge factor.
+	
+	return isDense(m,n,s) ? F*d*E*m*n*r : F*d*minpolyCost(m, n, s, d, m);
+    }
+
+    // ************************************************************************
+    double rankCost (int m, int n, double s, int d)
+    { 
+	int r = 0;
+
+	if ( ! GoodMachineCosts ) initMachineCosts();
+
+	if (m > n)
+	    {
+		// Swap m and n
+		int temp = m;
+		m = n;
+		n = temp;
+	    }
+
+	if (r == 0) r = m;
+	double F = 2; // rank fudge factor.
+	return isDense(m,n,s) ? F*E*m*n*r : F*minpolyCost(m, n, s, 1, m);
+    }
+
+    // ************************************************************************
+    double valenceCost (int m, int n, double s, int d)
+    { 
+	double F = 1; // valence fudge factor.
+	return F*minpolyCost(m,n,s,d, 0); 
+    }
+
+
+    // ************************************************************************
+
+
+    double traceCost (int m, int n, double s, int d)
+    {	// Trace is very fast.  Perhaps just return 1 second, ignoring input.
+	double F = 0.5; // trace fudge factor.
+	
+	if (m > n) 
+	    {
+		// Swamp m and n
+		int temp = m;
+		m = n;
+		n = temp;
+	    }
+	
+	return F*C*d*m;
+    }
+
+    // ************************************************************************
+
+    double smithformCost (int m, int n, double s, int d)
+    {
+	int r = 0;
+
+	double F = 1.0/6; // smithform fudge factor.
+
+	if (m > n) 
+	    {
+		// Swamp m and n
+		int temp = m;
+		m = n;
+		n = temp;
+	    }
+
+	if (r == 0) r = m;
+	
+	// cubic when m = n = r.  When structure is rich expect m^3.5 or even m^4.
+	return F*E*d*m*n*r;  // future tuning...
+    }
+
+ // ************************************************************************
+
+
+    double solveCost (int m, int n, double s, int d)
+    {
+	int r = 0;
+	double F = 1; // solve fudge factor.
+	double BBfactor = 6;
+
+	if (m > n) 
+	    {
+		// Swamp m and n
+		int temp = m;
+		m = n;
+		n = temp;
+	    }
+
+	if (r == 0) r = m;
+	
+	return isDense(m,n,s) ? F*d*E*m*n*r : 
+	    BBfactor*F*d*minpolyCost(m, n, s, d, m);
+    }
+
+ // ************************************************************************
+
+
+
+
+
+    String prettyIter (double t, int k)
+    {
+	int m = 60, h = 60*m, d = 24*h;
+	StringBuffer foo = new StringBuffer(MAX_LENGTH);
+	if (t < 1) foo.append("1s");
+	else if (k > 1)
+	    {
+		if (t < m)
+		    { foo.append(Math.floor(t)); foo.append("s");}
+
+		else if (t < h) 
+		    {foo.append(Math.floor(t/m)); foo.append("m "); 
+		    foo.append(prettyIter(t - m*Math.floor(t/m),1)); }
+
+		else if (t < d) 
+		    {foo.append(Math.floor(t/h)); foo.append("h ");
+		    foo.append(prettyIter(t - h*Math.floor(t/h),1));}
+
+		else 
+		    {foo.append(Math.floor(t/d)); foo.append("d "); 
+		    foo.append(prettyIter(t - d*Math.floor(t/d),1));}
+	    }
+	else 
+	    {
+		if (t < m)
+		    { foo.append(Math.ceil(t)); foo.append("s");}
+
+		else if (t < h) 
+		    {foo.append(Math.ceil(t/m)); foo.append("m");}
+
+		else if (t < d) 
+		    {foo.append(Math.ceil(t/h)); foo.append("h");}
+
+		else 
+		    {foo.append(Math.ceil(t/d)); foo.append("d");}
+	    }
+
+	return foo.toString();
+    }
+
+
+    // ************************************************************************
+
+    String pretty(double t) { return prettyIter (t, 2); }
+
+    // ************************************************************************
+
+    public OperationTimeEstimator()
+    {
+	initMachineCosts();
+    }
+
+
+    // ************************************************************************
+
+
+    public String estimateTime(String operation, String matrix, 
+			       boolean detailedEstimate)
+    {
+	// The estimated time
+	double estimate;
+
+	// Delineators for the matrix string
+	String[] temp = matrix.split("%0A|%0D|\\+");
+
+	// The rows and columns of the matrix
+	int m = -1;
+	int n = -1;
+
+	boolean pastM = false;
+	boolean firstSet = true;
+	boolean dense = false;
+
+
+	int count = 1;
+
+	// The largest entry
+	int d = -1;
+
+	// The number of non zero entries
+	double s = 0;
+	
+	for (int j = 0; j < temp.length; j++)	
+	    {	   
+		//System.out.println(temp[j]);
+
+		// Set m
+		if (m == -1 && !temp[j].equals(""))
+		    {
+			m = Integer.parseInt(temp[j]);
+			System.out.println("M is " + m);
+		    }
+		
+		// Set n
+		else if (n == -1 && m != -1 && !temp[j].equals(""))
+		    {
+			n = Integer.parseInt(temp[j]);
+			System.out.println("N is " + n);
+		    }
+		
+		// See if there is an 'M' or 'S' in the matrix file
+		else if (pastM == false && m != -1 && n != -1)
+
+		    {
+			// Sparse format
+			if (temp[j].equalsIgnoreCase("m") ||
+			  temp[j].equalsIgnoreCase("s"))
+			    {
+				pastM = true;
+				dense = false;
+			    }
+
+			// Dense format
+			else
+			    {
+				pastM = true;
+				dense = true;
+			    }
+		    }
+
+		// Find the largest value
+		else if (pastM == true && !temp[j].equals(""))
+		    {
+			// Since the file is in <i, j, v> pairs, we
+			// only need to look at every third number to
+			// determine the largest value
+			if (count < 3 && dense == false)
+			    count ++;
+			
+			// Temporarily let the first value in the
+			// matrix be the largest
+			else if (firstSet == true)
+			    {
+				firstSet = false;
+				d = Math.abs(Integer.
+					     parseInt(temp[j]));
+				count = 1;
+				
+				s++;
+			    }
+			
+			// If this value is bigger than the largest,
+			// it is the new largest
+			else if (firstSet == false)
+			    {
+				int tempD = 
+				    Integer.parseInt(temp[j]);
+
+				if (Math.abs(tempD) > d)
+				    d = Math.abs(tempD);
+				
+				count = 1;
+				
+				s++;
+			    }
+		    }			
+	    }
+	    
+
+	// Take log base 2 of d
+	Double tempDouble = Math.log(d) / Math.log(2);
+	d = tempDouble.intValue() + 1;
+
+	System.out.println("D is " + d);
+	System.out.println("S is " + s);
+
+
+	if (operation.equalsIgnoreCase("rank"))
+	    estimate = rankCost(m,n,s,d);
+	
+	else if (operation.equalsIgnoreCase("determinant"))
+	    estimate = detCost(m,n,s,d);
+
+	else if (operation.equalsIgnoreCase("valence"))
+	    estimate = valenceCost(m,n,s,d);
+
+	else if (operation.equalsIgnoreCase("trace"))
+	    estimate = traceCost(m,n,s,d);
+
+	else if (operation.equalsIgnoreCase("smithNormalForm"))
+	    estimate = smithformCost(m,n,s,d);
+
+	else
+	    return "No time estimate available for " + operation;
+
+	// The answer as a readable string	
+	if (detailedEstimate == true)
+		return pretty(estimate);
+
+	// The answer in seconds
+	else 
+	    return Double.toString(estimate);
+    }
+}
+
+
---
+++ LinBoxWebservice/src/samples/quickstart/queue/QueueManager.java
@@ -0,0 +1,629 @@
+// QueueManager.java
+// Matthew Fendt Summer '07-'08 Research
+// Receives calls from the 'Middleman' web service.  Enqueues these requests,
+// calls the appropriate 'Transfer Agent' web service, and gives the answers
+// back to the 'Middleman' web service
+
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.lang.Thread;
+import java.io.*;
+import java.net.*;
+
+import samples.quickstart.middleman.QueueRequest;
+import samples.quickstart.clients.OperationTimeEstimator;
+
+import samples.quickstart.service.adb.TransferAgentTransferAgentHttpportStub;
+import samples.quickstart.service.adb.TransferAgentTransferAgentHttpportCallbackHandler;
+
+public class QueueManager implements QueueRequest
+{
+    // ***********************************************************************
+    // The queue for holding requests
+    static LinBoxQueue q;
+
+    // Seconds since an enqueue request
+    static int secsSinceCall = 0;
+
+    // The maximum number of answers that can be stored
+    static final int SIZE = 100;
+
+    // Stores the answers until the 'Middleman' web service picks them up
+    static String[] answers;
+
+    // Stores the estimated time to complete the operation
+    static double[] timeEstimate;
+
+    // Keeps track of if we finished the computation
+    static int[] finished;
+    
+    // A connection to the 'Transfer Agent' web service
+    static TransferAgentTransferAgentHttpportStub stub;
+
+    // The number of operations in the queue;
+    static int numOps;
+
+    // The machine specs
+    //    static int[] specs;
+
+    // For quickly estimating computation time
+    static OperationTimeEstimator ote;
+
+    // Are we currently processing a request
+    static boolean processing = false;
+
+    // ***********************************************************************
+
+    // Constructor class
+    public QueueManager() 
+    {
+	// Creates the queue where the request will be stored until they are
+	// ready to be processed
+	q = new LinBoxQueue();	
+	
+	numOps = 0;
+
+
+	try {
+	    // Create the stub that will connect to the LinBox web service
+	    stub = new TransferAgentTransferAgentHttpportStub("http://hmrg.pc.cis.udel.edu:2000/axis2/services/TransferAgent");
+
+	    // Create an operation time estimator object
+	    ote = new OperationTimeEstimator();
+
+	/*
+	    // For getting the machine specs from the computing machine
+	    TransferAgentTransferAgentHttpportStub.GetMachineSpecsResponse 
+		res = stub.getMachineSpecs();
+
+	    specs = res.get_return();
+
+
+	*/
+	}
+
+	catch (Exception e) { }
+
+    }
+    
+    // Returns the number of operations in the queue
+    public int countOps()
+    {
+	return numOps;
+    }
+
+    // Returns the number of operations in the queue
+    public int checkID(int ID)
+    {
+	return q.checkID(ID);
+    }
+
+    // ***********************************************************************
+    public int enqueue(String matrixOp, String matrix, String IPAddress)
+    {
+	// Insert the request into the queue and get back the ID of the request
+	int ID = q.insertEntry(matrixOp, matrix, IPAddress);
+
+	// Reset the time since an enqueue request
+	secsSinceCall = 0;
+
+	numOps++;
+
+	finished[ID] = 0;
+
+	// Do a quick time estimate
+
+	String estimate = ote.estimateTime(matrixOp, matrix, false);
+
+	timeEstimate[ID] = 
+	    Double.parseDouble(estimate);
+
+	QueueEntry t = q.findEntry(ID);
+
+	t.setEstimatedComputationTime(estimate);
+
+	System.out.println("Estimate for " + ID + ":" + estimate);
+
+	System.out.println("**************************************");
+	System.out.println("New entry into queue:\n");
+	q.printLast();
+	System.out.println("**************************************");
+
+
+	// Gives back the request ID to the 'Middleman' web service
+	return (ID); 
+    }
+
+    // ***********************************************************************
+    private static void dequeue()
+    {
+	// Increase the timeout
+	long soTime = 60 * 60 * 1000; // 1 hour
+	stub._getServiceClient().getOptions().setTimeOutInMilliSeconds(soTime);
+
+	// When there is something in the queue, wait for a little 
+	// while and then remove it from the queue
+
+	try {
+	    Thread.currentThread().sleep(1000 * 5);
+
+	    System.out.println("**************************************");
+	    System.out.println("About to dequeue.  Printing contents of queue before:");
+	    q.printQueue();		
+
+	    
+	    System.out.println("**************************************");
+	    System.out.println("Removing entry " + 
+			       q.tail.getOperationID() + " from queue.");
+	    System.out.println("**************************************");
+
+	    // Where in the answers array the result will be stored
+	    final int answerLocation = q.tail.getOperationID();
+
+	    // Remove the request from the queue and store the operation and
+	    // matrix
+	    String[] s = q.removeEntry();	
+	    numOps--;
+
+	    System.out.println("**************************************");
+	    System.out.println("Printing contents of queue after:");
+	    q.printQueue();		
+
+
+	    // If the operation was rank, call the rank method from the
+	    // 'Transfer Agent' web service
+	    if (s[0].equalsIgnoreCase("rank"))
+		{
+		    // We are processing a request
+		    processing = true;
+
+		    // First, get a time estimate
+		    getTimeEstimate(s[1], "rank", answerLocation);
+
+		    // Creates a 'Rank' object
+		    TransferAgentTransferAgentHttpportStub.Rank req = 
+			new TransferAgentTransferAgentHttpportStub.Rank();
+
+		    // Set the parameter- the user's matrix
+		    req.setMatrix(s[1]);
+
+		    // The callback handler that will listen for the web
+		    // service to give back an answer
+		    TransferAgentTransferAgentHttpportCallbackHandler 
+			callBackHandler=
+			new TransferAgentTransferAgentHttpportCallbackHandler()
+			{
+			    public void receiveResultrank(TransferAgentTransferAgentHttpportStub.RankResponse res)
+			    {
+				// Store the user's answer
+				answers[answerLocation] = res.get_return();
+
+				// We finished the computation
+				finished[answerLocation] = 1;
+
+				// We are no longer processing a reuqest
+				processing = false;
+
+				System.out.println("Answer to operation " + 
+						   answerLocation + " was " +
+						   res.get_return());
+
+				synchronized (this) {
+				    this.notifyAll();
+				}
+			    }
+			    
+			    public void receiveErrorrank(Exception e)
+			    {
+				answers[answerLocation] = "Error in handler";
+
+				// We finished the computation
+				finished[answerLocation] = 1;
+
+				// We are no longer processing a reuqest
+				processing = false;
+
+			    }
+			    
+			};
+		    // Call the web service
+		    stub.startrank(req, callBackHandler);	
+		}
+
+	    // If the operation was determinant, call the determinant method 
+	    // from the 'Transfer Agent' web service
+	    if (s[0].equalsIgnoreCase("determinant"))
+		{
+		    // We are processing a request
+		    processing = true;
+
+		    // Creates a 'Determinant' object
+		    TransferAgentTransferAgentHttpportStub.Determinant req = 
+		      new TransferAgentTransferAgentHttpportStub.Determinant();
+
+		    // Set the parameter- the user's matrix
+		    req.setMatrix(s[1]);
+
+		    // The callback handler that will listen for the web
+		    // service to give back an answer
+		    TransferAgentTransferAgentHttpportCallbackHandler 
+			callBackHandler=
+			new TransferAgentTransferAgentHttpportCallbackHandler()
+			{
+			    public void receiveResultdeterminant(TransferAgentTransferAgentHttpportStub.DeterminantResponse res)
+			    {
+				// Store the user's answer
+				answers[answerLocation] = res.get_return();
+
+				// We finished the computation
+				finished[answerLocation] = 1;
+
+				// We are no longer processing a reuqest
+				processing = false;
+
+				// We are no longer processing a reuqest
+				processing = false;
+
+				System.out.println("Answer to operation " + 
+						   answerLocation + " was " +
+						   res.get_return());
+
+				synchronized (this) {
+				    this.notifyAll();
+
+				}
+			    }
+			    
+			    public void receiveErrordeterminant(Exception e)
+			    {
+				System.err.println("Error in QueueManager:dequeue:determinant: " + e.toString());
+				e.printStackTrace();
+				answers[answerLocation] = "Error in handler";
+
+				// We finished the computation
+				finished[answerLocation] = 1;
+
+				// We are no longer processing a reuqest
+				processing = false;
+			    }
+			    
+			};
+
+		    // Call the web service
+		    stub.startdeterminant(req, callBackHandler);	
+		}
+
+	    // If the operation was trace, call the trace method from the
+	    // 'Transfer Agent' web service
+	    if (s[0].equalsIgnoreCase("trace"))
+		{
+		    // We are processing a request
+		    processing = true;
+
+		    // Creates a 'Trace' object
+		    TransferAgentTransferAgentHttpportStub.Trace req = 
+			new TransferAgentTransferAgentHttpportStub.Trace();
+
+		    // Set the parameter- the user's matrix
+		    req.setMatrix(s[1]);
+
+		    // The callback handler that will listen for the web
+		    // service to give back an answer
+		    TransferAgentTransferAgentHttpportCallbackHandler 
+			callBackHandler=
+			new TransferAgentTransferAgentHttpportCallbackHandler()
+			{
+			    public void receiveResulttrace(TransferAgentTransferAgentHttpportStub.TraceResponse res)
+			    {
+				// Store the user's answer
+				answers[answerLocation] = res.get_return();
+
+				// We finished the computation
+				finished[answerLocation] = 1;
+
+				// We are no longer processing a reuqest
+				processing = false;
+
+				System.out.println("Answer to operation " + 
+						   answerLocation + " was " +
+						   res.get_return());
+
+				synchronized (this) {
+				    this.notifyAll();
+
+
+				}
+			    }
+			    
+			    public void receiveErrortrace(Exception e)
+			    {
+				answers[answerLocation] = "Error in handler";
+			    
+				// We finished the computation
+				finished[answerLocation] = 1;
+
+				// We are no longer processing a reuqest
+				processing = false;
+			    }
+			    
+			};
+
+		    // Call the web service
+		    stub.starttrace(req, callBackHandler);	
+		}
+
+	    // If the operation was valence, call the valence method from the
+	    // 'Transfer Agent' web service
+	    if (s[0].equalsIgnoreCase("valence"))
+		{
+		    // We are processing a request
+		    processing = true;
+
+		    // Creates a 'Valence' object
+		    TransferAgentTransferAgentHttpportStub.Valence req = 
+			new TransferAgentTransferAgentHttpportStub.Valence();
+
+		    // Set the parameter- the user's matrix
+		    req.setMatrix(s[1]);
+
+		    // The callback handler that will listen for the web
+		    // service to give back an answer
+		    TransferAgentTransferAgentHttpportCallbackHandler 
+			callBackHandler=
+			new TransferAgentTransferAgentHttpportCallbackHandler()
+			{
+			    public void receiveResultvalence(TransferAgentTransferAgentHttpportStub.ValenceResponse res)
+			    {
+				// Store the user's answer
+				answers[answerLocation] = res.get_return();
+
+				// We finished the computation
+				finished[answerLocation] = 1;
+
+				// We are no longer processing a reuqest
+				processing = false;
+
+				System.out.println("Answer to operation " + 
+						   answerLocation + " was " +
+						   res.get_return());
+
+				synchronized (this) {
+				    this.notifyAll();
+
+
+				}
+			    }
+			    
+			    public void receiveErrorvalence(Exception e)
+			    {
+				answers[answerLocation] = "Error in handler";
+
+				// We finished the computation
+				finished[answerLocation] = 1;
+
+				// We are no longer processing a reuqest
+				processing = false;
+			    }
+			    
+			};
+
+		    // Call the web service
+		    stub.startvalence(req, callBackHandler);	
+		}
+
+	    // If the operation was SNF, call the SNF method from the
+	    // 'Transfer Agent' web service
+	    if (s[0].equalsIgnoreCase("smithNormalForm"))
+		{
+		    // We are processing a request
+		    processing = true;
+
+		    // Creates a 'SNF' object
+		    TransferAgentTransferAgentHttpportStub.SmithNormalForm 
+			req = new 
+		      TransferAgentTransferAgentHttpportStub.SmithNormalForm();
+
+		    // Set the parameter- the user's matrix
+		    req.setMatrix(s[1]);
+
+		    // The callback handler that will listen for the web
+		    // service to give back an answer
+		    TransferAgentTransferAgentHttpportCallbackHandler 
+			callBackHandler=
+			new TransferAgentTransferAgentHttpportCallbackHandler()
+			{
+			    public void receiveResultsmithNormalForm(TransferAgentTransferAgentHttpportStub.SmithNormalFormResponse res)
+			    {
+				// Store the user's answer
+				answers[answerLocation] = res.get_return();
+
+				// We finished the computation
+				finished[answerLocation] = 1;
+
+				// We are no longer processing a reuqest
+				processing = false;
+
+				System.out.println("Answer to operation " + 
+						   answerLocation + " was " +
+						   res.get_return());
+
+				synchronized (this) {
+				    this.notifyAll();
+				}
+			    }
+			    
+			   public void receiveErrorsmithNormalForm(Exception e)
+			    {
+				answers[answerLocation] = "Error in handler";
+			    
+				// We finished the computation
+				finished[answerLocation] = 1;
+
+				// We are no longer processing a reuqest
+				processing = false;
+			    }
+			    
+			};
+
+		    // Call the web service
+		    stub.startsmithNormalForm(req, callBackHandler);	
+		}	    
+	}
+	
+	catch (Exception e) 
+	    {
+		System.out.println("Error in dequeue");
+		e.printStackTrace();
+	    }
+    }
+
+    // ***********************************************************************
+    // Returns the answer to the user
+    public String pollAnswer(int ID)
+    {
+	return answers[ID];
+    }
+
+
+    // ***********************************************************************
+    // Returns the time estimate to the user
+    public double[] getTime(int ID)
+    {
+	// The first number will be the estimate for that one particular 
+	// operation.  The second number will be an estimate of how long until
+	// that operation will be done.
+	double response[] = new double[2];
+
+	// Return the latest time estimate
+	response[0] = timeEstimate[ID];
+
+	// Now find the total length of all of the operations in front of this
+	// one.
+	double totalTime = q.totalComputationTime(ID);
+
+	// If the request is still in the queue, then set the remaining time
+	if (totalTime != 0)
+	    response[1] = totalTime;
+
+	// If the response is not in the queue and still being worked on, the
+	// time left is the time to do the operation
+	else if (totalTime == 0 && finished[ID] == 0)
+	    response[1] = response[0];
+
+	// If we have already finished the computation, there is no time
+	// remaining
+	else if (totalTime == 0 && finished[ID] == 1)
+	    response[1] = 0;
+
+	return response;
+    }
+
+    // ***********************************************************************
+
+    public static void getTimeEstimate(String matrix, String operation, int ID)
+    {
+	try 
+	    {
+
+		if (operation.equalsIgnoreCase("rank"))
+		    {
+			// Create an EstimateRankTime object
+			TransferAgentTransferAgentHttpportStub.EstimateRankTime
+			    req = new 
+			    TransferAgentTransferAgentHttpportStub.
+			    EstimateRankTime();
+			
+			// Set the parameters
+			req.setMatrix(matrix);
+			
+			// Create the response and get the estimate
+			TransferAgentTransferAgentHttpportStub.
+			    EstimateRankTimeResponse
+			    res = stub.estimateRankTime(req);
+			
+			double time = res.get_return();
+			
+			// Store the estimate
+			timeEstimate[ID] = time;
+		    }
+	    }
+
+	catch (Exception e)
+	    {
+		
+	    }
+    }
+
+
+    public static void main(String[] args)
+    {
+	try{
+	    QueueManager m = new QueueManager();
+
+	    // Makes the object available through RMI
+	    QueueRequest stub = 
+		(QueueRequest) UnicastRemoteObject.exportObject(m, 0);
+
+	    // Creates the answer array
+	    answers = new String[SIZE];
+	    for (int i = 0; i < SIZE; i++)
+		{
+		    answers[i] = null;
+		}
+
+	    // Creates the time estimate array
+	    timeEstimate = new double[SIZE];
+	    for (int i = 0; i < SIZE; i++)
+		{
+		    timeEstimate[i] = -2;
+		}
+
+
+	    // Creates the finished array
+	    finished = new int[SIZE];
+	    for (int i = 0; i < SIZE; i++)
+		{
+		    finished[i] = -1;
+		}
+
+	    // Bind the remote object's stub in the registry
+	    Registry registry = LocateRegistry.getRegistry();
+	    registry.bind("QueueRequest", stub);
+
+
+	    System.err.println("Server ready");
+
+	    // Starts listening for enqueues	    
+	    while (true)
+	    {				
+		// If there is nothing in the queue or we are already 
+		// processing,, sleep for a while and then check again
+		while (q.tail == null || processing == true)
+		    {
+			Thread.currentThread().sleep(1000);
+			secsSinceCall++;
+
+			if (secsSinceCall % 10 >= 0)
+			{
+			    System.out.println("It has been " + 
+				       secsSinceCall + 
+				       " seconds since a call to the queue.");
+			    continue;
+			}
+		    }
+
+		// When there are entries in the queue that are ready to be
+		// processed, dequeue them
+		dequeue();		
+	    }
+	}
+	    
+	catch(Exception e) 
+	    {
+		System.err.println("Server excpetion: " + e.toString());
+		e.printStackTrace();
+	    }
+    }
+}
