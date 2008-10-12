509cfadc2a293d410cc10841df9b478606f3e85c
dumas <dumas@8563fe0b-bd16-0410-b45f-99c24e105077>
2008-10-12 13:54:08 +0000

    binary accelerations for sparse solvers

--- trunk/linbox/linbox/algorithms/triangular-solve.h
+++ trunk/linbox/linbox/algorithms/triangular-solve.h
@@ -2,13 +2,14 @@
  * (C) LinBox 2008
  * Triangular Solve
  * See COPYING for license information.
- * Time-stamp: <15 Sep 08 14:30:09 Jean-Guillaume.Dumas@imag.fr> 
+ * Time-stamp: <18 Sep 08 17:49:04 Jean-Guillaume.Dumas@imag.fr> 
  * ===================================================================
  */
 #ifndef __TRI_SOLVE_INL
 #define __TRI_SOLVE_INL
 
 #include "linbox/vector/vector-domain.h"
+#include "linbox/field/gf2.h"
 
 namespace LinBox 
 {
@@ -113,6 +114,78 @@ namespace LinBox
         }
         
         commentator.stop ("done", NULL, "ltrsm");
+        return x;
+    }
+
+    template <class _Matrix, class Vector1, class Vector2> Vector1&
+    upperTriangularSolveBinary (Vector1& x,
+                          const _Matrix  &U,
+                          const Vector2& b)
+    {
+//         linbox_check( x.size() == U.coldim() );
+//         linbox_check( b.size() == U.rowdim() );
+        typedef _Matrix Matrix;
+        typedef GF2 Field;
+        const GF2 F;
+
+        commentator.start ("Sparse Elimination Upper Triangular Solve over GF(2)", "utrsmtwo");
+
+        typename Vector2::const_iterator vec=b.begin();
+        typename Vector1::iterator res=x.begin();
+        typename Matrix::const_iterator row=U.begin();
+
+            // Find last constrained values of x, U and b
+//         for( ; (res != x.end()) && (row != U.rowEnd()); ++res, ++row, ++vec) { }
+        size_t last = x.size();
+        if( b.size() < last ) last = b.size();
+        res += last;
+        row += last;
+        vec += last;
+
+        typename VectorTraits<Vector2>::VectorCategory V2VC;
+        VectorCategories::SparseZeroOneVectorTag SZOtag;
+        
+        bool consistant = true;
+        for(typename Vector2::const_iterator bcheck=vec; bcheck != b.end(); ++bcheck) {
+            if( ! F.isZero(*bcheck) ) {
+                consistant = false;
+                break;
+            }
+        }
+        if (consistant) {
+            --vec; --res; --row;
+            
+            VectorDomain<Field> VD(F);
+            for( ; row != U.begin(); --row, --vec, --res) {
+                F.init(*res, 0UL);
+                if (row->size()) {
+                    typename Field::Element tmp;
+                    VD.dotSpecialized(tmp, *row, x, SZOtag, V2VC);
+                    F.addin(tmp,*vec);
+                    F.assign(*res,tmp);
+                } else {
+                        // Consistency check
+                    if( ! F.isZero(*vec) ) {
+                        consistant = false;
+                        break;
+                    }
+                }
+            }
+            
+            F.init(*res, 0UL);
+            if (row->size()) {
+                typename Field::Element tmp;
+                VD.dotSpecialized(tmp, *row, x, SZOtag, V2VC);
+                F.addin(tmp,*vec);
+                F.assign(*res,tmp);
+            } else {
+                    // Consistency check
+                if( ! F.isZero(*vec) ) consistant = false;
+            }
+        }
+        if (! consistant) throw LinboxError ("upperTriangularSolve returned INCONSISTENT");
+        
+        commentator.stop ("done", NULL, "utrsm");
         return x;
     }
 
