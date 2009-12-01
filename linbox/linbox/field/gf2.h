e904a1eed673251d06e89b74cc052484cf064372
Jean-Guillaume Dumas <jgdumas@users.noreply.github.com>
2009-12-01 11:34:06 +0000

    specialization gf2 GivaroField

--- linbox/linbox/field/gf2.h
+++ linbox/linbox/field/gf2.h
@@ -582,6 +582,112 @@ class GF2 : public FieldInterface
 
 
 
+// Specialization of GivaroField for GF2
+#include "linbox/field/givaro-field.h"
+namespace LinBox 
+{
+
+  /** 
+  \brief give LinBox fields an allure of Givaro Fields
+  \ingroup field
+
+   *  This class adds the necessary requirements allowing 
+   *  the construction of an extension of a LinBox field.
+   */ 
+    template<>
+    struct GivaroField<LinBox::GF2> : public LinBox::GF2
+    {
+        typedef LinBox::GF2 BaseField;
+        typedef BaseField::Element TT;
+        typedef Signed_Trait<TT>::unsigned_type UTT;
+        typedef TT Rep;
+        typedef GivaroField<BaseField> Self_t;
+        typedef Rep Element;
+        typedef UTT Residu_t;
+
+        Element zero, one;
+        GivaroField(const BaseField& bf) : BaseField(bf) {
+            this->init(zero,0UL);
+            this->init(one, 1UL);
+        }
+
+
+            // -- amxy: r <- c - a * b mod p
+        Rep& amxy (Rep& r, const Rep a, const Rep b, const Rep c) const {
+            Rep tmp;
+            this->mul(tmp, a, b);
+            this->assign(r,c);
+            return this->subin(r,tmp);
+        }
+        std::_Bit_reference amxy (std::_Bit_reference r, const Rep a, const Rep b, const Rep c) const {
+            Rep tmp;
+            this->mul(tmp, a, b);
+            this->assign(r,c);
+            return this->subin(r,tmp);
+        }
+
+
+            // -- maxpy: r <- y - a * x
+        Rep& maxpy (Rep& r, const Rep a, const Rep x, const Rep y) const {
+            Rep tmp; this->mul(tmp, a, x);
+            return this->sub(r,y,tmp);
+        }
+        std::_Bit_reference maxpy (std::_Bit_reference r, const Rep a, const Rep x, const Rep y) const {
+            Rep tmp; this->mul(tmp, a, x);
+            return this->sub(r,y,tmp);
+        }
+            // -- axmyin: r <- r - a * x 
+        Rep& axmyin (Rep& r, const Rep a, const Rep x) const {
+            Rep tmp; this->mul(tmp, a, x);
+            return this->subin(r,tmp);
+        }
+        std::_Bit_reference axmyin (std::_Bit_reference r, const Rep a, const Rep x) const {
+            Rep tmp; this->mul(tmp, a, x);
+            return this->subin(r,tmp);
+        }
+            // -- maxpyin: r <- r - a * x
+        Rep& maxpyin (Rep& r, const Rep a, const Rep x) const {
+	    return axmyin(r,a,x);
+        }
+        std::_Bit_reference maxpyin (std::_Bit_reference r, const Rep a, const Rep x) const {
+	    return axmyin(r,a,x);
+        }
+
+ 
+
+        bool areNEqual ( const Rep a, const Rep b) const {
+            return ! this->areEqual(a,b);
+        }
+
+            // Access to the modulus, characteristic, size, exponent
+        UTT residu() const { integer c; BaseField::characteristic(c); return UTT(c); }
+        UTT characteristic() const  { integer c; BaseField::characteristic(c); return UTT(c); }
+        UTT cardinality() const  { integer c; BaseField::cardinality(c); return UTT(c); }
+        UTT exponent() const { return 1; }
+        UTT size() const  { integer c; BaseField::cardinality(c); return UTT(c); }
+
+
+            // ----- random generators
+        template<class RandIter> Rep& random(RandIter& g, Rep& r) const { return r = g() ; }
+        template<class RandIter> Rep& random(RandIter& g, Rep& r, long s) const { return r = g() ; }
+        template<class RandIter> Rep& random(RandIter& g, Rep& r, const Rep& b) const { return r = g() ; }
+        template<class RandIter> Rep& nonzerorandom(RandIter& g, Rep& r) const { return r = g() ; }
+        template<class RandIter> Rep& nonzerorandom(RandIter& g, Rep& r, long s) const { return r = g() ; }
+        template<class RandIter> Rep& nonzerorandom(RandIter& g, Rep& r, const Rep& b) const { return r = g() ; }
+
+        template<class RandIter> std::_Bit_reference random(RandIter& g, std::_Bit_reference r) const { return r = g() ; }
+        template<class RandIter> std::_Bit_reference random(RandIter& g, std::_Bit_reference r, long s) const { return r = g() ; }
+        template<class RandIter> std::_Bit_reference random(RandIter& g, std::_Bit_reference r, const std::_Bit_reference b) const { return r = g() ; }
+        template<class RandIter> std::_Bit_reference nonzerorandom(RandIter& g, std::_Bit_reference r) const { return r = g() ; }
+        template<class RandIter> std::_Bit_reference nonzerorandom(RandIter& g, std::_Bit_reference r, long s) const { return r = g() ; }
+        template<class RandIter> std::_Bit_reference nonzerorandom(RandIter& g, std::_Bit_reference r, const Rep& b) const { return r = g() ; }
+        template<class RandIter> std::_Bit_reference nonzerorandom(RandIter& g, std::_Bit_reference r, const std::_Bit_reference b) const { return r = g() ; }
+
+    };
+    
+}
+
+
 
 // Specialization of homomorphism for basefield
 #include "linbox/field/hom.h"
