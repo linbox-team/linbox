264942d3e92c327e4c372e236d97689c67d2dffd
Jean-Guillaume Dumas <jgdumas@users.noreply.github.com>
2010-11-03 08:30:14 +0000

    unambigous #ifdef witj "lzz"

--- linbox/linbox/field/ntl-lzz_p.h
+++ linbox/linbox/field/ntl-lzz_p.h
@@ -10,8 +10,8 @@
  *
  */
 
-#ifndef __LINBOX_field_ntl_zz_p_H
-#define __LINBOX_field_ntl_zz_p_H
+#ifndef __LINBOX_field_ntl_lzz_p_H
+#define __LINBOX_field_ntl_lzz_p_H
 
 #include <NTL/lzz_p.h>
 #include <NTL/ZZ.h>
@@ -247,7 +247,7 @@ namespace LinBox
 
 } // namespace LinBox
 
-#endif // __LINBOX_field_ntl_zz_p_H
+#endif // __LINBOX_field_ntl_lzz_p_H
 
 /* -*- mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 // vim:sts=8:sw=8:ts=8:noet:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s:syntax=cpp.doxygen:foldmethod=syntax
