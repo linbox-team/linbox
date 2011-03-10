3d261d8e165e9e1c6a1bb6e5ee9361b22f9d9543
saunders <bdsaunders@users.noreply.github.com>
2011-03-10 21:14:28 +0000

    adjustments in tests

--- linbox/tests/checker.C
+++ linbox/tests/checker.C
@@ -31,7 +31,7 @@ struct counts {
 	int total() { return pass + buildfail + runfail; }
 };
 
-void build_n_run(string s, counts& cnt, int flag = 2) ;
+void build_n_run(string s, counts& cnt, int flag = 2, string r = "") ;
 void no_build_n_run(string s, counts& cnt, int flag = 2, string r = "") ;
 
 int main(int argc, char* argv[]){
@@ -85,17 +85,22 @@ build |wc" should yield the same number of lines.
 //BASIC_TESTS
 	build_n_run("test-bitonic-sort", counter, flag);
 	build_n_run("test-blas-domain", counter, flag);
+	build_n_run("test-block-ring", counter, flag);
 	build_n_run("test-butterfly", counter, flag);
+	build_n_run("test-charpoly", counter, flag);//, "intermittent inf loop, bb or cp responsible?");
 	build_n_run("test-commentator", counter, flag);
 	build_n_run("test-companion", counter, flag);
+	build_n_run("test-cra", counter, flag);
+	build_n_run("test-cradomain", counter, flag);
 	build_n_run("test-dense", counter, flag);
 	build_n_run("test-det", counter, flag);
 	build_n_run("test-diagonal", counter, flag);
 	build_n_run("test-dif", counter, flag);
 	build_n_run("test-direct-sum", counter, flag);
-	build_n_run("test-ftrmm", counter, flag);
+	no_build_n_run("test-dyadic-to-rational", counter, flag, "bds responsible");
 	build_n_run("test-ffpack", counter, flag);
 	build_n_run("test-frobenius", counter, flag);
+	build_n_run("test-ftrmm", counter, flag);
 	build_n_run("test-getentry", counter, flag);
 	build_n_run("test-gf2", counter, flag);
 	build_n_run("test-gmp-rational", counter, flag);
@@ -104,6 +109,7 @@ build |wc" should yield the same number of lines.
 	build_n_run("test-inverse", counter, flag);
 	build_n_run("test-isposdef", counter, flag);
 	build_n_run("test-ispossemidef", counter, flag);
+	build_n_run("test-last-invariant-factor", counter, flag);
 	build_n_run("test-matrix-domain", counter, flag);
 	build_n_run("test-matrix-stream", counter, flag);
 	build_n_run("test-minpoly", counter, flag);
@@ -113,35 +119,30 @@ build |wc" should yield the same number of lines.
 	build_n_run("test-modular-double", counter, flag);
 	build_n_run("test-modular-int", counter, flag);
 	build_n_run("test-modular-short", counter, flag);
-	build_n_run("test-block-ring", counter, flag);
 	build_n_run("test-moore-penrose", counter, flag);
+	build_n_run("test-nullspace", counter, flag);
+	build_n_run("test-qlup", counter, flag);
 	build_n_run("test-randiter-nonzero", counter, flag);
 	build_n_run("test-rank", counter, flag);
 	build_n_run("test-rational-matrix-factory ", counter, flag);
+	build_n_run("test-rational-reconstruction-base", counter, flag);
+	build_n_run("test-rat-charpoly", counter, flag);//, "infinite loop, cp responsible?");
 	build_n_run("test-scalar-matrix", counter, flag);
+	build_n_run("test-smith-form", counter, flag);
+	build_n_run("test-smith-form-adaptive", counter, flag);
+	build_n_run("test-smith-form-binary", counter, flag);
+	build_n_run("test-smith-form-iliopoulos", counter, flag);
+	build_n_run("test-solve", counter, flag);
 	build_n_run("test-sparse", counter, flag);
 	build_n_run("test-subiterator", counter, flag);
 	build_n_run("test-submatrix", counter, flag);
 	build_n_run("test-subvector", counter, flag);
+	build_n_run("test-sum", counter, flag);
+	build_n_run("test-rational-solver", counter, flag);
 	build_n_run("test-trace", counter, flag);
 	build_n_run("test-triplesbb", counter, flag);
 	build_n_run("test-vector-domain", counter, flag);
-	build_n_run("test-qlup", counter, flag);
 	build_n_run("test-zero-one", counter, flag);
-	build_n_run("test-solve", counter, flag);
-	build_n_run("test-nullspace", counter, flag);
-	build_n_run("test-sum", counter, flag);
-	build_n_run("test-smith-form-iliopoulos", counter, flag);
-	build_n_run("test-smith-form", /*"make fails, bds responsible", */counter, flag);
-	build_n_run("test-smith-form-local", counter, flag); //"intermittent failures");
-	build_n_run("test-smith-form-adaptive", counter, flag);
-	build_n_run("test-smith-form-binary", counter, flag);
-	build_n_run("test-rational-solver", counter, flag);
-	build_n_run("test-dyadic-to-rational", counter, flag);
-	build_n_run("test-last-invariant-factor", counter, flag);
-	build_n_run("test-cra", counter, flag);
-	build_n_run("test-cradomain", counter, flag);
-	build_n_run("test-rational-reconstruction-base", counter, flag);
 
 #if __LINBOX_HAVE_GIVARO
 //	if (flag > 0) cout << "	Givaro tests" << endl;
@@ -159,32 +160,32 @@ build |wc" should yield the same number of lines.
 	if (flag > 0) cout << "	Lapack dependent tests" << endl;
 	build_n_run("test-rational-solver-adaptive", counter, flag);
 	// needs output cleanup.  Resolve whether a benchmark or a test.
-	build_n_run("test-solve-nonsingular", counter, flag);
+	no_build_n_run("test-solve-nonsingular", counter, flag, "bds responsible");
 #else
 	if (flag > 0) cout << "	not doing Lapack dependent tests" << endl;
 	no_build_n_run("test-rational-solver-adaptive", counter, flag);
 	// needs output cleanup.  Resolve whether a benchmark or a test.
-	no_build_n_run("test-solve-nonsingular", counter, flag);
+	no_build_n_run("test-solve-nonsingular", counter, flag, "bds responsible");
 #endif
 
 #if __LINBOX_HAVE_NTL
 	if (flag > 0) cout << "	NTL dependent tests" << endl;
-	build_n_run("test-ntl-lzz_p", counter, flag);
-	build_n_run("test-ntl-ZZ_p", counter, flag);
-	build_n_run("test-ntl-toeplitz", counter, flag);
 	build_n_run("test-ntl-hankel", counter, flag);
+	build_n_run("test-ntl-lzz_p", counter, flag);
+	build_n_run("test-ntl-toeplitz", counter, flag);
 	build_n_run("test-ntl-sylvester", counter, flag);
-	build_n_run("test-toeplitz-det", counter, flag);
 	build_n_run("test-ntl-RR", counter, flag);
+	build_n_run("test-ntl-ZZ_p", counter, flag);
+	build_n_run("test-toeplitz-det", counter, flag);
 #else
 	if (flag > 0) cout << "	not doing NTL dependent tests" << endl;
-	no_build_n_run("test-ntl-lzz_p", counter, flag);
-	no_build_n_run("test-ntl-ZZ_p", counter, flag);
-	no_build_n_run("test-ntl-toeplitz", counter, flag);
 	no_build_n_run("test-ntl-hankel", counter, flag);
-	no_build_n_run("test-ntl-sylvester", counter, flag);
-	no_build_n_run("test-toeplitz-det", counter, flag);
+	no_build_n_run("test-ntl-lzz_p", counter, flag);
+	no_build_n_run("test-ntl-toeplitz", counter, flag);
 	no_build_n_run("test-ntl-RR", counter, flag);
+	no_build_n_run("test-ntl-sylvester", counter, flag);
+	no_build_n_run("test-ntl-ZZ_p", counter, flag);
+	no_build_n_run("test-toeplitz-det", counter, flag, "can we have non NTL version?");
 #endif
 
 #if __LINBOX_HAVE_LIDIA
@@ -200,7 +201,7 @@ build |wc" should yield the same number of lines.
 	build_n_run("./test-optimization", counter, flag);
 #else
 	if (flag > 0) cout << "	not doing Atlas dependent test" << endl;
-	no_build_n_run("test-optimization", counter, flag);
+	no_build_n_run("test-optimization", counter, flag, "?");
 #endif
 
 	// tests of "doubles as a field" which doesn't adhere to LinBox field or ring specs.
@@ -209,16 +210,13 @@ build |wc" should yield the same number of lines.
 	build_n_run("test-param-fuzzy", counter, flag);
 
 #if 1
-	if (flag > 0) cout << "	Noncompiling tests" << endl;
-
-	if (flag > 0) cout << "	Failing tests" << endl;
-	no_build_n_run("test-charpoly", counter, flag, "intermittent inf loop, bb or cp responsible?");
-	no_build_n_run("test-rat-charpoly", counter, flag, "infinite loop, cp responsible?");
+	if (flag > 0) cout << "	Tests requiring further development" << endl;
+	build_n_run("test-smith-form-local", counter, flag, "bds"); //"intermittent failures");
 
 	if (flag > 0) cout << "	Immature tests" << endl;
 	no_build_n_run("test-quad-matrix", counter, flag, "half baked, bds responsible");
 	no_build_n_run("test-dense-zero-one", counter, flag, "half baked, bds responsible");
-	no_build_n_run("test-zo", counter, flag, "half baked, bds responsible");
+	build_n_run("test-zo", counter, flag, "half baked, BY responsible");
 	// test-integer-tools -- there is no test-integer-tools.C file
 	// no one has taken these on.
 	//no_build_n_run("test-mg-block-lanczos", counter, flag, "make fails, nobody responsible");
@@ -273,7 +271,8 @@ void no_build_n_run(string s, counts& cnt, int flag, string r) {
 	cnt.skipped++;
 }
 
-void build_n_run(string s, counts& cnt, int flag) {
+void build_n_run(string s, counts& cnt, int flag, string r) {
+// ignore r.
 	string cmd;
 	if (flag >= 5) { // force build
 		flag -= 5;
