60a9c384d03adf41b0173d0dcf0e2938daacc76d
bboyer <bboyer@8563fe0b-bd16-0410-b45f-99c24e105077>
2011-05-10 15:52:05 +0000

    harmless auto-generated modifications

--- linbox/linbox/ring/gcd32.h
+++ linbox/linbox/ring/gcd32.h
@@ -31,119 +31,150 @@ template<typename Ints> Ints GCD2E32( const Ints p)
 				if( p & 3UL) {
 					if( p & 1UL) {
 						return 1UL;
-					} else {
+					}
+					else {
 						return 2UL;
 					}
-				} else {
+				}
+				else {
 					if( p & 7UL) {
 						return 4UL;
-					} else {
+					}
+					else {
 						return 8UL;
 					}
 				}
-			} else {
+			}
+			else {
 				if( p & 63UL) {
 					if( p & 31UL) {
 						return 16UL;
-					} else {
+					}
+					else {
 						return 32UL;
 					}
-				} else {
+				}
+				else {
 					if( p & 127UL) {
 						return 64UL;
-					} else {
+					}
+					else {
 						return 128UL;
 					}
 				}
 			}
-		} else {
+		}
+		else {
 			if( p & 4095UL) {
 				if( p & 1023UL) {
 					if( p & 511UL) {
 						return 256UL;
-					} else {
+					}
+					else {
 						return 512UL;
 					}
-				} else {
+				}
+				else {
 					if( p & 2047UL) {
 						return 1024UL;
-					} else {
+					}
+					else {
 						return 2048UL;
 					}
 				}
-			} else {
+			}
+			else {
 				if( p & 16383UL) {
 					if( p & 8191UL) {
 						return 4096UL;
-					} else {
+					}
+					else {
 						return 8192UL;
 					}
-				} else {
+				}
+				else {
 					if( p & 32767UL) {
 						return 16384UL;
-					} else {
+					}
+					else {
 						return 32768UL;
 					}
 				}
 			}
 		}
-	} else {
+	}
+	else {
 		if( p & 16777215UL) {
 			if( p & 1048575UL) {
 				if( p & 262143UL) {
 					if( p & 131071UL) {
 						return 65536UL;
-					} else {
+					}
+					else {
 						return 131072UL;
 					}
-				} else {
+				}
+				else {
 					if( p & 524287UL) {
 						return 262144UL;
-					} else {
+					}
+					else {
 						return 524288UL;
 					}
 				}
-			} else {
+			}
+			else {
 				if( p & 4194303UL) {
 					if( p & 2097151UL) {
 						return 1048576UL;
-					} else {
+					}
+					else {
 						return 2097152UL;
 					}
-				} else {
+				}
+				else {
 					if( p & 8388607UL) {
 						return 4194304UL;
-					} else {
+					}
+					else {
 						return 8388608UL;
 					}
 				}
 			}
-		} else {
+		}
+		else {
 			if( p & 268435455UL) {
 				if( p & 67108863UL) {
 					if( p & 33554431UL) {
 						return 16777216UL;
-					} else {
+					}
+					else {
 						return 33554432UL;
 					}
-				} else {
+				}
+				else {
 					if( p & 134217727UL) {
 						return 67108864UL;
-					} else {
+					}
+					else {
 						return 134217728UL;
 					}
 				}
-			} else {
+			}
+			else {
 				if( p & 1073741823UL) {
 					if( p & 536870911UL) {
 						return 268435456UL;
-					} else {
+					}
+					else {
 						return 536870912UL;
 					}
-				} else {
+				}
+				else {
 					if( p & 2147483647UL) {
 						return 1073741824UL;
-					} else {
+					}
+					else {
 						return 2147483648UL;
 					}
 				}
