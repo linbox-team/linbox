935dcd37ef671979441eaa325a892508cd3ba6ff
bboyer <bboyer@8563fe0b-bd16-0410-b45f-99c24e105077>
2011-05-22 08:37:56 +0000

    misc

--- linbox/template.h
+++ linbox/template.h
@@ -1,5 +1,5 @@
 /* -*- mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
-// vim:sts=8:sw=8:ts=8:noet:sr:cino=g0,t0,\:0
+// vim:sts=8:sw=8:ts=8:noet:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s
 /* Copyright (C) <+year+> LinBox
  * Written by <+someone+> <<+her.mail@somewhere.net+>>
  *
