6bf028b9e839756ca2e6425c75342f4af07295c1
giorgi <giorgi@8563fe0b-bd16-0410-b45f-99c24e105077>
2011-10-08 18:59:03 +0000

    make block-wiedemann non singular solver works

--- linbox/linbox/algorithms/sigma-basis.h
+++ linbox/linbox/algorithms/sigma-basis.h
@@ -3,7 +3,7 @@
 /* linbox/algorithms/sigma-basis.h
  * Copyright (C) 2005 Pascal Giorgi
  *
- * Written by Pascal Giorgi pgiorgi@uwaterlo.ca
+ * Written by Pascal Giorgi pascal.giorgi@lirmm.fr
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
