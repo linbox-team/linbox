ea6fd3456a1db676df5af4efe639ed1b21be6e45
bboyer <bboyer@8563fe0b-bd16-0410-b45f-99c24e105077>
2012-01-12 14:07:51 +0000

    licence change/update/correct (3/n)

--- trunk/linbox/linbox/algorithms/whisart_trace.h
+++ trunk/linbox/linbox/algorithms/whisart_trace.h
@@ -6,10 +6,13 @@
  * Time-stamp: <19 Apr 10 18:35:14 Jean-Guillaume.Dumas@imag.fr>
  * Computes the trace of D1 B D2 B^T D1 or D1 B^T D2 B D1
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
+ * ========LICENCE========
+ * This file is part of the library LinBox.
+ *
+  * LinBox is free software: you can redistribute it and/or modify
+ * it under the terms of the  GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,9 +20,9 @@
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
- * Boston, MA 02110-1301, USA.
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * ========LICENCE========
  */
 #ifndef __LINBOX_whisart_trace_H
 #define __LINBOX_whisart_trace_H
