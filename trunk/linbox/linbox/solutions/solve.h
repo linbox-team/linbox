c4e79aa1798ce651786c1b910138ef79882023b1
bboyer <bboyer@8563fe0b-bd16-0410-b45f-99c24e105077>
2012-01-12 23:15:20 +0000

    oops, this shouldn't have been pushed in..

--- trunk/linbox/linbox/solutions/solve.h
+++ trunk/linbox/linbox/solutions/solve.h
@@ -4,20 +4,20 @@
  * Copyright(C) LinBox
  *  Evolved from an earlier one by Bradford Hovinen <hovinen@cis.udel.edu>
  *
- * 
+ *
  * ========LICENCE========
  * This file is part of the library LinBox.
- * 
+ *
  * LinBox is free software: you can redistribute it and/or modify
  * it under the terms of the  GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -48,7 +48,7 @@
 #include "linbox/algorithms/varprec-cra-early-multip.h"
 
 #ifdef __LINBOX_HAVE_IML
-#include "linbox/utils/iml_wrapper.h"
+#include "linbox/util/iml_wrapper.h"
 #endif
 
 namespace LinBox
