c1b3eddac38ecf33b7f2e9ff005c77663a2a0ab4
bboyer <bboyer@8563fe0b-bd16-0410-b45f-99c24e105077>
2012-01-12 14:07:51 +0000

    licence change/update/correct (3/n)

--- linbox/linbox/algorithms/cra-domain-omp.h
+++ linbox/linbox/algorithms/cra-domain-omp.h
@@ -8,10 +8,13 @@
  * Then synchronization and termintation test.
  * Time-stamp: <16 Jul 10 16:59:17 Jean-Guillaume.Dumas@imag.fr>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
+ * ========LICENCE========
+ * This file is part of the library LinBox.
+ *
+  * LinBox is free software: you can redistribute it and/or modify
+ * it under the terms of the  GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -19,9 +22,9 @@
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
- * Boston, MA 02110-1301, USA.
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * ========LICENCE========
  */
 
 /*! @file algorithms/cra-domain-omp.h
--- linbox/linbox/field/modular.h
+++ linbox/linbox/field/modular.h
@@ -32,7 +32,7 @@
  * Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
+ * License along with this library; if not, write to the Free Software Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  * ========LICENCE========
  *.
