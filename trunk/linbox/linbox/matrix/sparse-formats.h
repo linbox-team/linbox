5758a34dd0bcb04834d8969833bc19cd38a7c239
bboyer <bboyer@8563fe0b-bd16-0410-b45f-99c24e105077>
2013-06-07 17:24:14 +0000

    works best if the file exists

---
+++ trunk/linbox/linbox/matrix/sparse-formats.h
@@ -0,0 +1,64 @@
+/* linbox/matrix/sparse-formats.h
+ * Copyright (C) 2013 the LinBox group
+ *
+ * Written by BB <bbboyer@ncsu.edu>,
+ *
+ *
+ * ========LICENCE========
+ * This file is part of the library LinBox.
+ *
+ * LinBox is free software: you can redistribute it and/or modify
+ * it under the terms of the  GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * ========LICENCE========
+ *.
+ */
+
+#ifndef __LINBOX_matrix_sparse_formats_H
+#define __LINBOX_matrix_sparse_formats_H
+
+namespace LinBox {
+
+	/// tags for SparseMatrixBase::read() and write()
+	enum FileFormatTag {
+		FORMAT_DETECT,
+		FORMAT_GUILLAUME,
+		FORMAT_TURNER,
+		FORMAT_MATLAB,
+		FORMAT_MAPLE,
+		FORMAT_PRETTY,
+		FORMAT_MAGMACPT,
+		FORMAT_ONE_BASED
+	};
+
+	// add here the sparse matrix types
+
+	namespace SparseMatrix2Format {
+		class ANY {} ;
+		class COO : public ANY {} ; // Cordinate
+		class CSR : public ANY {} ; // compressed row
+		class ELL : public ANY {} ; // ellpack
+		class HYB : public ANY {} ; // hybrid
+		// class TPL : public ANY {} ; // triples
+	} // SparseMatrix2Format
+
+	namespace SparseFileFormat {
+		class SMS {} ; // JG format
+		class CSR {} ; // Raleigh 11 format
+		class COO {} ; // more standard coo format
+	} // SparseFileFormat
+}
+
+#endif
+
+
