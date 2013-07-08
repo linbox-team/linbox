da54c223a443c8388a7bf517aead98548dc0c6f8
stachnik <stachnik@8563fe0b-bd16-0410-b45f-99c24e105077>
2013-07-08 19:01:54 +0000

    triplesbb-omp fixes:  - Switched omp-benchmark to use omp_get_wtime() as timer  - Fixed bug causing improper sort in triplesbb-omp  - Templated triplesbb-omp on MatrixDomain instead of Field  - Expanded triplesbb-omp tests  - Added 64-bit size_t support to triplesbb-omp blocking

--- linbox/linbox/blackbox/triplesbb-omp.h
+++ linbox/linbox/blackbox/triplesbb-omp.h
@@ -44,6 +44,9 @@ using std::max;
 
 #include <vector>
 
+// TODO: Grab this from configure, or implement it in a smarter way
+#define USING_64_BIT_SIZE_T 1
+
 namespace LinBox
 {
 
@@ -52,11 +55,13 @@ namespace LinBox
  \ingroup blackbox
  * Sparse matrix representation which stores nonzero entries by i,j,value triples.
  */
-template<class Field_>
+template<class MatDom>
 class TriplesBBOMP : public BlackboxInterface {
 
 	public:
-	typedef Field_ Field;
+        typedef MatDom MatrixDomain;
+	typedef typename MatrixDomain::Field Field;
+	typedef typename MatrixDomain::Submatrix Matrix;
 	typedef typename Field::Element Element;
 	typedef TriplesBBOMP<Field> Self_t;
 	typedef size_t Index; // would prefer a signed type
@@ -103,6 +108,9 @@ class TriplesBBOMP : public BlackboxInterface {
 	template<class OutVector, class InVector>
 	OutVector & seqApply(OutVector &, const InVector &) const;
 
+	/// Y <- AX, requires conformal shapes.
+	Matrix & apply_right(Matrix &Y, const Matrix &X) const;
+
 	/** y <- A^T x.
 	 *
 	 *  Performance will be better if A is in rowMajor or colMajor order.
@@ -124,6 +132,8 @@ class TriplesBBOMP : public BlackboxInterface {
 	size_t coldim() const;
 
 	const Field & field() const;
+
+        const MatrixDomain & domain() const;
 
 	/* Returns number of non-zero entries */
 	size_t size() const;
@@ -151,73 +161,119 @@ class TriplesBBOMP : public BlackboxInterface {
 
 	protected:
 	const Field *field_; // The field used by this class
+        MatrixDomain MD_;
 
-	union Coord {
-		Index rowCol[2];
-		Llong blockIx;
-	};
+        union Coord {
+                Index rowCol[2];
+#ifdef USING_64_BIT_SIZE_T
+                Llong blockIxArr[2]; // little endian
+#endif
+                Llong blockIx;
+        };
 
-	struct Triple {
-		union Coord coord;
-		Element elt;
-		Triple(Index& r, Index& c, const Element& e)
-		{ init(r, c, e); }
-		void init(Index r, Index c, const Element& e)
-		{
-			row()=r;
-			col()=c;
-			elt = e;
-		}
-		inline Index& row() {return coord.rowCol[0];}
-		inline Index& col() {return coord.rowCol[1];}
-		inline const Index& getRow() const {return coord.rowCol[0];}
-		inline const Index& getCol() const {return coord.rowCol[1];}
-		inline Llong& blockIx() {return coord.blockIx;}
-		static Llong toBlockIx (Index row, Index col)
-		{
-			Llong temp,final;
-			final=(((Llong)row)<<32)|((Llong)col);
-			temp=(final^(final>>16))&0x00000000FFFF0000;
-			final^=temp^(temp<<16);
-			temp=(final^(final>>8))&0x0000FF000000FF00;
-			final^=temp^(temp<<8);
-			temp=(final^(final>>4))&0x00F000F000F000F0;
-			final^=temp^(temp<<4);
-			temp=(final^(final>>2))&0x0C0C0C0C0C0C0C0C;
-			final^=temp^(temp<<2);
-			temp=(final^(final>>1))&0x2222222222222222;
-			final^=temp^(temp<<1);
-			return final;
-		}
-		void toBlock()
-		{
-			blockIx()=toBlockIx(row(),col());
-		}
-		static Llong fromBlockIx(Llong final)
-		{
-			Llong temp;
-			temp=(final^(final>>1))&0x2222222222222222;
-			final^=temp^(temp<<1);
-			temp=(final^(final>>2))&0x0C0C0C0C0C0C0C0C;
-			final^=temp^(temp<<2);
-			temp=(final^(final>>4))&0x00F000F000F000F0;
-			final^=temp^(temp<<4);
-			temp=(final^(final>>8))&0x0000FF000000FF00;
-			final^=temp^(temp<<8);
-			temp=(final^(final>>16))&0x00000000FFFF0000;
-			final^=temp^(temp<<16);
-			return final;
-		}
-		void fromBlock()
-		{
-			Llong final=fromBlockIx(blockIx());
-			row()=(Index)((final>>32)&0xFFFFFFFF);
-			col()=(Index)(final&0xFFFFFFFF);
-		}
-		static bool compareBlockTriples(const Triple& left, const Triple& right) {
-			return left.coord.blockIx<right.coord.blockIx;
-		}
-	};
+        struct Triple {
+                union Coord coord;
+                Element elt;
+                Triple(Index& r, Index& c, const Element& e)
+                { init(r, c, e); }
+                void init(Index r, Index c, const Element& e)
+                {
+                        row()=r;
+                        col()=c;
+                        elt = e;
+                }
+                inline Index& row() {return coord.rowCol[0];}
+                inline Index& col() {return coord.rowCol[1];}
+                inline const Index& getRow() const {return coord.rowCol[0];}
+                inline const Index& getCol() const {return coord.rowCol[1];}
+                inline Llong& blockIx() {return coord.blockIx;}
+                void toBlock()
+                {
+                        Llong temp,final;
+                        Index localRow=row(),localCol=col();
+#ifdef USING_64_BIT_SIZE_T
+                        Index highOrderLocalRow=localRow&(~0xFFFFFFFF);
+                        Index highOrderLocalCol=localCol&(~0xFFFFFFFF);
+                        Llong highOrderFinal;
+                        coord.blockIxArr[1]=0;
+                        if ((highOrderLocalRow != 0) || (highOrderLocalCol != 0)) {
+                                highOrderFinal=((Llong)localRow)|(((Llong)localCol)>>32);
+                                temp=(highOrderFinal^(highOrderFinal>>16))&0x00000000FFFF0000;
+                                highOrderFinal^=temp^(temp<<16);
+                                temp=(highOrderFinal^(highOrderFinal>>8))&0x0000FF000000FF00;
+                                highOrderFinal^=temp^(temp<<8);
+                                temp=(highOrderFinal^(highOrderFinal>>4))&0x00F000F000F000F0;
+                                highOrderFinal^=temp^(temp<<4);
+                                temp=(highOrderFinal^(highOrderFinal>>2))&0x0C0C0C0C0C0C0C0C;
+                                highOrderFinal^=temp^(temp<<2);
+                                temp=(highOrderFinal^(highOrderFinal>>1))&0x2222222222222222;
+                                highOrderFinal^=temp^(temp<<1);
+                                coord.blockIxArr[1]=highOrderFinal;
+                        }
+                        localCol&=0xFFFFFFFF;
+                        localRow&=0xFFFFFFFF;
+#endif
+                        final=(((Llong)localRow)<<32)|((Llong)localCol);
+                        temp=(final^(final>>16))&0x00000000FFFF0000;
+                        final^=temp^(temp<<16);
+                        temp=(final^(final>>8))&0x0000FF000000FF00;
+                        final^=temp^(temp<<8);
+                        temp=(final^(final>>4))&0x00F000F000F000F0;
+                        final^=temp^(temp<<4);
+                        temp=(final^(final>>2))&0x0C0C0C0C0C0C0C0C;
+                        final^=temp^(temp<<2);
+                        temp=(final^(final>>1))&0x2222222222222222;
+                        final^=temp^(temp<<1);
+                        coord.blockIx=final;
+                }
+
+                void fromBlock()
+                {
+                        Llong temp,final;
+                        Index localRow=0,localCol=0;
+#ifdef USING_64_BIT_SIZE_T
+                        if (coord.blockIxArr[1]!=0) {
+                                final=coord.blockIxArr[1];
+                                temp=(final^(final>>1))&0x2222222222222222;
+                                final^=temp^(temp<<1);
+                                temp=(final^(final>>2))&0x0C0C0C0C0C0C0C0C;
+                                final^=temp^(temp<<2);
+                                temp=(final^(final>>4))&0x00F000F000F000F0;
+                                final^=temp^(temp<<4);
+                                temp=(final^(final>>8))&0x0000FF000000FF00;
+                                final^=temp^(temp<<8);
+                                temp=(final^(final>>16))&0x00000000FFFF0000;
+                                final^=temp^(temp<<16);
+                                localRow=(Index)(final&(~0xFFFFFFFF));
+                                localCol=(Index)((final<<32)&(~0xFFFFFFFF));
+                        }
+#endif
+                        final=coord.blockIx;
+                        temp=(final^(final>>1))&0x2222222222222222;
+                        final^=temp^(temp<<1);
+                        temp=(final^(final>>2))&0x0C0C0C0C0C0C0C0C;
+                        final^=temp^(temp<<2);
+                        temp=(final^(final>>4))&0x00F000F000F000F0;
+                        final^=temp^(temp<<4);
+                        temp=(final^(final>>8))&0x0000FF000000FF00;
+                        final^=temp^(temp<<8);
+                        temp=(final^(final>>16))&0x00000000FFFF0000;
+                        final^=temp^(temp<<16);
+                        localRow|=(Index)((final>>32)&0xFFFFFFFF);
+                        localCol|=(Index)(final&0xFFFFFFFF);
+                        row()=localRow;
+                        col()=localCol;
+                }
+                static bool compareBlockTriples(const Triple& left, const Triple& right) {
+#ifdef USING_64_BIT_SIZE_T
+                        return (left.coord.blockIxArr[1]<right.coord.blockIxArr[1])||
+                                ((!(left.coord.blockIxArr[1]>right.coord.blockIxArr[1])) &&
+                                 (left.coord.blockIxArr[0]<right.coord.blockIxArr[0]));
+#else
+                        return left.coord.blockIx<right.coord.blockIx;
+#endif
+                }
+        };
 
 	// the data
 	std::vector<Triple> data_;
@@ -225,7 +281,6 @@ class TriplesBBOMP : public BlackboxInterface {
 	/// The number of rows, columns
 	Index rows_, cols_;
 
-        
         #define TRIPLES_UNSORTED 1
         #define TRIPLES_ROW_MAJOR_SORT 2
         #define TRIPLES_COL_MAJOR_SORT 4
