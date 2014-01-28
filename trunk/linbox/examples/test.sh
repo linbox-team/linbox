608414a45198a13b8907d1854cccfc5dd9ca4f10
bboyer <bboyer@8563fe0b-bd16-0410-b45f-99c24e105077>
2014-01-28 20:11:12 +0000

    revive test

--- trunk/linbox/examples/test.sh
+++ trunk/linbox/examples/test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash - 
+#!/bin/bash 
 
 # written by bb <bbboyer@ncsu.edu> , part of LinBox, see COPYING
 
@@ -13,19 +13,19 @@ success() {
 }
 
 
-pass=true
+pass="true"
 
 echo -n "check rank ... "
 rank_cmd="Rank\sis\s"
 ./rank  data/test.matrix 7 > linbox-tmp.data
 result=`cat linbox-tmp.data | grep ${rank_cmd} | sed 's/'"$rank_cmd"'\([0-9]*\).*/\1/'`
-[ "${result}" -eq "9" ] && success || { fail ;  pass=false ; }
+[ "$result" -eq "9" ] && success || { fail ;  pass="false" ; }
 
 echo -n "check rank ... "
 ./rank  data/test.matrix > linbox-tmp.data
 result=`cat linbox-tmp.data | grep ${rank_cmd} | sed 's/'"$rank_cmd"'\([0-9]*\).*/\1/'`
-[ "${result}" -eq "10" ] && success || { fail ; pass=false ; }
+[ "$result" -eq "10" ] && success || { fail ; pass="false" ; }
 
 
-[ "${pass}" -eq "true" ] && { echo -e "\nSUCCESS" ; exit 0  ;} || { echo -e "\nFAIL" ; exit 1 ;};
+[ "$pass" == "true" ] && { success ; exit 0  ;} || { fail ; exit 1 ;};
 
