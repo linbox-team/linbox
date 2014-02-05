de0abc4820022d007e94bb67f9059f3c912afc6c
bboyer <bboyer@8563fe0b-bd16-0410-b45f-99c24e105077>
2014-02-05 22:46:24 +0000

    weird

--- linbox/tests/test-transpose.C
+++ linbox/tests/test-transpose.C
@@ -218,11 +218,10 @@ int main (int argc, char **argv)
 	return pass ? 0 : -1;
 }
 
-// vim:sts=8:sw=8:ts=8:noet:sr:cino=>s,f0,{0,g0,(0,:0,t0,+0,=s
 // Local Variables:
 // mode: C++
 // tab-width: 8
 // indent-tabs-mode: nil
 // c-basic-offset: 8
 // End:
-
+// vim:sts=8:sw=8:ts=8:noet:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s
