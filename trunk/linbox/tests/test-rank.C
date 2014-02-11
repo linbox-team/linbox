9a05e4f8eb267207323688cc4ebb9ae5a5d20d5b
bboyer <bboyer@8563fe0b-bd16-0410-b45f-99c24e105077>
2014-02-11 20:33:24 +0000

    COO is better

--- trunk/linbox/tests/test-rank.C
+++ trunk/linbox/tests/test-rank.C
@@ -73,8 +73,8 @@ bool testRankMethods(const Field &F, size_t n, unsigned int iterations, double s
 	 // typedef SparseMatrix<Field,SparseMatrixFormat::SparseSeq > Blackbox;
 	 // typedef SparseMatrix<Field,SparseMatrixFormat::SparsePar > Blackbox;
 	 // typedef SparseMatrix<Field,SparseMatrixFormat::SparseMap > Blackbox;
-	// typedef SparseMatrix<Field,SparseMatrixFormat::COO> Blackbox;
-	typedef SparseMatrix<Field,SparseMatrixFormat::CSR> Blackbox; // inf loop
+	typedef SparseMatrix<Field,SparseMatrixFormat::COO> Blackbox;
+	// typedef SparseMatrix<Field,SparseMatrixFormat::CSR> Blackbox; // inf loop
 	// typedef SparseMatrix<Field,SparseMatrixFormat::ELL> Blackbox;
 	// typedef SparseMatrix<Field,SparseMatrixFormat::ELL_R> Blackbox;
 	// typedef SparseMatrix<Field,SparseMatrixFormat::HYB> Blackbox;
