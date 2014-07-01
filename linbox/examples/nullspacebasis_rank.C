19ac871297cef5c11abb001bba12c66fdd7cd7af
mdutour <mdutour@8563fe0b-bd16-0410-b45f-99c24e105077>
2014-07-01 18:19:19 +0000

    Change for some clarity.
      Mathieu

--- linbox/examples/nullspacebasis_rank.C
+++ linbox/examples/nullspacebasis_rank.C
@@ -8,53 +8,56 @@ using namespace LinBox;
 
 int main (int argc, char **argv)
 {
+  if ( argc <  2 || argc > 4) {
+    std::cerr << "Usage to get a random null space basis over GF(p,k):  <matrix-file-in-SMS-format> p [k]" << std::endl;
+    return -1;
+  }
 
-	if ( argc <  2 || argc > 4) {
-		std::cerr << "Usage to get a random null space basis over GF(p,k):  <matrix-file-in-SMS-format> p [k]" << std::endl;
-		return -1;
-	}
+  std::ifstream input (argv[1]);
+  if (!input) {
+    std::cerr << "Error opening matrix file " << argv[1] << std::endl;
+    return -1;
+  }
+  int pVal = atoi(argv[2]);
+  int kVal = atoi(argv[3]);
 
-	std::ifstream input (argv[1]);
-	if (!input) { std::cerr << "Error opening matrix file " << argv[1] << std::endl; return -1; }
 
-	//typedef Modular<int> Field;
-	typedef GivaroGfq Field;
-	Field F(atoi(argv[2]),argc>3?atoi(argv[3]):1);
-	SparseMatrix<Field, SparseMatrixFormat::SparseSeq > A (F);
-	A.read (input);
-	std::cout << "A is " << A.rowdim() << " by " << A.coldim() << std::endl;
+  //typedef Modular<int> Field;
+  typedef GivaroGfq Field;
+  Field F(pVal, argc>3?kVal:1);
+  SparseMatrix<Field, SparseMatrixFormat::SparseSeq > A (F);
+  A.read (input);
+  std::cout << "A is " << A.rowdim() << " by " << A.coldim() << std::endl;
 
-    GaussDomain<Field> GD(F);
-    
-    typename Field::Element Det;
-    unsigned long Rank;
-    size_t Ni(A.rowdim()),Nj(A.coldim());
+  GaussDomain<Field> GD(F);
+  
+  typename Field::Element Det;
+  unsigned long Rank;
+  size_t Ni(A.rowdim()),Nj(A.coldim());
 
-    Permutation<Field> P((int)Nj,F);
+  Permutation<Field> P((int)Nj,F);
 
-    GD.InPlaceLinearPivoting(Rank, Det, A, P, Ni, Nj );
+  GD.InPlaceLinearPivoting(Rank, Det, A, P, Ni, Nj );
 
-    for(size_t i=0; i< Ni; ++i) {
-        if (A[i].size() == 0) {
-            size_t j(i);
-            if (nextnonzero(j,Ni,A)) {
-                A[i] = A[j];
-                A[j].resize(0);
-            }
-            else {
-                break;
-            }
-        }
+  for(size_t i=0; i< Ni; ++i) {
+    if (A[i].size() == 0) {
+      size_t j(i);
+      if (nextnonzero(j,Ni,A)) {
+	A[i] = A[j];
+	A[j].resize(0);
+      }
+      else {
+	break;
+      }
     }
-    size_t nullity = A.coldim()-Rank;
-    BlasMatrix<Field> NullSpace(F,A.coldim(),nullity);
-	GD.nullspacebasis(NullSpace, Rank, A, P);
+  }
+  size_t nullity = A.coldim()-Rank;
+  BlasMatrix<Field> NullSpace(F,A.coldim(),nullity);
+  GD.nullspacebasis(NullSpace, Rank, A, P);
 
-    NullSpace.write( std::cerr << "X:=", Tag::FileFormat::Maple ) << ';' << std::endl;
+  NullSpace.write( std::cerr << "X:=", Tag::FileFormat::Maple ) << ';' << std::endl;
     
-    std::cerr << "NullsSpace dimensions:" << NullSpace.rowdim() << 'x' << NullSpace.coldim() << std::endl;
+  std::cerr << "NullsSpace dimensions:" << NullSpace.rowdim() << 'x' << NullSpace.coldim() << std::endl;
     
-    return 0;
-    
-
+  return 0;
 }
