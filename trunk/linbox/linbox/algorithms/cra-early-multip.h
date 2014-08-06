65003fd1a9e353524a5787fb7dee9e8077070919
bboyer <bboyer@8563fe0b-bd16-0410-b45f-99c24e105077>
2014-08-06 09:00:26 +0000

    casts

--- trunk/linbox/linbox/algorithms/cra-early-multip.h
+++ trunk/linbox/linbox/algorithms/cra-early-multip.h
@@ -59,7 +59,7 @@ namespace LinBox
 		// of the elements to be reconstructed
 		std::vector< unsigned long >      	randv;
 
-		Integer& result(Integer &d) ; // DON'T TOUCH
+		Integer& result(Integer &d) { std::cout << "should not be called" << std::endl; } ; // DON'T TOUCH
 	public:
 
 		EarlyMultipCRA(const unsigned long EARLY=DEFAULT_EARLY_TERM_THRESHOLD) :
