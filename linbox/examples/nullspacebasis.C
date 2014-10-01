8b7312b9bbcb490be2a5d714e582f1d14662cb73
Jean-Guillaume Dumas <jgdumas@users.noreply.github.com>
2014-10-01 15:07:39 +0000

    licence

--- linbox/examples/nullspacebasis.C
+++ linbox/examples/nullspacebasis.C
@@ -1,6 +1,34 @@
-#include <iostream>
+/*
+ * examples/nullspacebasis.C
+ *
+ * Copyright (C) 2014 J-G. Dumas
+ * ========LICENCE========
+ * This file is part of the library LinBox.
+ *
+ * LinBox is free software: you can redistribute it and/or modify
+ * it under the terms of the  GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * ========LICENCE========
+ */
 
-//#include "linbox/field/modular-int.h"
+/**\file examples/nullspacebasis.C
+ * @example  examples/nullspacebasis_rank.C
+  \brief NullSpace of sparse matrix over GFq.
+  \brief nullspace is allocated m \times n.
+  \ingroup examples
+  */
+
+#include <iostream>
 #include "linbox/field/Givaro/givaro-gfq.h"
 #include "linbox/algorithms/gauss.h"
 
