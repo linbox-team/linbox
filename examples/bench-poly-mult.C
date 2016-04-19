b1b20cceb3183c7f66f577570d8c2c1d226b1526
Pascal Giorgi <pascal.giorgi@lirmm.fr>
2016-04-19 10:00:00 +0200

    add update on pol mul bench

--- examples/bench-poly-mult.C
+++ examples/bench-poly-mult.C
@@ -99,6 +99,51 @@ void randomVect (Rand& r, Vect& v) {
 
 
 template<typename Field, typename RandIter>
+void check_pol_mulfft(const Field& fld,  RandIter& Gen, size_t d) {
+	typedef typename  Field::Element Element;
+	typedef PolynomialMatrix<PMType::polfirst,PMStorage::plain,Field> MatrixP;
+
+	std::vector<Element> A(d),B(d),C(2*d-1),D(2*d-1,0);
+	// Generate random matrix of polynomial
+	randomVect(Gen,A);
+	randomVect(Gen,B);
+
+	std::cout<<"degree= "<<d<<" "<<std::endl;
+	typedef PolynomialFFTMulDomain<Field>    FFT;
+	FFT  PMFFT(fld);
+	myTimer chrono;
+	//size_t mmul=2*n*n*n;
+	//size_t madd=n*n;
+	//size_t fft= 17 *d *log(2.*d)/log(2.);
+	//size_t costFFT  = mmul*2*d + 3*madd*fft;
+	size_t count=0;
+	bool correct=true;
+	do {
+		PMFFT.mul(C,A,B);
+		for(size_t i=0;i<d;i++)
+			for(size_t j=0;j<d;j++)
+				fld.axpyin(D[i+j],A[i],B[j]);		
+		size_t k=0;
+		while(k<2*d-1 && (D[k]==C[k])) {k++;}
+
+		correct= (correct&&(k==(2*d-1)));
+		if (!correct){
+			cout<<"A= "; std::copy(A.begin(), A.end(), std::ostream_iterator<Element>(std::cout, " "));cout<<endl;
+			cout<<"B= "; std::copy(B.begin(), B.end(), std::ostream_iterator<Element>(std::cout, " "));cout<<endl;
+			cout<<"at : "<<k<<endl;
+			cout<<"C= "; std::copy(C.begin(), C.end(), std::ostream_iterator<Element>(std::cout, " "));cout<<endl;
+			cout<<"D= "; std::copy(D.begin(), D.end(), std::ostream_iterator<Element>(std::cout, " "));cout<<endl;
+			
+	}
+
+		count++;
+	} while (count<3 && correct);
+	cout<<"polmul fft is "<<(correct?" OK":" KO")<<endl;
+	cout<<endl;
+}
+
+
+template<typename Field, typename RandIter>
 void profile_pol_mulfft(const Field& fld,  RandIter& Gen, size_t d) {
 	typedef PolynomialMatrix<PMType::polfirst,PMStorage::plain,Field> MatrixP;
 
@@ -136,11 +181,17 @@ void profile_pol_mulfft(const Field& fld,  RandIter& Gen, size_t d) {
 
 
 template<typename Field>
-void runTest(const Field& F, size_t n, long b, long d, long seed, std::string test){
+void runTest(const Field& F, long b, long d, long seed, std::string test){
 	
 	typename Field::RandIter G(F,b,seed);
-	//typename Field::RandIter G(F,seed);	
-	//if (test == "fft")
+	//typename Field::RandIter G(F,seed);
+	if (test == "all"){
+		check_pol_mulfft(F,G,d);
+		profile_pol_mulfft(F,G,d);
+	}
+	if (test == "check")
+		check_pol_mulfft(F,G,d);
+	if (test == "fft")
 		profile_pol_mulfft(F,G,d);
 	if (test == "longfft"){
 		size_t D[15]={64,128,256,512,1024,2048,4096,8192};
@@ -150,20 +201,18 @@ void runTest(const Field& F, size_t n, long b, long d, long seed, std::string te
 }
 
 int main(int argc, char** argv){
-	static size_t  n = 32; // matrix dimension
 	static long    b = 20; // entries bitsize
-	static uint64_t d = 32;  // matrix degree
+	static uint64_t d = 32;  // polynomial degree
 	static bool    z = false; // computer over  Z[x]
 	static long    seed = time(NULL);
 	static std::string  test ="all";
 
 	static Argument args[] = {
-		{ 'n', "-n N", "Set dimension of test matrices to NxN.", TYPE_INT,     &n },
 		{ 'd', "-d D", "Set degree of test matrices to D.", TYPE_INT,     &d },
 		{ 'b', "-b B", "Set bitsize of the matrix entries", TYPE_INT, &b },
 		{ 'z', "-z y", "Perform the computation over Z[x]", TYPE_BOOL, &z},
 		{ 's', "-s s", "Set the random seed to a specific value", TYPE_INT, &seed},
-		{ 't', "-t t", "Choose the targeted test {all,check,bench,fft,kara,longfft}", TYPE_STR, &test},
+		{ 't', "-t t", "Choose the targeted test {all,check,fft,longfft}", TYPE_STR, &test},
 		END_OF_ARGUMENTS
 	};
 	parseArguments (argc, argv, args);
@@ -175,7 +224,7 @@ int main(int argc, char** argv){
 		cout<<"Computation over Z[x]  "<<endl;
 		cout<<"++++++++++++++++++++++++++++++++++++"<<endl;
 		Givaro::ZRing<integer> F;
-		runTest (F,n,b,d,seed,test);
+		runTest (F,b,d,seed,test);
 	}
 	else {
 		if (b > 29){
@@ -200,7 +249,7 @@ int main(int argc, char** argv){
 			Givaro::Modular<double> F((int32_t)p);
 			cout<<"Computation over Fp[x] with p=  "<<p<<" (FFT prime)"<<endl;
 			cout<<"++++++++++++++++++++++++++++++++++++"<<endl;
-			runTest (F,n,b,d,seed,test);
+			runTest (F,b,d,seed,test);
 		}
 	}
 	return 0;
