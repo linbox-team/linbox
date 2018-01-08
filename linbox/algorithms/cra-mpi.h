d503cbacf4718f1331d8a25dc335b8d73e6a1ae2
ZHG <ZHG2011823@hotmail.com>
2018-01-08 17:37:27 +0100

    Test program for the CRA method using or without using MPI with input parameters added but results from BlasElimination and CRA are not consistent.

--- linbox/algorithms/cra-mpi.h
+++ linbox/algorithms/cra-mpi.h
@@ -40,6 +40,13 @@
 #include "linbox/algorithms/rational-cra.h"
 #include "linbox/util/mpicpp.h"
 
+template <typename T > class chooseMPItype;
+template <> struct chooseMPItype<unsigned int>{ static constexpr MPI_Datatype val = MPI_UNSIGNED;};
+template <> struct chooseMPItype<unsigned long long int>{ static constexpr MPI_Datatype val = MPI_UNSIGNED_LONG_LONG;};
+template <> struct chooseMPItype<unsigned long int>{ static constexpr MPI_Datatype val = MPI_UNSIGNED_LONG;};
+#include <gmp++/gmp++.h>
+#include <string>
+
 namespace LinBox
 {
 
@@ -226,8 +233,6 @@ namespace LinBox
 
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 	template<class RatCRABase>
 	struct MPIratChineseRemainder  {
 		typedef typename RatCRABase::Domain	Domain;
@@ -242,6 +247,7 @@ namespace LinBox
 		MPIratChineseRemainder(const Param& b, Communicator *c) :
 			Builder_(b), _commPtr(c), _numprocs(c->size())
 		{}
+
 		template<class Function, class PrimeIterator>
 		Integer & operator() (Integer& num, Integer& den, Function& Iteration, PrimeIterator& primeg)
 		{
@@ -318,8 +324,8 @@ namespace LinBox
 
 
 
-		template<class Function, class PrimeIterator>
-		BlasVector<Givaro::ZRing<Integer> > & operator() ( BlasVector<Givaro::ZRing<Integer> >& num, Integer& den, Function& Iteration, PrimeIterator& primeg)
+		template<class T, class Function, class PrimeIterator>
+		BlasVector<Givaro::ZRing<T> > & operator() ( BlasVector<Givaro::ZRing<T> >& num, Integer& den, Function& Iteration, PrimeIterator& primeg)
 		{
 			//  if there is no communicator or if there is only one process,
 			//  then proceed normally (without parallel)
@@ -331,7 +337,7 @@ namespace LinBox
 			int procs = _commPtr->size();
 			int process = _commPtr->rank();
 
-			typename Rebind<BlasVector<Givaro::ZRing<Integer> >, Domain>::other r;
+			typename Rebind<BlasVector<Givaro::ZRing<T> >, Domain>::other r;
 
 			//  parent propcess
 			if(process == 0){
@@ -390,9 +396,138 @@ namespace LinBox
 		}
 
 
-	};
-////////////////////////////////////////////////////////////////////////////////////////////
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+/*
+		template<class Function, class PrimeIterator>
+		BlasVector<Givaro::ZRing<Integer> > & operator() ( BlasVector<Givaro::ZRing<Integer> >& num, Integer& den, Function& Iteration, PrimeIterator& primeg)
+		{
+			//  if there is no communicator or if there is only one process,
+			//  then proceed normally (without parallel)
+			if(_commPtr == 0 || _commPtr->size() == 1) {
+				RationalRemainder< RatCRABase > sequential(Builder_);
+				return sequential(num, den, Iteration, primeg);
+			}
 
+			int procs = _commPtr->size();
+			int process = _commPtr->rank();
+
+			typename Rebind<BlasVector<Givaro::ZRing<Integer> >, Domain>::other r;
+
+size_t nj=num.size();
+  int B_mp_alloc[nj], B_a_size[nj]; 
+  unsigned lenB;  Givaro::Integer temp; 
+  std::vector<mp_limb_t> B_mp_data;
+   
+
+
+			//  parent propcess
+			if(process == 0){
+				int primes[procs - 1];
+				Domain D(*primeg);
+				//  for each slave process...
+				for(int i=1; i<procs; i++){
+					//  generate a new prime
+					++primeg; while(Builder_.noncoprime(*primeg) ) ++primeg;
+					//  fix the array of currently sent primes
+					primes[i - 1] = *primeg;
+					//  send the prime to a slave process
+					_commPtr->send(primes[i - 1], i);
+				}
+				Builder_.initialize( D, Iteration(r, D) );
+				int poison_pills_left = procs - 1;
+				while(poison_pills_left > 0 ){
+					int idle_process = 0;
+					//  receive the beginnin and end of a vector in heapspace
+					//_commPtr->recv(r.begin(), r.end(), MPI_ANY_SOURCE, 0); //<---------<<
+  MPI_Recv(&B_mp_alloc[0], nj, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  MPI_Recv(&B_a_size[0], nj, MPI_INT, MPI_ANY_SOURCE,0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);  
+  MPI_Recv(&lenB, 1, MPI_UNSIGNED, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);B_mp_data.resize(lenB);
+  MPI_Recv(&B_mp_data[0], lenB, chooseMPItype<mp_limb_t>::val,  MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    //Reconstruction of vector r
+    std::cerr << "<<< received r: " << std::endl;
+    __mpz_struct * ptr2;
+    size_t count=0;    
+    for(int j=0;j<nj;j++){ 
+      ptr2 = const_cast<__mpz_struct*>(temp.get_mpz());
+      ptr2->_mp_alloc = B_mp_alloc[j];
+      ptr2->_mp_size = B_a_size[j];
+      _mpz_realloc(ptr2,ptr2->_mp_alloc);
+      for(size_t i=0; i< ptr2->_mp_alloc; ++i){
+	ptr2->_mp_d[i] = (B_mp_data[i+count]);
+      }count+=ptr2->_mp_alloc;
+      r.setEntry(j,temp);
+      std::cerr << r.getEntry(j) << "\t" ; std::cerr<< std::endl;       
+    }
+
+
+
+					//  determine which process sent answer
+					//  and give them a new prime
+					idle_process = (_commPtr->get_stat()).MPI_SOURCE;
+					Domain D(primes[idle_process - 1]);
+std::cerr << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
+					Builder_.progress(D, r);
+std::cerr << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << std::endl;
+					//  if still working, queue a prime
+					if(! Builder_.terminated()){
+						++primeg;
+						primes[idle_process - 1] = *primeg;
+					}
+					//  otherwise, queue a poison pill
+					else{
+						primes[idle_process - 1] = 0;
+						poison_pills_left--;
+					}
+					//  send the prime or poison
+					_commPtr->send(primes[idle_process - 1], idle_process);
+
+				}  // while
+				return Builder_.result(num,den);
+
+			}
+			//  child process
+			else{
+
+				int pp;
+				//  get a prime, compute, send back start and end
+				//  of heap addresses
+				while(true){
+					_commPtr->recv(pp, 0);
+					if(pp == 0)
+						break;
+					Domain D(pp);
+					Iteration(r, D);
+					//_commPtr->send(r.begin(), r.end(), 0, 0); //<---------<<
+    //Split vector r into arrays
+    __mpz_struct * ptr;
+std::cerr << ">>> sending r: " << std::endl;
+    for(int j=0;j<nj;j++){
+      std::cerr << r.getEntry(j)<< "\t" ; std::cerr<< std::endl;
+      ptr = const_cast<__mpz_struct*>(r.getEntry(j).get_mpz());
+      B_mp_alloc[j] = ptr->_mp_alloc;
+      B_a_size[j] = ptr->_mp_size;
+      mp_limb_t * a_array = ptr->_mp_d;
+      for(size_t i=0; i< ptr->_mp_alloc; ++i){
+	B_mp_data.push_back(a_array[i]);
+      }
+    }    
+    lenB = B_mp_data.size();
+
+  MPI_Send(&B_mp_alloc[0], nj, MPI_INT, 0, 0, MPI_COMM_WORLD);
+  MPI_Send(&B_a_size[0], nj, MPI_INT, 0, 0, MPI_COMM_WORLD);  
+  MPI_Send(&lenB, 1, MPI_UNSIGNED, 0, 0, MPI_COMM_WORLD);
+  MPI_Send(&B_mp_data[0], lenB, chooseMPItype<mp_limb_t>::val, 0, 0, MPI_COMM_WORLD);
+
+
+				}
+				return num;
+			}
+		}
+
+*/
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	};
 
 
 
--- tests/test-solveCRA.C
+++ tests/test-solveCRA.C
@@ -1,6 +1,6 @@
 
 /* tests/test-solve.C
- * Copyright (C) 2001, 2002 Bradford Hovinen
+ * Copyright (C) 2001, 2018
  *
  * Written by
  *
@@ -38,13 +38,6 @@
 #include <givaro/zring.h>
 #include <linbox/matrix/sparse-matrix.h>
 
-#ifdef __LINBOX_HAVE_MPI
-#include <mpi.h>
-#include "linbox/util/mpicpp.h"
-#endif
-#include <linbox/solutions/methods.h>
-#include <linbox/solutions/solve.h>
-
 using namespace LinBox;
 using namespace std;
 
@@ -57,42 +50,184 @@ T& myrand (T& r, long size)
     return r = T(  lrand48() % size ) ;
 };
 
-
 #ifdef __LINBOX_HAVE_MPI
+#include <mpi.h>
+#include "linbox/util/mpicpp.h"
+#endif
+#include <linbox/solutions/methods.h>
+#include <linbox/solutions/solve.h>
+
+
+
+
+
+/* 
 template <typename T > class chooseMPItype;
 template <> struct chooseMPItype<unsigned int>{ static constexpr MPI_Datatype val = MPI_UNSIGNED;};
 template <> struct chooseMPItype<unsigned long long int>{ static constexpr MPI_Datatype val = MPI_UNSIGNED_LONG_LONG;};
 template <> struct chooseMPItype<unsigned long int>{ static constexpr MPI_Datatype val = MPI_UNSIGNED_LONG;};
+*/
+#ifdef __LINBOX_HAVE_MPI
+#include <gmp++/gmp++.h>
+#include <string>
+template <class Vector>
+//void MPIgmpBcast(Vector& B, int ni, int nj, Communicator *Cptr)
+void MPIgmpBcast(Vector& B, int nj, Communicator *Cptr)
+{ 
+  int B_mp_alloc[nj], B_a_size[nj]; 
+  unsigned lenB;  Givaro::Integer temp; 
+  std::vector<mp_limb_t> B_mp_data;
+  MPI_Barrier(MPI_COMM_WORLD);
+  if(0==Cptr->rank()){
+    //std::cerr << "B=:= " << std::endl;
+    //Split vector B into arrays
+    __mpz_struct * ptr;
+    for(int j=0;j<nj;j++){
+      //std::cerr << B.getEntry(j)<< "\t" ; std::cerr<< std::endl;
+      ptr = const_cast<__mpz_struct*>(B.getEntry(j).get_mpz());
+      B_mp_alloc[j] = ptr->_mp_alloc;
+      B_a_size[j] = ptr->_mp_size;
+      mp_limb_t * a_array = ptr->_mp_d;
+      for(size_t i=0; i< ptr->_mp_alloc; ++i){
+	B_mp_data.push_back(a_array[i]);
+      }
+    }
+    
+    lenB = B_mp_data.size();
+    
+  }
+  //Distribut Givaro::Integer through its elementary parts
+  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Bcast(&B_mp_alloc[0], nj, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&B_a_size[0], nj, MPI_INT, 0, MPI_COMM_WORLD);
+  
+  MPI_Bcast(&lenB, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+  if(0!=Cptr->rank()) B_mp_data.resize(lenB);
+  MPI_Bcast(&B_mp_data[0], lenB, chooseMPItype<mp_limb_t>::val,  0, MPI_COMM_WORLD);
+  MPI_Barrier(MPI_COMM_WORLD);
+  
+  if(0!=Cptr->rank()){
+    //Reconstruction of vector B
+    //std::cerr << "received B::= " << std::endl;
+    __mpz_struct * ptr2;
+    size_t count=0;
+    
+    for(int j=0;j<nj;j++){ 
+      ptr2 = const_cast<__mpz_struct*>(temp.get_mpz());
+      ptr2->_mp_alloc = B_mp_alloc[j];
+      ptr2->_mp_size = B_a_size[j];
+      _mpz_realloc(ptr2,ptr2->_mp_alloc);
+      for(size_t i=0; i< ptr2->_mp_alloc; ++i){
+	ptr2->_mp_d[i] = (B_mp_data[i+count]);
+      }count+=ptr2->_mp_alloc;
+      B.setEntry(j,temp);
+      //std::cerr << B.getEntry(j) << "\t" ; std::cerr<< std::endl; 
+      
+    }
+    
+  }MPI_Barrier(MPI_COMM_WORLD);
+  
+}
 
+//template<typename T>
+//void MPIvecBcast(LinBox::SparseMatrix< T > & A, int ni, int nj, Communicator *Cptr)
+template <class LinMat>
+void MPIgmpBcast(LinMat& A, int ni, int nj, Communicator *Cptr)
+{ 
+  int A_mp_alloc[ni*nj], A_a_size[ni*nj];
+  unsigned lenA;
+  std::vector<mp_limb_t> A_mp_data;
+  MPI_Barrier(MPI_COMM_WORLD);
+  if(0==Cptr->rank()){
+    
+    //Split Matrix A into arrays 
+    //std::cerr << "A=:= " << std::endl; 
+    __mpz_struct * ptr;
+    for (long i = 0; i < ni; ++i){
+      for (long j = 0; j < nj; ++j){
+	
+	//std::cerr << A.getEntry(i,j)<< "\t" ; std::cerr<< std::endl;
+	ptr = const_cast<__mpz_struct*>(A.getEntry(i,j).get_mpz());
+	A_mp_alloc[j+i*nj] = ptr->_mp_alloc;
+	A_a_size[j+i*nj] = ptr->_mp_size;
+	mp_limb_t * a_array = ptr->_mp_d;
+	for(size_t k=0; k< ptr->_mp_alloc; ++k)
+	  A_mp_data.push_back(a_array[k]);
+	
+	
+      }
+    }
+    lenA = A_mp_data.size();
+  }
+  //Distribut Givaro::Integer through its elementary parts
+  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Bcast(&A_mp_alloc[0], ni*nj, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&A_a_size[0], ni*nj, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&lenA, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+  if(0!=Cptr->rank()) A_mp_data.resize(lenA);
+  MPI_Bcast(&A_mp_data[0], lenA, chooseMPItype<mp_limb_t>::val,  0, MPI_COMM_WORLD);
+  MPI_Barrier(MPI_COMM_WORLD);
+  
+  if(0!=Cptr->rank()){
+    //Reconstruction of matrix A
+    __mpz_struct * ptr2;
+    size_t count=0;
+    Givaro::Integer temp; 
+    //std::cerr << "received A:= " << std::endl;
+    for (long i = 0; i < ni; ++i){
+      for (long j = 0; j < nj; ++j){
 
-void printMPItype(MPI_Datatype type){ 
-	if(MPI_UNSIGNED==type)  std::cout<<"MPI_UNSIGNED is to be used"<< std::endl;
-	if(MPI_UNSIGNED_LONG_LONG==type)   std::cout<<"MPI_UNSIGNED_LONG_LONG is to be used"<< std::endl;
-	if(MPI_UNSIGNED_LONG==type) std::cout<<"MPI_UNSIGNED_LONG is to be used"<<std::endl;
-};
+	ptr2 = const_cast<__mpz_struct*>(temp.get_mpz());
+	ptr2->_mp_alloc = A_mp_alloc[j+i*nj];
+	ptr2->_mp_size = A_a_size[j+i*nj];
+	_mpz_realloc(ptr2,ptr2->_mp_alloc);
+	for(size_t k=0; k< ptr2->_mp_alloc; ++k){
+	  ptr2->_mp_d[k] = (A_mp_data[k+count]);	
+	}count+=ptr2->_mp_alloc;
+	A.setEntry(i,j,temp);
+	//std::cerr << A.getEntry(i,j) << "\t" ; std::cerr<< std::endl;
+      }
+    }
+    
+  }MPI_Barrier(MPI_COMM_WORLD);
+  
+}
+
 
 #endif
 
 
 
-#include <gmp++/gmp++.h>
-#include <string>
+
 
 
 int main (int argc, char **argv)
 {
 	bool pass = true;
 
-	size_t ni = 10;
-	size_t nj = ni;
-
-	size_t r = 20;
-	size_t N = 16;
+	size_t ni ;
+	size_t nj ;
+	int max;  
 
 #ifdef __LINBOX_HAVE_MPI
+
   Communicator *Cptr = NULL;
   Cptr = new Communicator(&argc, &argv);
-  int max=10000;  
+
+
+    ni=3,nj=3,max=100; 
+std::cerr << "proc("<< Cptr->rank() <<") defaut: ni = nj = "<< nj << std::endl;
+	static Argument args[] = {
+		{ 'n', "-n N", "Set column and row dimension of test matrices to N.", TYPE_INT,     &ni },
+		{ 'm', "-m M", "Set the mxaimum for the range of integers to generate.", TYPE_INT,     &max },
+		END_OF_ARGUMENTS
+	};	
+parseArguments (argc, argv, args);
+
+ MPI_Bcast(&ni, 1, MPI_INT, 0, MPI_COMM_WORLD); nj=ni;
+std::cerr << "proc("<< Cptr->rank() <<") after: ni = nj = "<< nj << std::endl;
+
+
   Givaro::ZRing<Integer> ZZ;
   SparseMatrix<Givaro::ZRing<Integer> > A (ZZ,ni,nj);
 
@@ -101,19 +236,8 @@ DenseVector X(ZZ, A.coldim()), X2(ZZ, A.coldim()),  B(ZZ, A.coldim());
 Givaro::ZRing<Integer>::Element d;
 
 
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-int B_mp_alloc[nj], B_a_size[nj]; 
-int A_mp_alloc[ni*nj], A_a_size[ni*nj];
-unsigned lenA,lenB;
-std::vector<mp_limb_t> B_mp_data;
-std::vector<mp_limb_t> A_mp_data;
-
   if(0==Cptr->rank()){
-	static Argument args[] = {
-		{ 'n', "-n N", "Set column and row dimension of test matrices to N.", TYPE_INT,     &ni },
-		{ 'm', "-m M", "Set the mxaimum for the range of integers to generate.", TYPE_INT,     &max },
-		END_OF_ARGUMENTS
-	};	nj = ni;
+
 
 std::cout<<"Computation is done over Q"<<std::endl;
 
@@ -129,114 +253,21 @@ Givaro::Integer valeur("12345678912345678912345678912345678912345678912345678912
 //   std::cout << "A is " << A.rowdim() << " by " << A.coldim() << std::endl;
 //   if (A.rowdim() <= 20 && A.coldim() <= 20) A.write(std::cerr << "A:=",Tag::FileFormat::Maple) << ';' << std::endl;
 
-//Split Matrix A into arrays 
-//std::cerr << "A=:= " << std::endl; 
-__mpz_struct * ptr;
-   for (long i = 0; i < ni; ++i){
-     for (long j = 0; j < nj; ++j){
-
-//std::cerr << A.getEntry(i,j)<< "\t" ; std::cerr<< std::endl;
-ptr = const_cast<__mpz_struct*>(A.getEntry(i,j).get_mpz());
-A_mp_alloc[j+i*nj] = ptr->_mp_alloc;
-A_a_size[j+i*nj] = ptr->_mp_size;
-    mp_limb_t * a_array = ptr->_mp_d;
-    for(size_t k=0; k< ptr->_mp_alloc; ++k)
-        A_mp_data.push_back(a_array[k]);
-
-
-}
-}
-lenA = A_mp_data.size();
-
-//Split vector B into arrays
-Givaro::Integer valeur2("448832189123456789123456789123456789123456789123456789123456789123456789"); 
-   for (long j = 0; j < nj-1; ++j)
-	B.setEntry(j,myrand(tmp, max));
+    Givaro::Integer valeur2("448832189123456789123456789123456789123456789123456789123456789123456789"); 
+    for (long j = 0; j < nj; ++j)
+      B.setEntry(j,myrand(tmp, max));
 //B.setEntry(nj-1,valeur2);
-//std::cerr << "B:= " << std::endl; 
-//__mpz_struct * ptr;
-for(int j=0;j<nj;j++){
-//std::cerr << B.getEntry(j)<< "\t" ; std::cerr<< std::endl;
-ptr = const_cast<__mpz_struct*>(B.getEntry(j).get_mpz());
-B_mp_alloc[j] = ptr->_mp_alloc;
-B_a_size[j] = ptr->_mp_size;
-    mp_limb_t * a_array = ptr->_mp_d;
-    for(size_t i=0; i< ptr->_mp_alloc; ++i){
-        B_mp_data.push_back(a_array[i]);
-    }
-}
-
-lenB = B_mp_data.size();
 
 
   }//End of BLock for process(0)
 
 
+  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Bcast(&ni, 1, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
 //Distribut Givaro::Integer through its elementary parts
-MPI_Barrier(MPI_COMM_WORLD);
-MPI_Bcast(&A_mp_alloc[0], ni*nj, MPI_INT, 0, MPI_COMM_WORLD);
-MPI_Bcast(&A_a_size[0], ni*nj, MPI_INT, 0, MPI_COMM_WORLD);
-
-MPI_Bcast(&lenA, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
-MPI_Barrier(MPI_COMM_WORLD);
-if(0!=Cptr->rank()) A_mp_data.resize(lenA);
-MPI_Bcast(&A_mp_data[0], lenA, chooseMPItype<mp_limb_t>::val,  0, MPI_COMM_WORLD);
-MPI_Barrier(MPI_COMM_WORLD);
-
-MPI_Barrier(MPI_COMM_WORLD);
-MPI_Bcast(&B_mp_alloc[0], nj, MPI_INT, 0, MPI_COMM_WORLD);
-MPI_Bcast(&B_a_size[0], nj, MPI_INT, 0, MPI_COMM_WORLD);
-
-MPI_Bcast(&lenB, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
-MPI_Barrier(MPI_COMM_WORLD);
-if(0!=Cptr->rank()) B_mp_data.resize(lenB);
-MPI_Bcast(&B_mp_data[0], lenB, chooseMPItype<mp_limb_t>::val,  0, MPI_COMM_WORLD);
-MPI_Barrier(MPI_COMM_WORLD);
-
- if(0!=Cptr->rank()){
-
-
-//Reconstruction of matrix A
-__mpz_struct * ptr2;
-mp_limb_t * a_array;
-size_t count=0;
- Givaro::Integer temp; 
-//std::cerr << "received A:= " << std::endl;
-   for (long i = 0; i < ni; ++i){
-     for (long j = 0; j < nj; ++j){
-
-ptr2 = const_cast<__mpz_struct*>(temp.get_mpz());//ptr2 = const_cast<__mpz_struct*>(A.getEntry(i,j).get_mpz());
-ptr2->_mp_alloc = A_mp_alloc[j+i*nj];
-ptr2->_mp_size = A_a_size[j+i*nj];
-   _mpz_realloc(ptr2,ptr2->_mp_alloc);
-   for(size_t k=0; k< ptr2->_mp_alloc; ++k){
-        ptr2->_mp_d[k] = (A_mp_data[k+count]);	
-    }count+=ptr2->_mp_alloc;
-A.setEntry(i,j,temp);//<----------------------------<<<<<
-//std::cerr << A.getEntry(i,j) << "\t" ; std::cerr<< std::endl;
-}
-}
-
-
-//Reconstruction of vector B
-//std::cerr << "received B::= " << std::endl;
-//__mpz_struct * ptr2;
-count=0;
-for(int j=0;j<nj;j++){ 
-ptr2 = const_cast<__mpz_struct*>(temp.get_mpz());//ptr2 = const_cast<__mpz_struct*>(B.getEntry(j).get_mpz());
-ptr2->_mp_alloc = B_mp_alloc[j];
-ptr2->_mp_size = B_a_size[j];
-    _mpz_realloc(ptr2,ptr2->_mp_alloc);
-    for(size_t i=0; i< ptr2->_mp_alloc; ++i){
-        ptr2->_mp_d[i] = (B_mp_data[i+count]);
-    }count+=ptr2->_mp_alloc;
-B.setEntry(j,temp);//<----------------------------<<<<<
-//std::cerr << B.getEntry(j) << "\t" ; std::cerr<< std::endl; 
-
-}
-
-  }
-
+  MPIgmpBcast(A, ni, ni, Cptr);
+  MPIgmpBcast(B, nj, Cptr);
+  
 
 /***********************
 * Results verification 
@@ -248,7 +279,7 @@ Timer chrono;
 solveCRA (X2, d, A, B, tg, Method::Hybrid(*Cptr),Cptr);	
    endtime   = MPI_Wtime(); 
 	
-
+MPI_Barrier(MPI_COMM_WORLD);
 if(0==Cptr->rank()){
 
 
@@ -290,38 +321,97 @@ if(0==Cptr->rank()){
 
 }
 
-  MPI_Finalize();
+MPI_Finalize();
 return pass ? 0 : -1;
 	
 #else
 Timer chrono;
-                std::cout << "BlasElimination" << std::endl;
-                chrono.start();
-                solve (X, d, A, B, Method::BlasElimination());
-                chrono.stop();
-                std::cout << "CPU time (seconds): " << chrono.usertime() << std::endl;
+    ni=3,nj=3,max=100; 
 
-		
-		std::cout << "CRA" << std::endl;
-                chrono.start();
-		solveCRA (X2, d, A, B, tg, Method::Hybrid(*Cptr),Cptr);	
-                chrono.stop();
-		std::cout << "CPU time (seconds): " <<  chrono.usertime() << std::endl;
-	for (size_t j = 0 ; j < nj ; ++j){
-		if(!Givaro::ZRing<Integer>().areEqual(X[j],X2[j])){
-			std::cerr << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << std::endl;
-			std::cerr << "                  ***Failed***                   " << std::endl;
-			std::cerr << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
-			pass = false;
-			break;
-		}
-	}
+	static Argument args[] = {
+		{ 'n', "-n N", "Set column and row dimension of test matrices to N.", TYPE_INT,     &ni },
+		{ 'm', "-m M", "Set the mxaimum for the range of integers to generate.", TYPE_INT,     &max },
+		END_OF_ARGUMENTS
+	};	
+parseArguments (argc, argv, args);nj = ni;
 
+std::cout<<"Computation is done over Q"<<std::endl;
+  
+  srand48(time(NULL));
+  //	int offset = 0;
+  Givaro::ZRing<Integer> ZZ;
+  SparseMatrix<Givaro::ZRing<Integer> > A (ZZ,ni,nj);
+  
+  typedef DenseVector<Givaro::ZRing<Integer> > DenseVector;
+  DenseVector X(ZZ, A.coldim()), X2(ZZ, A.coldim()),  B(ZZ, A.coldim());
+  Givaro::ZRing<Integer>::Element d;
+  long tmp;
+  Givaro::Integer valeur("123456789123456789123456789123456789123456789123456789123456789123456789"); 
+  printf("%ld %ld M\n",1, ni, nj);
+  for (long i = 0; i < ni; ++i)
+    for (long j = 0; j < nj; ++j){
+      A.setEntry(i,j,myrand(tmp, max));
+    }
+  //A.setEntry(nj-1,nj-1,valeur);
+  //   std::cout << "A is " << A.rowdim() << " by " << A.coldim() << std::endl;
+  //   if (A.rowdim() <= 20 && A.coldim() <= 20) A.write(std::cerr << "A:=",Tag::FileFormat::Maple) << ';' << std::endl;
+  Givaro::Integer valeur2("448832189123456789123456789123456789123456789123456789123456789123456789"); 
+  for (long j = 0; j < nj-1; ++j)
+    B.setEntry(j,myrand(tmp, max));
+  //B.setEntry(nj-1,valeur2);
+      
+      
+
+ /***********************
+  Results verification 
+ ***********************/
+      /*
+	std::cerr << "Compute with B: " << std::endl;
+	for(int j=0;j<nj;j++) std::cerr << B.getEntry(j) << std::endl; 
+	
+	std::cout << "Compute with A: " << A.rowdim() << " by " << A.coldim() << std::endl;
+	if (A.rowdim() <= 20 && A.coldim() <= 20) A.write(std::cout << "A::=",Tag::FileFormat::Maple) << ';' << std::endl;
+      */
+  
+  // BlasElimination
+  //DenseVector X(ZZ, A.coldim());
+  std::cout << "BlasElimination" << std::endl;
+  chrono.start();
+  solve (X, d, A, B, Method::BlasElimination());
+  chrono.stop();
+  
+  std::cout << "(BlasElimination) Solution is [";
+  for(DenseVector::const_iterator it=X.begin();it != X.end(); ++it)
+    ZZ.write(cout, *it) << " ";
+  std::cout << "] / ";
+  ZZ.write(std::cout, d)<< std::endl;
+  std::cout << "CPU time (seconds): " << chrono.usertime() << std::endl;
+
+  
+  // solveCRA
+  std::cout << "solveCRA" << std::endl;
+  RingCategories::IntegerTag tg;
+  chrono.start();
+  solveCRA (X2, d, A, B, tg, Method::BlasElimination());	
+  chrono.stop();
+  std::cout << "MPI CRA Solution is [";
+  for(DenseVector::const_iterator it=X2.begin();it != X2.end(); ++it)
+    ZZ.write(cout, *it) << " ";
+  std::cout << "] / ";
+  ZZ.write(std::cout, d) << std::endl;
+  std::cout << "CPU time (seconds): " << chrono.usertime() << std::endl;
+  
+  
+  for (size_t j = 0 ; j < nj ; ++j){
+    if(!Givaro::ZRing<Integer>().areEqual(X[j],X2[j])){
+      std::cerr << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << std::endl;
+      std::cerr << "                  ***Failed***                   " << std::endl;
+      std::cerr << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
+      break;
+    }
+  }
 	return pass ? 0 : -1;
 #endif
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-
 	
 }
 
