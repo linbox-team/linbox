8b8f515c5d3b53da073d1f7db4187af114d93205
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2018-01-23 19:13:21 +0100

    cout in std

--- examples/blassolve.C
+++ examples/blassolve.C
@@ -106,7 +106,7 @@ int main (int argc, char **argv) {
 
         // BlasElimination
     Method::BlasElimination M;
-    M.singular(Specifier::NONSINGULAR);
+    //M.singular(Specifier::NONSINGULAR);
 
     chrono.start();
     solve (X, d, A, B, M);
