27e97480ed7ba6fb103f5cd60468969a4232a7ba
Daniel S. Roche <roche@usna.edu>
2018-06-21 16:46:07 +0200

    cleanup cra-early-multip

--- linbox/algorithms/cra-early-multip.h
+++ linbox/algorithms/cra-early-multip.h
@@ -57,6 +57,8 @@ namespace LinBox
 		typedef Domain_Type			Domain;
 		typedef typename Domain::Element DomainElement;
 		typedef EarlyMultipCRARGen<Domain,RandIter_Type> Self_t;
+        using SingleParent = EarlySingleCRA<Domain>;
+        using MultiParent = FullMultipCRA<Domain>;
 
         const unsigned short LINEAR_COMB_RBITS = 15; // # of random bits in linear combination
 
@@ -86,121 +88,61 @@ namespace LinBox
 
 		EarlyMultipCRARGen( RandIter_Type * rgen,
                             const unsigned long EARLY=DEFAULT_EARLY_TERM_THRESHOLD) :
-			EarlySingleCRA<Domain>(EARLY), FullMultipCRA<Domain>(),
+			SingleParent(EARLY), MultiParent(),
             rgen_ (rgen)
 		{}
 
 		Integer& getModulus(Integer& m)
 		{
-			EarlySingleCRA<Domain>::getModulus(m);
+			SingleParent::getModulus(m);
 			return m;
 		}
 		Integer& getResidue(Integer& m)
 		{
-			EarlySingleCRA<Domain>::getResidue(m);
+			SingleParent::getResidue(m);
 			return m;
 		}
 
 		template<template<class T> class Vect>
 		Vect<Integer>& getResidue(Vect<Integer>& m)
 		{
-			FullMultipCRA<Domain>::getResidue(m);
+			MultiParent::getResidue(m);
 			return m;
 		}
 
 		//! Init
-		template<template<class T> class Vect>
-		void initialize (const Integer& D, const Vect<Integer>& e)
-		{
+        template <class DomainOrInteger, class Vect>
+        void initialize (const DomainOrInteger& D, const Vect& e) {
             choose_linear_comb(e.size());
-			Integer z;
-			dot(z, D, e, randv_);
-			EarlySingleCRA<Domain>::initialize(D, z);
-			FullMultipCRA<Domain>::initialize(D, e);
+			SingleParent::initialize(D, dot(D, e, randv_));
+			MultiParent::initialize(D, e);
 		}
-
-		template<class Vect>
-		void initialize (const Domain& D, const Vect& e)
-		{
-            choose_linear_comb(e.size());
-			DomainElement z;
-			EarlySingleCRA<Domain>::initialize (D, dot(z, D, e, randv_));
-			FullMultipCRA<Domain>::initialize (D, e);
-		}
-
-        /* TODO is this needed?
-		template<class OKDomain>
-		void initialize (const Domain& D, const BlasVector<OKDomain>& e)
-		{
-            choose_linear_comb(e.size());
-			DomainElement z;
-			EarlySingleCRA<Domain>::initialize (D, dot(z, D, e, randv_));
-			FullMultipCRA<Domain>::initialize (D, e);
-		}
-        */
 
 		//! Progress
-		template<template<class T> class Vect>
-		void progress (const Integer& D, const Vect<Integer>& e)
+        template <class DomainOrInteger, class Vect>
+        void progress (const DomainOrInteger& D, const Vect& e)
 		{
 
-			Integer z;
-			EarlySingleCRA<Domain>::progress(D, dot(z, D, e, randv_));
-			FullMultipCRA<Domain>::progress(D, e);
-		}
-
-#if 1
-		template<class Vect>
-                //		template<template <class> class Alloc, template<class, class> class Vect>
-		void progress (const Domain& D, const Vect& e)
-		{
-			// DomainElement z;
-			/*!@todo Could be much faster
-			  - do not compute twice the product of moduli
-			  - reconstruct one element of e until Early Termination,
-			  then only, try a random linear combination.
-			*/
-                        DomainElement z;
-                        EarlySingleCRA<Domain>::progress(D, dot(z, D, e, randv_));
-                        FullMultipCRA<Domain>::progress(D, e);
-		}
-#endif
-
-		template<class OKDomain>
-		void progress (const Domain& D, const BlasVector<OKDomain>& e)
-		{
-			DomainElement z;
-			/*!@todo Could be much faster
-			  - do not compute twice the product of moduli
-			  - reconstruct one element of e until Early Termination,
-			  then only, try a random linear combination.
-			*/
-			EarlySingleCRA<Domain>::progress(D, dot(z, D, e, randv_));
-			FullMultipCRA<Domain>::progress(D, e);
+			SingleParent::progress(D, dot(D, e, randv_));
+			MultiParent::progress(D, e);
 		}
 
 		//! Result
-//		template<template <class> class Alloc, template<class, class> class Vect>
 		template<class Vect>
                 Vect& result(Vect& d)
 		{
-			return FullMultipCRA<Domain>::result(d);
-		}
-
-		BlasVector<Givaro::ZRing<Integer> >& result(BlasVector<Givaro::ZRing<Integer> >& d)
-		{
-			return FullMultipCRA<Domain>::result(d);
+			return MultiParent::result(d);
 		}
 
 		//! terminate
 		bool terminated()
 		{
-			return EarlySingleCRA<Domain>::terminated();
+			return SingleParent::terminated();
 		}
 
 		bool noncoprime(const Integer& i) const
 		{
-			return EarlySingleCRA<Domain>::noncoprime(i);
+			return SingleParent::noncoprime(i);
 		}
 
 		bool changeVector()
@@ -209,29 +151,28 @@ namespace LinBox
 			std::vector<Integer> e(randv_.size());
             // TODO clean this up, decouple with early and full classes
 			/* clear CRAEarlySingle; */
-			EarlySingleCRA<Domain>::occurency_ = 0;
-			EarlySingleCRA<Domain>::nextM_ = 1UL;
-			EarlySingleCRA<Domain>::primeProd_ = 1UL;
-			EarlySingleCRA<Domain>::residue_ = 0;
+			SingleParent::occurency_ = 0;
+			SingleParent::nextM_ = 1UL;
+			SingleParent::primeProd_ = 1UL;
+			SingleParent::residue_ = 0;
 
 			/* Computation of residue_ */
-			std::vector< LazyProduct >::iterator _mod_it = FullMultipCRA<Domain>::RadixPrimeProd_.begin();// list of prime products
-			std::vector< std::vector<Integer> >::iterator _tab_it = FullMultipCRA<Domain>::RadixResidues_.begin();// list of residues as vectors of size 1
-			std::vector< bool >::iterator    _occ_it = FullMultipCRA<Domain>::RadixOccupancy_.begin();//flags of occupied fields
+			std::vector< LazyProduct >::iterator _mod_it = MultiParent::RadixPrimeProd_.begin();// list of prime products
+			std::vector< std::vector<Integer> >::iterator _tab_it = MultiParent::RadixResidues_.begin();// list of residues as vectors of size 1
+			std::vector< bool >::iterator    _occ_it = MultiParent::RadixOccupancy_.begin();//flags of occupied fields
 			int prev_shelf=0, shelf = 0;
-			for (;_occ_it != FullMultipCRA<Domain>::RadixOccupancy_.end(); ++_mod_it, ++_tab_it, ++_occ_it ) {
+			for (;_occ_it != MultiParent::RadixOccupancy_.end(); ++_mod_it, ++_tab_it, ++_occ_it ) {
 				++shelf;
 				if (*_occ_it) {
 					Integer D = _mod_it->operator()();
 					std::vector<Integer> e_v(randv_.size());
 					e_v = *_tab_it;
-					Integer z;
-					dot(z,D, e_v, randv_);
-					Integer prev_residue_ = EarlySingleCRA<Domain>::residue_;
-					EarlySingleCRA<Domain>::progress(D,z);
-					if (prev_residue_ == EarlySingleCRA<Domain>::residue_ )
-						EarlySingleCRA<Domain>::occurency_ = EarlySingleCRA<Domain>::occurency_ +  (shelf - prev_shelf);
-					if ( EarlySingleCRA<Domain>::terminated() ) {
+                    auto z = dot(D, e_v, randv_);
+					Integer prev_residue_ = SingleParent::residue_;
+					SingleParent::progress(D,z);
+					if (prev_residue_ == SingleParent::residue_ )
+						SingleParent::occurency_ = SingleParent::occurency_ +  (shelf - prev_shelf);
+					if ( SingleParent::terminated() ) {
 						return true;
 					}
 					prev_shelf = shelf;
@@ -242,59 +183,28 @@ namespace LinBox
 
 	protected:
 
-		/*! @bug why a dot product here ?
-		 */
-		template <class Vect1, class Vect2>
-		Integer& dot (Integer& z, const Integer& D, const Vect1& v1, const Vect2& v2)
-		{
-			z = 0;
-			typename Vect1::const_iterator v1_p;
-			typename Vect2::const_iterator v2_p;
-			for (v1_p  = v1. begin(), v2_p = v2. begin(); v1_p != v1. end(); ++ v1_p, ++ v2_p) {
-				z = (z + (*v1_p)*(*v2_p))%D;
-			}
-			return z;
-		}
+        template <typename DomElem, typename Elt2>
+        inline void axpyin (DomElem& out, const DomElem& a, const Elt2& b, const Domain& D) {
+            DomElem tmp;
+            D.axpyin(out, a, D.init(tmp, b));
+        }
 
-		/*! @bug why a dot product here ?
-		 */
-		template <class Vect1, class Vect2>
-		DomainElement& dot (DomainElement& z, const Domain& D,
-				    const Vect1& v1,
-				    const Vect2& v2)
-		{
+        template <typename Elt2>
+        inline void axpyin (Integer& out, const Integer& a, const Elt2& b, const Integer& D) {
+            out = (out + a*b) % D;
+        }
 
-			D.assign(z,D.zero); DomainElement tmp;
-			typename Vect1::const_iterator v1_p;
-			typename Vect2::const_iterator v2_p;
-			for (v1_p  = v1. begin(), v2_p = v2. begin();
-			     v1_p != v1. end();
-			     ++ v1_p, ++ v2_p)
-				D.axpyin(z, (*v1_p), D.init(tmp, (*v2_p)));
-
-			//             commentator().report(Commentator::LEVEL_ALWAYS, INTERNAL_DESCRIPTION) << "v: " << v2 << std::endl;
-			//             commentator().report(Commentator::LEVEL_ALWAYS, INTERNAL_DESCRIPTION) << "z: " << z << std::endl;
-			return z;
-		}
-
-		template <class Vect2, class OKDomain>
-		DomainElement& dot (DomainElement& z, const Domain& D,
-				    const BlasVector<OKDomain>& v1,
-				    const Vect2& v2)
-		{
-
-			D.assign(z,D.zero); DomainElement tmp;
-			typename BlasVector<Domain>::const_iterator v1_p;
-			typename Vect2::const_iterator v2_p;
-			for (v1_p  = v1. begin(), v2_p = v2. begin();
-			     v1_p != v1. end();
-			     ++ v1_p, ++ v2_p)
-				D.axpyin(z, (*v1_p), D.init(tmp, (*v2_p)));
-
-			//             commentator().report(Commentator::LEVEL_ALWAYS, INTERNAL_DESCRIPTION) << "v: " << v2 << std::endl;
-			//             commentator().report(Commentator::LEVEL_ALWAYS, INTERNAL_DESCRIPTION) << "z: " << z << std::endl;
-			return z;
-		}
+        template <typename DomainOrInteger, typename Vect1, typename Vect2>
+        typename Vect1::value_type
+        dot (const DomainOrInteger& D, const Vect1& v1, const Vect2& v2) {
+            typename Vect1::value_type z = 0;
+            auto v1_p = v1.begin();
+            auto v2_p = v2.begin();
+            for (; v1_p != v1.end(); ++v1_p, ++v2_p) {
+                axpyin(z, *v1_p, *v2_p, D);
+            }
+            return z;
+        }
 
 	};
 
