616280e179627860cc7a72c6bdaa9e068eb47bd8
Alexis Breust <alexis.breust@gmail.com>
2019-01-15 15:47:09 +0100

    Dispatch for CRA are now structures

--- linbox/vector/mdr-vector.h
+++ linbox/vector/mdr-vector.h
@@ -10,12 +10,21 @@ namespace LinBox {
      */
     template <class Vector>
     struct MdrVector {
+        using Field = typename Vector::Field;
+        using Element = typename Field::Element;
+
         // @fixme Do we need accesors to elements though methods,
         // so that it can be used like any vector?
         // If so, there won't be any reference to element.
 
         Vector num;
-        typename Vector::Field::Element den;
-    };
+        Element den;
 
+        MdrVector(const Field& F, size_t n)
+            : num(F, n)
+        {
+        }
+
+        size_t size() const { return num.size(); }
+    };
 }
