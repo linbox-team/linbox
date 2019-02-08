cba16d091c878b8770d8c65373603f19c74a99f7
ZHG <ZHG2011823@hotmail.com>
2019-02-08 09:20:01 +0100

    A cleaner paladin impl with which duplicated prime number could be found while running the program with following runtime pparameters:  -n 2 -b 40 -q 1  -s -1980263541 -l

--- linbox/algorithms/cra-domain-omp.h
+++ linbox/algorithms/cra-domain-omp.h
@@ -69,11 +69,6 @@ namespace LinBox {
         template <class Function, class PrimeIterator>
         Integer& operator()(Integer& res, Function& Iteration, PrimeIterator& primeiter)
         {
-            //! @bug why why why ???
-            /** erreur: ‘omp_get_num_threads’ has not been declared
-             * ../linbox/algorithms/cra-domain-omp.h:152:16: note: suggested alternative:
-             * /usr/lib/gcc/x86_64-linux-gnu/4.6/include/omp.h:64:12: note:   ‘Givaro::omp_get_num_threads’
-             */
 
             int NN;
             PAR_BLOCK { NN = NUM_THREADS; }
@@ -108,11 +103,7 @@ namespace LinBox {
         template <class Function, class PrimeIterator>
         Integer& operator()(Integer& res, Integer& den, Function& Iteration, PrimeIterator& primeiter)
         {
-            //! @bug why why why ???
-            /** erreur: ‘omp_get_num_threads’ has not been declared
-             * ../linbox/algorithms/cra-domain-omp.h:152:16: note: suggested alternative:
-             * /usr/lib/gcc/x86_64-linux-gnu/4.6/include/omp.h:64:12: note:   ‘Givaro::omp_get_num_threads’
-             */
+
             int NN;
             PAR_BLOCK { NN = NUM_THREADS; }
 
@@ -133,12 +124,13 @@ namespace LinBox {
             int NN;
             PAR_BLOCK { NN = NUM_THREADS; }
 
-#if 1 //----------------------------------task based paladin--------------------------------------------
+
             typedef typename CRATemporaryVectorTrait<Function, Domain>::Type_t ElementContainer;
             // std::vector<LinBox::MaskedPrimeIterator<LinBox::IteratorCategories::HeuristicTag>> m_primeiters;
             std::vector<int> m_primeiters;
-            LinBox::MaskedPrimeIterator<LinBox::IteratorCategories::HeuristicTag> gen( 1, NN);;
+//            LinBox::MaskedPrimeIterator<LinBox::IteratorCategories::HeuristicTag> gen( 1, NN);;
 //            LinBox::MaskedPrimeIterator<LinBox::IteratorCategories::DeterministicTag> gen;
+            LinBox::PrimeIterator<LinBox::IteratorCategories::HeuristicTag> gen;
             long Niter = std::ceil(1.442695040889 * B / (double)(gen.getBits() - 1));
             m_primeiters.reserve(Niter);
             std::vector<CRABase> vBuilders;
@@ -168,38 +160,10 @@ std::cout<<*gen<<std::endl;
             ROUNDdomains.resize(Niter);
 
             compute_task((this->Builder_), m_primeiters, Iteration, ROUNDdomains, ROUNDresidues, gen);
-#else
-            typedef typename CRATemporaryVectorTrait<Function, Domain>::Type_t ElementContainer;
-            std::vector<LinBox::MaskedPrimeIterator<LinBox::IteratorCategories::HeuristicTag>> m_primeiters;
-            //std::vector<LinBox::MaskedPrimeIterator<LinBox::IteratorCategories::DeterministicTag>> m_primeiters;
-
-            m_primeiters.reserve(NN);
-            std::vector<CRABase> vBuilders;
-            vBuilders.reserve(NN);
-
-            for (auto j = 0; j < NN; j++) {
-                LinBox::MaskedPrimeIterator<LinBox::IteratorCategories::HeuristicTag> m_primeiter( j, NN);
-                //LinBox::MaskedPrimeIterator<LinBox::IteratorCategories::DeterministicTag> m_primeiter(j, NN);
-                m_primeiters.push_back(m_primeiter);
-
-                CRABase Builder_(B);
-                vBuilders.push_back(Builder_);
-            }
-
-            long Niter = std::ceil(1.442695040889 * B / (double)(m_primeiters[0].getBits() - 1));
-
-            std::vector<ElementContainer> ROUNDresidues;
-            ROUNDresidues.resize(Niter);
-            std::vector<Domain> ROUNDdomains;
-            ROUNDdomains.resize(Niter);
-
-            compute_task((this->Builder_), m_primeiters, Iteration, ROUNDdomains, ROUNDresidues,vBuilders);
-#endif
-
 
         }
 
-#if 1 //----------------------------------task based paladin--------------------------------------------
+
         template <class Function, class Domain, class ElementContainer>
         void solve_with_prime(int m_primeiters, Function& Iteration, Domain& ROUNDdomains, ElementContainer& ROUNDresidues, bool& foundInconsistentSystem)
         {
@@ -208,51 +172,8 @@ std::cout<<*gen<<std::endl;
 
             Iteration(ROUNDresidues, ROUNDdomains, foundInconsistentSystem);
         }
-#else
-        template <class Function, class PrimeIterator, class Domain, class ElementContainer>
-        void solve_with_prime(PrimeIterator& m_primeiters, Function& Iteration, Domain& ROUNDdomains,
-                              ElementContainer& ROUNDresidues, CRABase& vBuilders)
-        {
 
-            ++m_primeiters;
-Givaro::IntPrimeDom _IPD; //!< empty struct dealing with primality.
-            while (vBuilders.noncoprime(*m_primeiters)) ++m_primeiters;
-if(!_IPD.isprime(*m_primeiters)) std::cerr<<*m_primeiters<<" is not a prime number ! ********************* "<<std::endl;
-            ROUNDdomains = Domain(*m_primeiters);
 
-            Iteration(ROUNDresidues, ROUNDdomains);
-        }
-#endif
-
-#if 1 //=============================================Task based ===========================================
-#if 1 // ---------------------------- Paladin impl ----------------------------
-        /*
-        #define TASK(M, I)                             \
-            PRAGMA_OMP_IMPL(omp task M)           \
-            {I;}
-
-        #define FORBLOCK1D(iter, m, Helper, Args...)                                       \
-            { FFLAS::ForStrategy1D<std::remove_const<decltype(m)>::type, typename decltype(Helper)::Cut, typename
-        decltype(Helper)::Param > iter(m, Helper); \
-              for(iter.initialize(); !iter.isTerminated(); ++iter){ {Args;}  } }
-
-        #define FOR1D(i, m, Helper, Args...)                             \
-            FORBLOCK1D(_internal_iterator, m, Helper,                           \
-                TASK( , \
-                     {for(auto i=_internal_iterator.begin(); i!=_internal_iterator.end(); ++i) \
-                         { Args; } });)                                         \
-                WAIT;
-
-        // overload of SPLITTER
-        #define splitting_0() FFLAS::ParSeqHelper::Parallel<FFLAS::CuttingStrategy::Block,FFLAS::StrategyParameter::Threads>()
-        #define splitting_1(a) FFLAS::ParSeqHelper::Parallel<FFLAS::CuttingStrategy::Block,FFLAS::StrategyParameter::Threads>(a)
-        #define splitting_2(a,c) FFLAS::ParSeqHelper::Parallel<FFLAS::CuttingStrategy::Block,c>(a)
-        #define splitting_3(a,b,c) FFLAS::ParSeqHelper::Parallel<b,c>(a)
-
-        // parallel region
-        #define PAR_BLOCK  PRAGMA_OMP_IMPL(omp parallel)   \
-            PRAGMA_OMP_IMPL(omp single)
-        */
 
         template <class pFunc, class Function, class Domain, class ElementContainer, class PrimeGen>
         void compute_task(pFunc& pF, std::vector<int>& m_primeiters, Function& Iteration, std::vector<Domain>& ROUNDdomains,
@@ -262,10 +183,6 @@ if(!_IPD.isprime(*m_primeiters)) std::cerr<<*m_primeiters<<" is not a prime numb
             // LinBox::PrimeIterator<LinBox::IteratorCategories::DeterministicTag> gen;
             long Niter = std::ceil(1.442695040889 * B / (double)(gen.getBits() - 1));
 
-            // std::cout<<"============================= Niter:= "<<Niter<<" ================================"<<std::endl;
-            /*@Try PlanB as follows:
-            *  As the Niter is known then first generate Niter prime numbers and put them into a vector then start paladin impl
-            */
 
             std::vector<int> failedPrimesIndices; // Failed due to inconsistent systems mod p
 
@@ -311,122 +228,7 @@ if(!_IPD.isprime(*m_primeiters)) std::cerr<<*m_primeiters<<" is not a prime numb
                 this->Builder_.progress(ROUNDdomains[j], ROUNDresidues[j]);
             }
         }
-#else // ---------------------------- OMP impl ----------------------------
-        template <class pFunc, class Function, class PrimeIterator, class Domain, class ElementContainer>
-        void compute_task(pFunc& pF, std::vector<PrimeIterator>& m_primeiters, Function& Iteration,
-                          std::vector<Domain>& ROUNDdomains, std::vector<ElementContainer>& ROUNDresidues,
-                          std::vector<CRABase>& vBuilders)
-        {
 
-            long Niter = std::ceil(1.442695040889 * B / (double)(m_primeiters[0].getBits() - 1));
-            int NN;
-#pragma omp parallel
-#pragma omp single
-            NN = NUM_THREADS;
-
-            solve_with_prime(m_primeiters[0], Iteration, ROUNDdomains[0], ROUNDresidues[0], vBuilders[0]);
-            this->Builder_.initialize(ROUNDdomains[0], ROUNDresidues[0]);
-
-#pragma omp parallel num_threads(NN)
-#pragma omp single
-            for (auto j = 1; j < Niter; j++) {
-#pragma omp task
-                solve_with_prime(m_primeiters[omp_get_thread_num()], Iteration, ROUNDdomains[j], ROUNDresidues[j],
-                                 vBuilders[omp_get_thread_num()]);
-            }
-
-            //            this->Builder_.initialize( ROUNDdomains[0], ROUNDresidues[0]);
-
-            for (auto j = 1; j < Niter; j++) {
-
-                this->Builder_.progress(ROUNDdomains[j], ROUNDresidues[j]);
-
-            }
-
-        }
-#endif // --------------------------------------------------------------------------
-
-#else //=============================================For loop based ===========================================
-#if 0 // ---------------------------- Paladin impl ----------------------------
-        template<class pFunc, class Function, class PrimeIterator, class Domain, class ElementContainer>
-        void compute_task(pFunc& pF, std::vector<PrimeIterator>& m_primeiters,
-                          Function& Iteration, std::vector<Domain>& ROUNDdomains,
-                          std::vector<ElementContainer>& ROUNDresidues, std::vector<CRABase>& vBuilders)
-        {
-
-            long Niter=std::ceil(1.442695040889*B/(double)(m_primeiters[0].getBits()-1));
-            int NN;
-            PAR_BLOCK{ NN=NUM_THREADS; }
-
-            FFLAS::ParSeqHelper::Parallel<FFLAS::CuttingStrategy::Row,FFLAS::StrategyParameter::Grain> H;
-
-            if(NN>Niter){
-SYNCH_GROUP(
-	            PARFORBLOCK1D(k,Niter,H,
-                    solve_with_prime(m_primeiters[k], Iteration, ROUNDdomains[k], ROUNDresidues[k], vBuilders[k]);
-                );
-)
-
-            }else{
-SYNCH_GROUP(
-	            PARFORBLOCK1D(k,NN,H,
-	                for(auto j=k*(Niter/NN);j<(k+1)*(Niter/NN);j++)
-                        solve_with_prime(m_primeiters[k], Iteration, ROUNDdomains[j], ROUNDresidues[j], vBuilders[k]);
-                );
-)
-                if(Niter%NN>0){
-SYNCH_GROUP(
-	                PARFORBLOCK1D(k,Niter%NN,H,
-                        solve_with_prime(m_primeiters[k], Iteration, ROUNDdomains[k+NN*(Niter/NN)], ROUNDresidues[k+NN*(Niter/NN)], vBuilders[k]);
-                    );
-)
-                }
-
-            }
-
-
-            this->Builder_.initialize( ROUNDdomains[0], ROUNDresidues[0]);
-
-            for(auto j=1;j<Niter;j++)
-                {
-
-                        this->Builder_.progress( ROUNDdomains[j], ROUNDresidues[j]);
-
-                }
-
-        }
-#else // ---------------------------- OMP impl ----------------------------
-        template <class pFunc, class Function, class PrimeIterator, class Domain, class ElementContainer>
-        void compute_task(pFunc& pF, std::vector<PrimeIterator>& m_primeiters, Function& Iteration,
-                          std::vector<Domain>& ROUNDdomains, std::vector<ElementContainer>& ROUNDresidues,
-                          std::vector<CRABase>& vBuilders)
-        {
-
-            long Niter = std::ceil(1.442695040889 * B / (double)(m_primeiters[0].getBits() - 1));
-            int NN;
-#pragma omp parallel
-#pragma omp single
-            NN = NUM_THREADS;
-
-            solve_with_prime(m_primeiters[0], Iteration, ROUNDdomains[0], ROUNDresidues[0], vBuilders[0]);
-            this->Builder_.initialize(ROUNDdomains[0], ROUNDresidues[0]);
-
-#pragma omp parallel for schedule(dynamic, 1) num_threads(NN)
-            for (auto j = 1; j < Niter; j++) {
-                solve_with_prime(m_primeiters[omp_get_thread_num()], Iteration, ROUNDdomains[j], ROUNDresidues[j],
-                                 vBuilders[omp_get_thread_num()]);
-            }
-
-            //            this->Builder_.initialize( ROUNDdomains[0], ROUNDresidues[0]);
-
-            for (auto j = 1; j < Niter; j++) {
-
-                this->Builder_.progress(ROUNDdomains[j], ROUNDresidues[j]);
-            }
-        }
-
-#endif // ---------------------------------------------------------------
-#endif //=============================================================================================
 
         template <class Container, class Function, class PrimeIterator>
         Container& operator()(Container& res, Integer& den, Function& Iteration, PrimeIterator& primeiter)
@@ -441,14 +243,8 @@ SYNCH_GROUP(
 
             // commentator().stop ("done", NULL, "mmcrait");
 
-            // return this->Builder_.result(res,den);
-this->Builder_.result(res, den);
-//if(res.getEntry(0).bitsize() > primeiter.getBits()) std::cerr << res.getEntry(res.size()) << std::endl;
-std::cerr << ">>>>res: " << std::endl; 
-for(long j=0;j<res.size();j++) std::cerr <<">>>>"<< res.getEntry(j) << std::endl;
-std::cerr << " >>>>>>>>>>>>> den:= "<< den <<" <<<<<<<<<<<<<<<"<< std::endl; 
+             return this->Builder_.result(res,den);
 
-            return res;
         }
     };
 }
