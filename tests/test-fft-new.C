b76e0add40316b227a955faa6c79e2f6b01eaaec
Cyril Bouvier <cyril.bouvier@lirmm.fr>
2019-02-15 10:52:20 +0100

    Update test-fft-new to test FFT with ModularExtended

--- tests/test-fft-new.C
+++ tests/test-fft-new.C
@@ -28,20 +28,24 @@
 #include "linbox/randiter/random-fftprime.h"
 #include "linbox/ring/modular.h"
 
+#include <givaro/modular-extended.h>
+
 #include <functional>
 #include <iostream>
 #include <string>
 #include <vector>
 
-using namespace std; 
+using namespace std;
 using namespace LinBox;
+using Givaro::Modular;
+using Givaro::ModularExtended;
 
 /* For pretty printing type */
-template<typename T> const char *TypeName();
-template<template <typename S> typename T> const char *TypeName();
+template<typename...> const char *TypeName();
+template<template <typename...> class> const char *TypeName();
 
 #define REGISTER_TYPE_NAME(type) \
-	template<> const char *TypeName<type>(){return #type;}
+    template<> const char *TypeName<type>(){return #type;}
 
 REGISTER_TYPE_NAME(double);
 REGISTER_TYPE_NAME(uint16_t);
@@ -51,168 +55,174 @@ REGISTER_TYPE_NAME(uint128_t);
 REGISTER_TYPE_NAME(NoSimd);
 REGISTER_TYPE_NAME(Simd128);
 REGISTER_TYPE_NAME(Simd256);
+REGISTER_TYPE_NAME(Modular);
+REGISTER_TYPE_NAME(ModularExtended);
 
 /******************************************************************************/
 template<typename Field>
 struct Checker {
-	typedef typename Field::Element Elt;
-	typedef AlignedAllocator<Elt, Alignment::DEFAULT> _Alignement;
-	typedef typename std::vector<Elt, _Alignement> EltVector;
+    typedef typename Field::Element Elt;
+    typedef AlignedAllocator<Elt, Alignment::DEFAULT> _Alignement;
+    typedef typename std::vector<Elt, _Alignement> EltVector;
 
-	const Field& _F;
-	size_t _k;
-	size_t _n;
-	FFT_init<Field> _fft;
+    const Field& _F;
+    size_t _k;
+    size_t _n;
+    FFT_init<Field> _fft;
 
-	/* ctor */
-	Checker (const Field& F, size_t k) : _F(F), _k(k), _n(1<<k), _fft(F,k) {
-	}
+    /* ctor */
+    Checker (const Field& F, size_t k) : _F(F), _k(k), _n(1<<k), _fft(F,k) {
+    }
 
-	/* compute r = P evaluated at x using Horner method */
-	void horner_eval (Elt& r, EltVector& P, Elt& x) {
-		_F.assign (r, _F.zero);
-		for (auto ptr = P.rbegin(); ptr != P.rend(); ptr++ ) {
-			_F.mulin (r, x);
-			_F.addin (r, *ptr);
-		}
-	}
+    /* compute r = P evaluated at x using Horner method */
+    void horner_eval (Elt& r, EltVector& P, Elt& x) {
+        _F.assign (r, _F.zero);
+        for (auto ptr = P.rbegin(); ptr != P.rend(); ptr++ ) {
+            _F.mulin (r, x);
+            _F.addin (r, *ptr);
+        }
+    }
 
-	/* return i with its first _k bits reversed */
-	size_t bitreversed (size_t i) {
-		size_t r = 0;
-		for (size_t j = 0; j < _k; j++, i>>=1)
-		{
-			r <<= 1;
-			r |= i & 1; 
-		}
-		return r;
-	}
+    /* return i with its first _k bits reversed */
+    size_t bitreversed (size_t i) {
+        size_t r = 0;
+        for (size_t j = 0; j < _k; j++, i>>=1)
+        {
+            r <<= 1;
+            r |= i & 1;
+        }
+        return r;
+    }
 
-	/* check DIF and DIT */
-	template<template <typename Elt> typename SimdType>
-	bool actual_check (const EltVector& in, const EltVector& in_br,
-							const EltVector& out, const EltVector& out_br) {
-		FFT_algorithms<Field, SimdType<Elt>> fft_algo (_fft);
-		EltVector v(_n);
-		string s;
-		s.append ("<"); s.append (TypeName<SimdType>()); s.append ("> ");
-		s.append (string (80-(s.size()+16), '.'));
+    /* check DIF and DIT */
+    template<template <typename Elt> class SimdType>
+    bool actual_check (const EltVector& in, const EltVector& in_br,
+                            const EltVector& out, const EltVector& out_br) {
+        FFT_algorithms<Field, SimdType<Elt>> fft_algo (_fft);
+        EltVector v(_n);
+        string s;
+        s.append ("<"); s.append (TypeName<SimdType>()); s.append ("> ");
+        s.append (string (80-(s.size()+16), '.'));
 
-		/* DIF : natural order => bitreversed order */
-		v = in;
-		fft_algo.DIF (v.data());
-		bool bf = equal (v.begin(), v.end(), out_br.begin());
-		cout << "     DIF" << s << (bf ? " success" : " failure") << endl;
+        /* DIF : natural order => bitreversed order */
+        v = in;
+        fft_algo.DIF (v.data());
+        bool bf = equal (v.begin(), v.end(), out_br.begin());
+        cout << "     DIF" << s << (bf ? " success" : " failure") << endl;
 
-		/* DIT : bitreversed order => natural order */
-		v = in_br;
-		fft_algo.DIT (v.data());
-		bool bt = equal (v.begin(), v.end(), out.begin());
-		cout << "     DIT" << s << (bt ? " success" : " failure") << endl;
+        /* DIT : bitreversed order => natural order */
+        v = in_br;
+        fft_algo.DIT (v.data());
+        bool bt = equal (v.begin(), v.end(), out.begin());
+        cout << "     DIT" << s << (bt ? " success" : " failure") << endl;
 
-		bool b = bt & bf;
-		return b;
-	}
-	
-	/* draw random vector and check DIF and DIT for all available SIMD implem */
-	bool check (unsigned long seed) {
-		bool passed = true;
-		EltVector in(_n), in_br(_n), out(_n), out_br(_n);
+        bool b = bt & bf;
+        return b;
+    }
 
-		/* Generate random input */
-		typename Field::RandIter Gen (_F, 0, seed+_k); /*0 has no meaning here*/
-		for (auto v = in.begin(); v < in.end(); v++)
-			Gen.random (*v);
+    /* draw random vector and check DIF and DIT for all available SIMD implem */
+    bool check (unsigned long seed) {
+        bool passed = true;
+        EltVector in(_n), in_br(_n), out(_n), out_br(_n);
 
-		/* Compute the out vector using a naive polynomial evaluation and set
-		 * the bitreversed version of in and out */
-		Elt x(1);
-		for (size_t i = 0; i < _n; _F.mulin (x, _fft._w), i++) {
-			size_t i_br = bitreversed (i);
-			in_br[i_br] = in[i];
-			horner_eval (out[i], in, x);
-			out_br[i_br] = out[i];
-		}
+        /* Generate random input */
+        typename Field::RandIter Gen (_F, 0, seed+_k); /*0 has no meaning here*/
+        for (auto v = in.begin(); v < in.end(); v++)
+            Gen.random (*v);
 
-		/* NoSimd */
-		passed &= actual_check<NoSimd> (in, in_br, out, out_br);
+        /* Compute the out vector using a naive polynomial evaluation and set
+         * the bitreversed version of in and out */
+        Elt x(1);
+        for (size_t i = 0; i < _n; _F.mulin (x, _fft._w), i++) {
+            size_t i_br = bitreversed (i);
+            in_br[i_br] = in[i];
+            horner_eval (out[i], in, x);
+            out_br[i_br] = out[i];
+        }
 
-		/* Simd128 */
+        /* NoSimd */
+        passed &= actual_check<NoSimd> (in, in_br, out, out_br);
+
+        /* Simd128 */
 #if defined(__FFLASFFPACK_HAVE_SSE4_1_INSTRUCTIONS)
-		if (Simd128<Elt>::vect_size == 4 || Simd128<Elt>::vect_size == 8)
-			passed &= actual_check<Simd128> (in, in_br, out, out_br);
+        if (Simd128<Elt>::vect_size == 4 || Simd128<Elt>::vect_size == 8)
+            passed &= actual_check<Simd128> (in, in_br, out, out_br);
 #endif
 
-		/* Simd256 */
+        /* Simd256 */
 #if defined(__FFLASFFPACK_HAVE_AVX2_INSTRUCTIONS)
-		if (Simd256<Elt>::vect_size == 4 || Simd256<Elt>::vect_size == 8)
-			passed &= actual_check<Simd256> (in, in_br, out, out_br);
+        if (Simd256<Elt>::vect_size == 4 || Simd256<Elt>::vect_size == 8)
+            passed &= actual_check<Simd256> (in, in_br, out, out_br);
 #endif
 
-		return passed;
-	}
+        return passed;
+    }
 };
 
 /* Test FFT on polynomial with coefficients in Modular<T1,T2> with a random
  * prime p with the required number of bits and such that 2^k divides p-1
  */
-template<typename T1, typename T2>
+template<template<typename, typename...> class ModImplem, typename Elt, typename... C>
 bool test_one_modular_implem (uint64_t bits, size_t k, unsigned long seed)
 {
-	typedef typename Givaro::Modular<T1, T2> ModImplem;
 
-	RandomFFTPrime RandomGen (1<<bits, seed);
-	T1 p = (T1) RandomGen.randomPrime (k);
-	ModImplem GFp(p);
-	
-	cout << endl << string (80, '*') << endl;
-	cout << "Test FFT with Modular<" << TypeName<T1>() << ", ";
-	cout << TypeName<T2>() << ">, p=" << GFp._p << " (" << bits << " bits, 2^";
-	cout << k << " divides p-1)" << endl;
+    RandomFFTPrime RandomGen (integer(1)<<bits, seed);
+    integer p = RandomGen.randomPrime (k);
+    ModImplem<Elt, C...> GFp ((Elt) p);
 
-	bool b = true;
-	for (size_t kc = 1; kc <= k; kc++) {
-		cout << "** with n=2^" << kc << endl;
-		Checker<ModImplem> C(GFp, kc);
-		b &= C.check (seed);
-	}
+    cout << endl << string (80, '*') << endl;
+    cout << "Test FFT with " << TypeName<ModImplem>() << "<" << TypeName<Elt>();
+    if (sizeof...(C) > 0)
+        cout << ", " << TypeName<C...>();
+    cout << ">, p=" << GFp.cardinality() << " (" << bits << " bits, 2^" << k;
+    cout << " divides p-1)" << endl;
 
-	return b;
+    bool b = true;
+    for (size_t kc = 1; kc <= k; kc++) {
+        cout << "** with n=2^" << kc << endl;
+        Checker<ModImplem<Elt, C...>> Test(GFp, kc);
+        b &= Test.check (seed);
+    }
+
+    return b;
 }
 
 /******************************************************************************/
 /************************************ main ************************************/
 /******************************************************************************/
 int main (int argc, char *argv[]) {
-	bool pass = true;
-	unsigned long seed = time (NULL);
+    bool pass = true;
+    unsigned long seed = time (NULL);
 
-	Argument args[] = {
-		{ 's', "-s seed", "set the seed.", TYPE_INT, &seed },
-		END_OF_ARGUMENTS
-	};
+    Argument args[] = {
+        { 's', "-s seed", "set the seed.", TYPE_INT, &seed },
+        END_OF_ARGUMENTS
+    };
 
-	parseArguments (argc, argv, args);
+    parseArguments (argc, argv, args);
 
-	cout << "# To rerun this test: test-fft-new -s " << seed << endl;
-	cout << "# seed = " << seed << endl;
+    cout << "# To rerun this test: test-fft-new -s " << seed << endl;
+    cout << "# seed = " << seed << endl;
 
-	/* Test with Modular<double, double>, and a 22-bit prime and k=10 */
-	pass &= test_one_modular_implem<double, double> (22, 10, seed);
+    /* Test with Modular<double>, and a 22-bit prime and k=10 */
+    pass &= test_one_modular_implem<Modular,double> (22, 10, seed);
 
-	/* Test with Modular<uint16_t,uint32_t>, and 11-bit prime and k=7 */
-	pass &= test_one_modular_implem<uint16_t, uint32_t> (11, 7, seed);
+    /* Test with ModularExtended<double>, and a 51-bit prime and k=11 */
+    pass &= test_one_modular_implem<ModularExtended,double> (51, 11, seed);
 
-	/* Test with Modular<uint32_t, uint64_t>, and 27-bit prime and k=10 */
-	pass &= test_one_modular_implem<uint32_t, uint64_t> (27, 10, seed);
+    /* Test with Modular<uint16_t,uint32_t>, and 11-bit prime and k=7 */
+    pass &= test_one_modular_implem<Modular,uint16_t,uint32_t> (11, 7, seed);
+
+    /* Test with Modular<uint32_t, uint64_t>, and 27-bit prime and k=10 */
+    pass &= test_one_modular_implem<Modular,uint32_t,uint64_t> (27, 10, seed);
 
 #ifdef __FFLASFFPACK_HAVE_INT128
-	/* Test with Modular<uint64_t,uint128_t>, and a 59-bit prime and k=11 */
-	pass &= test_one_modular_implem<uint64_t, uint128_t> (59, 11, seed);
+    /* Test with Modular<uint64_t,uint128_t>, and a 59-bit prime and k=11 */
+    pass &= test_one_modular_implem<Modular,uint64_t,uint128_t> (59, 11, seed);
 #endif
 
-	cout << endl << "Test " << (pass ? "passed" : "failed") << endl;
-	return pass ? 0 : 1;
+    cout << endl << "Test " << (pass ? "passed" : "failed") << endl;
+    return pass ? 0 : 1;
 }
 
 // Local Variables:
