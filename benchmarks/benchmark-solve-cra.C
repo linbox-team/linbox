164671065efacc4bf39c3d02f28c64344dc9f062
Alexis Breust <alexis.breust@gmail.com>
2019-03-11 11:08:25 +0100

    Finished review changes

--- benchmarks/benchmark-solve-cra.C
+++ benchmarks/benchmark-solve-cra.C
@@ -20,11 +20,6 @@
  * ========LICENCE========
  */
 
-/*! @file tests/test-solveCRA.C
-* @ingroup benchmarks
-* @brief Testing the MPI parallel/serial rational solver
-*/
-#include <unistd.h>
 #include "givaro/modular.h"
 #include "givaro/zring.h"
 #include "linbox/linbox-config.h"
@@ -37,10 +32,11 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 using namespace LinBox;
 
-using Field = Givaro::ZRing<Integer>;
+using Ring = Givaro::ZRing<Integer>;
 
 #if defined(__LINBOX_HAVE_MPI)
 #define getWTime(...) MPI_Wtime(__VA_ARGS__);
@@ -48,55 +44,51 @@ using Field = Givaro::ZRing<Integer>;
 #define getWTime(...) omp_get_wtime(__VA_ARGS__);
 #endif
 
-template <class Field, class Matrix>
-static bool checkResult(Matrix& A, BlasVector<Field>& B, BlasVector<Field>& X, Integer& d)
+template <class Matrix>
+static bool checkResult(Matrix& A, BlasVector<Ring>& B, BlasVector<Ring>& X, Integer& d)
 {
-    Field ZZ;
-    BlasVector<Field> B2(ZZ, A.coldim());
-    BlasVector<Field> B3(ZZ, A.coldim());
+    const Ring& ZZ = A.field();
+    BlasVector<Ring> B2(ZZ, A.coldim());
+    BlasVector<Ring> B3(ZZ, A.coldim());
     A.apply(B2, X);
-   
-    for (size_t j = 0; j < B.size(); ++j) {
-        ZZ.mul(B3[j],B[j],d);
+
+    VectorDomain<Ring> VD(ZZ);
+    VD.mul(B3, B, d);
+
+    if (!VD.areEqual(B2, B3)) {
+        std::cerr << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << std::endl;
+        std::cerr << "               The solution of solveCRA is incorrect                " << std::endl;
+        std::cerr << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
+        return false;
     }
 
-    for (size_t j = 0; j < A.coldim(); ++j) {
-        if (!ZZ.isZero(B2[j]-B3[j])) {
-            std::cerr << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << std::endl;
-            std::cerr << "               The solution of solveCRA is incorrect                " << std::endl;
-            std::cerr << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
-            return false;
-        }
-    }
     return true;
 }
 
-template <class Field, class Matrix, class RandIterator>
-void genData(Field& F, RandIterator& RandIter, Matrix& A,  BlasVector<Field>& B)
+template <class Matrix, class RandIterator>
+void genData(Ring& ZZ, RandIterator& randIter, Matrix& A, BlasVector<Ring>& B)
 {
 
-    LinBox::RandomDenseMatrix<typename Field::RandIter, Field> RDM(F, RandIter);
+    LinBox::RandomDenseMatrix<typename Ring::RandIter, Ring> RDM(ZZ, randIter);
     RDM.randomFullRank(A);
-    B.random(RandIter);
+    B.random(randIter);
 }
 
-bool benchmark(size_t niter, BlasVector<Field>& x, BlasMatrix<Field>& A, BlasVector<Field>& B, Communicator* Cptr)
+bool benchmark(size_t niter, BlasVector<Ring>& x, BlasMatrix<Ring>& A, BlasVector<Ring>& B, Communicator& communicator)
 {
-    Field::Element d;
+    Ring::Element d;
 
     double startTime = getWTime();
-    solveCRA(x, d, A, B, RingCategories::IntegerTag(), Method::DenseElimination(), Cptr);
+    solveCRA(x, d, A, B, RingCategories::IntegerTag(), Method::DenseElimination(), &communicator);
 
     bool ok = false;
-    if (Cptr->master()) {
+    if (communicator.master()) {
         double endTime = getWTime();
-        std::cout << "CPU time (seconds): " << (endTime - startTime)/double(niter) << std::endl;
+        std::cout << "CPU time (seconds): " << (endTime - startTime) / double(niter) << std::endl;
         ok = checkResult(A, B, x, d);
     }
 
-#ifdef __LINBOX_HAVE_MPI
-    MPI_Bcast(&ok, 1, MPI_C_BOOL, 0, MPI_COMM_WORLD);
-#endif
+    communicator.bcast(ok, 0);
     return ok;
 }
 
@@ -112,48 +104,40 @@ int main(int argc, char** argv)
     size_t bits = 10;
     size_t niter = 1;
     size_t n = 100;
+    bool loop = false;
 
     static Argument args[] = {{'n', "-n N", "Set column and row dimension of test matrices to N.", TYPE_INT, &n},
                               {'b', "-b B", "Set the maximum number of digits of integers to generate.", TYPE_INT, &bits},
                               {'i', "-i I", "Set the number of times to do the random unit tests.", TYPE_INT, &niter},
                               {'s', "-s SEED", "Set the seed for randomness (random if negative).", TYPE_INT, &seed},
+                              {'l', "-l", "Infinite loop (ignoring -i).", TYPE_BOOL, &loop},
                               END_OF_ARGUMENTS};
     parseArguments(argc, argv, args);
 
-    if (seed < 0) {
-        seed = time(NULL);
+    // As only master uses the seed, there is no need to broadcast it
+    if (communicator.master()) {
+        if (seed < 0) {
+            seed = time(NULL);
+        }
+        srand(seed);
     }
-    srand(seed);
 
-/*    
-#ifdef __LINBOX_HAVE_MPI
-    MPI_Bcast(&seed, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&bits, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&niter, 1, MPI_INT, 0, MPI_COMM_WORLD);
-#endif
-*/
+    Ring ZZ;
+    DenseMatrix<Ring> A(ZZ, n, n);
+    BlasVector<Ring> b(ZZ, A.coldim());
+    BlasVector<Ring> x(ZZ, A.coldim());
 
-    Field ZZ;
-    DenseMatrix<Field> A(ZZ, n, n);
-    BlasVector<Field> b(ZZ, A.coldim());
-    BlasVector<Field> x(ZZ, A.coldim());
-    
-    Field::RandIter RI(ZZ, bits, seed);
-    
     bool ok = true;
-    for (size_t j = 0u; j < niter; j++) {
-
+    for (size_t j = 0u; loop | (j < niter); j++) {
         if (communicator.master()) {
-            genData(ZZ, RI, A, b);
+            Ring::RandIter randIter(ZZ, bits, seed);
+            genData(ZZ, randIter, A, b);
         }
 
-#ifdef __LINBOX_HAVE_MPI
         communicator.bcast(A, 0);
         communicator.bcast(b, 0);
-#endif
 
-        ok = benchmark(niter, x, A, b, &communicator);
+        ok = benchmark(niter, x, A, b, communicator);
         if (!ok) break;
 
         ++seed;
--- linbox/algorithms/cra-mpi.h
+++ linbox/algorithms/cra-mpi.h
@@ -203,3 +203,11 @@ namespace LinBox {
         }
     };
 }
+
+// Local Variables:
+// mode: C++
+// tab-width: 4
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// End:
+// vim:sts=4:sw=4:ts=4:et:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s
