92ab216c2db13c297fa66dcffce3e776c3743f5e
Alexis Breust <alexis.breust@gmail.com>
2019-05-03 16:01:22 +0200

    More doc before implem

--- linbox/algorithms/dixon-rns-solver.h
+++ linbox/algorithms/dixon-rns-solver.h
@@ -46,23 +46,27 @@ namespace LinBox {
      *      (iii)   Rational reconstruct with c = c0 + c1 p + ... + ck p^{k-1} (over the integers)
      *
      * The RNS part:
-     *      (i)     Use p = p1p2...pl with an arbitrary l
-     *      (ii)    We can compute the residues for each pj by having ci expressed in an RNS system.
+     *      (i)     Use p = p0p1...p{lp-1} with an arbitrary lp and (q0, q1, ..., q{lq-1}) also primes.
+     *      (ii)    We now do our computation in a RNS system (p0, ..., p{lp-1}, q0, ..., q{lq-1}):
     *                               r = b
      *                              for i = 0 .. k-1:
-     *                              |   for j = 0 .. l-1:
-     *                              |   |   ci[j] = B r mod pj
+     *                              |   for j = 0 .. lq-1:
+     *                              |   |   ci[qj] = Bj r mod qj
      *                              |   (Q, R) = such that r = pQ + R with |R| < p
      *                              |   r = Q + (R - A ci) / p      < Matrix-vector multiplication done in RNS domain
      *                              |                                 and final addition over ZZ
-     *              /!\ @fixme I do not get it, how can 1 / p be computed in the RNS system, or is it just R - A ci?
+     *              @note (R - A ci) / p can be computed in a RNS system.
+     *              We know that (R - A ci) is divisible by p,
+     *              so its representation is 0 on all lp first terms of the representation, meaning
+     *              we just need representation of ci mod (q0, ..., q{lq-1}).
+     *              For the division part, we just have to multiply the RNS representation of (R - A ci) by
+     *              (1/p) mod (q0, ..., q{lq-1}).
      *              /!\ @fixme The paper does not talk about matrix-matrix multiplication,
      *              but instead about exploiting RNS.
-     *      (iii)   Having solved the system for each pj, we first RNS-reconstruct the solution mod p
-     *              before rational reconstruction.
+     *      (iii)   We first RNS-reconstruct the solution before rational reconstruction.
      *
      * One can configure how many primes are used with `Method::DixonRNS.primeBaseLength`.
-     * According to the paper, a value of l = 2 (ln(n) + log2(||A||)) or without the factor 2
+     * According to the paper, a value of lp = 2 (ln(n) + log2(||A||)) or without the factor 2
      * can be used, but it depends on the problem, really.
      */
     template <class Field, class Ring, class PrimeGenerator>
