5bb101fee136bd264fb75f4232bad3285aa0a327
Alexis Breust <alexis.breust@gmail.com>
2019-05-22 15:29:09 +0200

    Initializing up to inverse of A mod pi

--- linbox/algorithms/multi-mod-lifting-container.h
+++ linbox/algorithms/multi-mod-lifting-container.h
@@ -69,9 +69,10 @@ namespace LinBox {
         using Field = _Field;
         using PrimeGenerator = _PrimeGenerator;
 
-        using IElement = typename _Ring::Element;
+        using IElement = typename Ring::Element;
         using IMatrix = DenseMatrix<_Ring>;
         using IVector = DenseVector<_Ring>;
+        using FElement = typename Field::Element;
         using FMatrix = DenseMatrix<_Field>;
         using FVector = DenseVector<_Field>;
 
@@ -83,23 +84,70 @@ namespace LinBox {
         MultiModLiftingContainer(const Ring& ring, PrimeGenerator primeGenerator, const IMatrix& A,
                                  const IVector& b, const Method::DixonRNS& m)
             : _ring(ring)
-            , _r(_ring)
-            , _c(_field)
+            , _n(A.rowdim())
         {
-            // @fixme Compute hadamard and such
+            linbox_check(A.rowdim() == A.coldim());
 
-            // @note From baseClass, we have _length = log2(2 * N * D)
+            A.write(std::cout << "A: ", Tag::FileFormat::Maple) << std::endl;
+            std::cout << "b: " << b << std::endl;
 
             // @fixme Have l = log(||A||) + log(n) or so
+            _l = 2;
+            std::cout << "l: " << _l << std::endl;
 
-            // @fixme Initialize fields _F[i]
+            // Generating primes
+            IElement iTmp;
+            _ring.assign(_p, _ring.one);
+            for (auto i = 0u; i < _l; ++i) {
+                // @fixme Ensure that all primes are different
+                // @fixme Take into account bestBitSize!
+                _primes.emplace_back(*primeGenerator);
+                _fields.emplace_back(_primes.back());
+                _ring.init(iTmp, _primes.back());
+                _ring.mulin(_p, iTmp);
 
-            // Ap[0] = A mod p[0]
-            // Ap[1] = A mod p[1]
+                std::cout << "primes[" << i << "]: " << Integer(_primes.back()) << std::endl;
 
-            // B[0] = inv(Ap[0]) mod p[0]
-            // B[1] = inv(Ap[1]) mod p[1]
-            // @fixme How?
+                ++primeGenerator;
+            }
+
+            std::cout << "p: " << _p << std::endl;
+
+            // Compute how many iterations are needed
+            auto hb = RationalSolveHadamardBound(A, b);
+            double pLog = Givaro::logtwo(_p);
+            _k = std::ceil((1.0 + hb.numLogBound + hb.denLogBound)
+                           / pLog); // log2(2 * N * D) / log2(p)
+            std::cout << "k: " << _k << std::endl;
+
+            // @fixme Fact is RationalReconstruction which needs numbound and denbound
+            // expects them to be in non-log...
+            _ring.init(_numbound, Integer(1) << static_cast<uint64_t>(std::ceil(hb.numLogBound)));
+            _ring.init(_denbound, Integer(1) << static_cast<uint64_t>(std::ceil(hb.denLogBound)));
+
+            // Initialize all inverses
+            // @fixme Somehow, the inverse mod p within DixonSolver<Dense> was already computed,
+            // and pass through to the lifting container. Here, we can't do that, because p is
+            // bigger than what DixonSolver<Dense> thought about it. So there might be a lot of
+            // computation done there that is completely useless when using this container. Meaning
+            // that we need a RNSDixonSolver<Dense>.
+            {
+                for (const auto& F : _fields) {
+                    BlasMatrixDomain<Field> bmd(F);
+                    auto Bpi = std::make_unique<FMatrix>(F, _n, _n);
+
+                    // @fixme Taken for rational-solver.inl. BETTER USE REBIND!!!
+                    for (size_t i = 0; i < _n; ++i) {
+                        for (size_t j = 0; j < _n; ++j) {
+                            F.init(Bpi->refEntry(i, j), A.getEntry(i, j));
+                        }
+                    }
+
+                    bmd.invin(*Bpi);
+                    Bpi->write(std::cout << "B mod " << Integer(F.characteristic()) << ": ", Tag::FileFormat::Maple) << std::endl;
+                    _B.emplace_back(std::move(Bpi));
+                }
+            }
 
             // @note As _r is row major, we store each ri on each row.
             // So that r[i] = current residue for p[i].
@@ -115,48 +163,6 @@ namespace LinBox {
             // @fixme Allocate c
 
             // @todo Set up an RNS system
-        }
-
-        // @fixme USELESS?
-        IVector& nextdigit(IVector& digit, const IVector& residu) const
-        {
-            // @fixme The residu can't be r, here!
-            // So the overall does a lot more job than it needs.
-            // See below for the solution.
-
-            // @fixme With this design, are we forced to CRT_Reconstruct each ci?
-            // Is this bad?
-            // If we don't want that, we need to not extent LiftingContainerBase,
-            // and reimplement some of the behavior.
-            // Because the only thing needed to user API (rational reconstruction)
-            // is bool next (IVector& digit) from iterator.
-
-            /*  for i = 1 .. l:
-             *  |   (Qi, Ri) = such that ri = pi Qi + Ri with |Ri| < pi
-             *  |   ci = Bi Ri mod pi                   < Matrix-vector in Z/pZ
-             *  V = [R1|...|Rl] - A [c1|...|cl]         < Matrix-matrix in ZZ
-             *  for i = 1 .. l:
-             *  |   ri = Qi + (Vi / pi)
-             */
-
-            // @fixme Could be parallel!
-            // for (auto i = 0u; i < l; ++i) {
-            //     Hom<Ring, Field> hom(_ring, _F[i]);
-
-            //     // @fixme How to do euclidian division?
-            //     // ri = pi Qi + Ri
-
-            //     // @todo If R might already be a field element
-            //     _B[i]->apply(_c[i], hom.convert(_R[i]));
-
-            //     // @todo Convert _c[i] to RNS
-            // }
-
-            // @fixme How can we do A [c1|...|cl] in ZZ if the ci are in the fields?
-
-            // @fixme Compute the next residue!
-
-            return digit;
         }
 
         // --------------------------
@@ -180,15 +186,9 @@ namespace LinBox {
         // ----- NOT LiftingContainer API
         // ----- but still needed
 
-		const IElement numbound() const
-		{
-			return _numbound;
-		}
+        const IElement numbound() const { return _numbound; }
 
-		const IElement denbound() const
-		{
-			return _denbound;
-		}
+        const IElement denbound() const { return _denbound; }
 
         // --------------
         // ----- Iterator
@@ -199,36 +199,65 @@ namespace LinBox {
          */
         class const_iterator {
         private:
-            BlasVector<Ring> _res;
             const MultiModLiftingContainer& _lc;
             size_t _position;
 
         public:
-            const_iterator(const MultiModLiftingContainer& lc, size_t end = 0)
+            const_iterator(const MultiModLiftingContainer& lc, size_t position = 0)
                 : _lc(lc)
-                , _position(end)
+                , _position(position)
             {
-                // @fixme Initialize _residue
+                // @fixme Initialize reisdue _r
             }
 
             /**
              * Returns false if the next digit cannot be computed (bad modulus).
+             * ci is a vector of integers but all element are below p = p1 * ... * pl
              */
-            bool next(IVector& digit)
+            bool next(IVector& ci)
             {
-                // compute v2 = _matA * digit
-                IVector v2(_lc.ring(), _lc.size());
-                // @fixme _lc._MAD.applyV(v2, digit, _res);
+                /*  for i = 1 .. l:
+                 *  |   (Qi, Ri) = such that ri = pi Qi + Ri with |Ri| < pi
+                 *  |   ci = Bi Ri mod pi                   < Matrix-vector in Z/pZ
+                 *  V = [R1|...|Rl] - A [c1|...|cl]         < Matrix-matrix in ZZ
+                 *  for i = 1 .. l:
+                 *  |   ri = Qi + (Vi / pi)
+                 */
 
-                // update _res -= v2
-                // @fixme _lc._VDR.subin(_res, v2);
-                typename BlasVector<Ring>::iterator p0;
+                std::cout << "ci: " << ci << std::endl;
 
-                // update _res = _res / p
-                int index = 0;
-                for (p0 = _res.begin(); p0 != _res.end(); ++p0, ++index) {
-                    _lc.ring().divin(*p0, _lc._p);
-                }
+                // @fixme Could be parallel!
+                // for (auto i = 0u; i < l; ++i) {
+                //     Hom<Ring, Field> hom(_ring, _F[i]);
+
+                //     // @fixme How to do euclidian division?
+                //     // ri = pi Qi + Ri
+
+                //     // @todo If R might already be a field element
+                //     _B[i]->apply(_c[i], hom.convert(_R[i]));
+
+                //     // @todo Convert _c[i] to RNS
+                // }
+
+                // @fixme How can we do A [c1|...|cl] in ZZ if the ci are in the fields?
+
+                // @fixme Compute the next residue!
+
+                // @fixme @note For us, Aci is a matrix!
+
+                // // compute Aci = _matA * ci
+                // IVector Aci(_lc.ring(), _lc.size());
+                // // @fixme _lc._MAD.applyV(Aci, ci, _res);
+
+                // // update _res -= Aci
+                // // @fixme _lc._VDR.subin(_res, Aci);
+                // typename BlasVector<Ring>::iterator p0;
+
+                // // update _res = _res / p
+                // int index = 0;
+                // for (p0 = _res.begin(); p0 != _res.end(); ++p0, ++index) {
+                //     _lc.ring().divin(*p0, _lc._p);
+                // }
 
                 // increase position of the iterator
                 ++_position;
@@ -251,21 +280,22 @@ namespace LinBox {
 
     private:
         const Ring& _ring;
-        Field _field;
 
         IElement _numbound;
         IElement _denbound;
 
-        IElement _p;
-        size_t _k; //< Length of the ci sequence. So that p^{k-1} > 2ND (Hadamard bound)
-        size_t _n; //< Row/column dimension of A.
+        IElement _p;                   // The global modulus for lifting: a multiple of all _primes.
+        std::vector<FElement> _primes; // @fixme We might want something else as a type!
+        size_t _k; // Length of the ci sequence. So that p^{k-1} > 2ND (Hadamard bound).
+        size_t _n; // Row/column dimension of A.
+        size_t _l; // How many primes. Equal to _primes.size().
 
         // @note r is a big matrix in ZZ holding all residues
-        IMatrix _r;
-        FMatrix _c;
-        std::vector<FMatrix> _B; // Inverses of A mod p[i]
-        std::vector<IVector> _Q;
-        std::vector<FVector> _R;
-        std::vector<Field> _F;
+        // IMatrix _r;
+        // FMatrix _c;
+        std::vector<std::unique_ptr<FMatrix>> _B; // Inverses of A mod p[i]
+        // std::vector<IVector> _Q;
+        // std::vector<FVector> _R;
+        std::vector<Field> _fields;
     };
 }
