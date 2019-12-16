b56e43a2ede20641959f4f6ac2719923ceb77377
Pascal Giorgi <pascal.giorgi@lirmm.fr>
2019-12-16 14:24:36 +0100

    add test for polynomial matrix

--- linbox/algorithms/polynomial-matrix/matpoly-mult-naive.h
+++ linbox/algorithms/polynomial-matrix/matpoly-mult-naive.h
@@ -41,93 +41,97 @@ namespace LinBox
 		BlasMatrixDomain<Field>   _BMD;
 
 		// Polynomial matrix stored as a matrix of polynomial
-		typedef PolynomialMatrix<PMType::polfirst,PMStorage::plain,Field> MatrixP;
-		// Polynomial matrix stored as a polynomial of matrix
-		typedef PolynomialMatrix<PMType::matfirst,PMStorage::plain,Field> PMatrix;
-                template <PMStorage S> using _PMatrix = PolynomialMatrix<PMType::matfirst,S,Field>;
+		//typedef PolynomialMatrix<PMType::polfirst,PMStorage::plain,Field> MatrixP;
+        typedef PolynomialMatrix<Field,PMType::polfirst> MatrixP;
+
+        // Polynomial matrix stored as a polynomial of matrix
+		//typedef PolynomialMatrix<PMType::matfirst,PMStorage::plain,Field> PMatrix;
+        typedef PolynomialMatrix<Field,PMType::matfirst> PMatrix;
+
+        //template <PMStorage S> using _PMatrix = PolynomialMatrix<PMType::matfirst,S,Field>;
 	public:
 
-                inline const Field& field() const { return *_field; }
+        inline const Field& field() const { return *_field; }
 
-                PolynomialMatrixNaiveMulDomain(const Field &F) :
+        PolynomialMatrixNaiveMulDomain(const Field &F) :
 			_field(&F), _BMD(F){}
                 
 
-                void mul(PMatrix&  c, const PMatrix&  a, const PMatrix&  b) const
-                {
-                        for (size_t k=0;k<a.size()+b.size()-1;k++){
-                                size_t idx_min= (k+1<b.size()?0:k+1-b.size());
-                                size_t idx_max=std::min(k,a.size()-1);                                
-                                _BMD.mul(c[k],a[idx_min],b[k-idx_min]);
-                                for (size_t i=idx_min+1;i<=idx_max;i++){ 
-                                        _BMD.axpyin(c[k],a[i],b[k-i]);
-                                }
-                        }
-                }                
+        void mul(PMatrix&  c, const PMatrix&  a, const PMatrix&  b) const
+        {
+            for (size_t k=0;k<a.size()+b.size()-1;k++){
+                size_t idx_min= (k+1<b.size()?0:k+1-b.size());
+                size_t idx_max=std::min(k,a.size()-1);                                
+                _BMD.mul(c[k],a[idx_min],b[k-idx_min]);
+                for (size_t i=idx_min+1;i<=idx_max;i++){ 
+                    _BMD.axpyin(c[k],a[i],b[k-i]);
+                }
+            }
+        }                
 
-                // c must be allocated with the right size
-                template<typename Matrix1,typename Matrix2,typename Matrix3>
-                void mul(Matrix1& c, const Matrix2&  a, const Matrix3&  b) const
-                {
-                        PMatrix a2(field(),a.rowdim(),a.coldim(),a.size());
-			PMatrix b2(field(),b.rowdim(),b.coldim(),b.size());
+        // c must be allocated with the right size
+        template<typename Matrix1,typename Matrix2,typename Matrix3>
+        void mul(Matrix1& c, const Matrix2&  a, const Matrix3&  b) const
+        {
+            PMatrix a2(field(),a.rowdim(),a.coldim(),a.size());
+            PMatrix b2(field(),b.rowdim(),b.coldim(),b.size());
 			a2.copy(a,0,a.size()-1);
 			b2.copy(b,0,b.size()-1);
 			PMatrix c2(field(),c.rowdim(),c.coldim(),a.size()+b.size()-1);
 			mul(c2, a2, b2);
 			c.copy(c2,0,c2.size()-1);
-                }                          
+        }                          
 
 
                 
-                // c must be allocated with the right size 
-                // a and b can have a size smaller than required
-                template<typename Matrix1,typename Matrix2,typename Matrix3>
-                void midproduct(Matrix1& c, const Matrix2&  a, const Matrix3&  b,
-                                bool smallLeft=true, size_t n0=0,size_t n1=0) const
-                {
-                        PMatrix a2(field(),a.rowdim(),a.coldim(),a.size());
+        // c must be allocated with the right size 
+        // a and b can have a size smaller than required
+        template<typename Matrix1,typename Matrix2,typename Matrix3>
+        void midproduct(Matrix1& c, const Matrix2&  a, const Matrix3&  b,
+                        bool smallLeft=true, size_t n0=0,size_t n1=0) const
+        {
+            PMatrix a2(field(),a.rowdim(),a.coldim(),a.size());
 			PMatrix b2(field(),b.rowdim(),b.coldim(),b.size());
 			a2.copy(a,0,a.size()-1);
 			b2.copy(b,0,b.size()-1);
 			PMatrix c2(field(),c.rowdim(),c.coldim(),c.size());
 			midproduct(c2, a2, b2, smallLeft, n0, n1);
 			c.copy(c2,0,c2.size()-1);
-                }                          
-                void midproduct(PMatrix&  c, const PMatrix&  a, const PMatrix&  b,
-                                bool smallLeft=true, size_t n0=0,size_t n1=0) const {
-                        //cout<<"naive midprod "<<a.size()<<"x"<<b.size()<<"->"<<c.size()<<endl;
-                        size_t hdeg = ((n0==0)?c.size():n0);
+        }                          
+        void midproduct(PMatrix&  c, const PMatrix&  a, const PMatrix&  b,
+                        bool smallLeft=true, size_t n0=0,size_t n1=0) const {
+            //cout<<"naive midprod "<<a.size()<<"x"<<b.size()<<"->"<<c.size()<<endl;
+            size_t hdeg = ((n0==0)?c.size():n0);
 			size_t deg  = ((n1==0)?2*hdeg-1:n1);
-                        //cout<<"("<<hdeg-1<<","<<deg-1<<")"<<endl;
-                        if (smallLeft){
-                                // for (size_t k=hdeg-1;k<std::min(a.size()+b.size()-1,deg);k++){
-                                //         _BMD.mul(c[k-hdeg+1],a[0],b[k]);
-                                //         for (size_t j=1;j<std::min(hdeg,a.size());++j)
-                                //                 _BMD.axpyin(c[k-hdeg+1],a[j],b[k-j]);
-                                //}
-                                for (size_t k=hdeg-1;k<std::min(a.size()+b.size()-1,deg);k++){
-                                        size_t idx_b=std::min(k,b.size()-1);
-                                        size_t idx_a=k-idx_b;
-                                        //cout<<k<<" : "<<idx_a<<"---"<<idx_b<<endl;
-                                        _BMD.mul(c[k-hdeg+1],a[idx_a],b[idx_b]);
-                                        for (size_t j=idx_a+1;j<=std::min(k,a.size()-1);++j){
-                                                //cout<<k<<" : "<<j<<"---"<<k-j<<endl;
-                                                _BMD.axpyin(c[k-hdeg+1],a[j],b[k-j]);
-                                        }
-                                }
-                        }else {
-                                for (size_t k=hdeg-1;k<std::min(a.size()+b.size()-1,deg);k++){
-                                        size_t idx_a=std::min(k,a.size()-1);
-                                        size_t idx_b=k-idx_a;
-                                        _BMD.mul(c[k-hdeg+1],a[idx_a],b[idx_b]);
-                                        for (size_t j=idx_b+1;j<=std::min(k,b.size()-1);++j){
-                                                _BMD.axpyin(c[k-hdeg+1],a[k-j],b[j]);
-                                        }
-                                }
-                        }
-                }               
-        };
+            //cout<<"("<<hdeg-1<<","<<deg-1<<")"<<endl;
+            if (smallLeft){
+                // for (size_t k=hdeg-1;k<std::min(a.size()+b.size()-1,deg);k++){
+                //         _BMD.mul(c[k-hdeg+1],a[0],b[k]);
+                //         for (size_t j=1;j<std::min(hdeg,a.size());++j)
+                //                 _BMD.axpyin(c[k-hdeg+1],a[j],b[k-j]);
+                //}
+                for (size_t k=hdeg-1;k<std::min(a.size()+b.size()-1,deg);k++){
+                    size_t idx_b=std::min(k,b.size()-1);
+                    size_t idx_a=k-idx_b;
+                    //cout<<k<<" : "<<idx_a<<"---"<<idx_b<<endl;
+                    _BMD.mul(c[k-hdeg+1],a[idx_a],b[idx_b]);
+                    for (size_t j=idx_a+1;j<=std::min(k,a.size()-1);++j){
+                        //cout<<k<<" : "<<j<<"---"<<k-j<<endl;
+                        _BMD.axpyin(c[k-hdeg+1],a[j],b[k-j]);
+                    }
+                }
+            }else {
+                for (size_t k=hdeg-1;k<std::min(a.size()+b.size()-1,deg);k++){
+                    size_t idx_a=std::min(k,a.size()-1);
+                    size_t idx_b=k-idx_a;
+                    _BMD.mul(c[k-hdeg+1],a[idx_a],b[idx_b]);
+                    for (size_t j=idx_b+1;j<=std::min(k,b.size()-1);++j){
+                        _BMD.axpyin(c[k-hdeg+1],a[k-j],b[j]);
+                    }
+                }
+            }
+        }               
+    };
 	
 } // end of namespace LinBox
 
