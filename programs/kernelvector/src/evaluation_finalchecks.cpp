8fffe31717392aebb013a64fe584b5550aa48774
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2020-01-14 18:05:32 +0100

    loop on applications of A

--- programs/kernelvector/src/evaluation_finalchecks.cpp
+++ programs/kernelvector/src/evaluation_finalchecks.cpp
@@ -1,6 +1,6 @@
 /*  Copyright (c) 2015 HPAC
- *  Written by 	Alexis Breust <alexis.breust@gmail.fr>
- * 		Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
+ *  Written by	Alexis Breust <alexis.breust@gmail.fr>
+ *		Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
  */
 
 #include "common_define.hpp"
@@ -19,15 +19,18 @@
 int main(int argc, char **argv)
 {
     std::string folder = "output/";
+    Size_t retries = 4u;
+
 
     static Argument as[] =
     {
-        { 'd', "-d FOLDER",  "The directory where kernel.dv is stored.", TYPE_STR, &folder },
-        END_OF_ARGUMENTS
+	{ 'd', "-d FOLDER",  "The directory where kernel.dv is stored.", TYPE_STR, &folder },
+	{ 'r', "-r R1", "Set the maximal number of reapplication of A.", TYPE_INT, &retries },
+	END_OF_ARGUMENTS
     };
 
     FFLAS::parseArguments(argc, argv, as);
-    
+
     std::cerr << "[FCHK] Checking that the computed vector is correct..." << std::endl;
 
     // Get values
@@ -42,46 +45,61 @@ int main(int argc, char **argv)
 
     std::cerr << "[FCHK] Matrix read." << std::endl;
     //----- Final checks -----//
-    
+
     // Check that A * X == 0 and X != 0
     DVector_t X;
-    std::ifstream kernelStream(folder + "/kernel.dv");
-    readDV(kernelStream, X);
-    
+    {
+      std::ifstream kernelStream(folder + "/kernel.dv");
+      readDV(kernelStream, X);
+    }
+
     DVector_t Y(X.size());
 
     if (isNullVector(p,X)) {
-        std::cerr << "[ERROR] The computed vector is the null one. Sorry. Try again." << std::endl;
-        return EXIT_FAILURE;
+	std::cerr << "[ERROR] The computed vector is the null one. Sorry. Try again." << std::endl;
+	return EXIT_FAILURE;
     }
-    
+
     PAR_BLOCK { spmv(p, Y, A, X); }
 
-    if (!isNullVector(p,Y)) {
+    bool kernelfound(isNullVector(p,Y));
+    DVector_t Z(X.size());
+    std::ofstream kernelvecStream(folder + "/kernel.dv");
+    writeDV(kernelvecStream,Y);
 
-        std::cerr << "[FCHK] First attempt failed : the computed vector is not from the kernel..." << std::endl;
-	std::cerr<<"[FCHK] Second chance: trying to apply A on the vector"<<std::endl;
-	DVector_t Z(X.size());
-	PAR_BLOCK { spmv(p, Z, A, Y); }
-	if (!isNullVector(p,Z)) {
-	  std::ofstream verifStream(folder + "/outvect.dv");
-	  writeDV(verifStream,Y);
-	  //        writeDV(std::cout,Y);
-	  std::cerr<<"[ERROR] still not a vector from the Kernel. Sorry. Try again."<<std::endl;
-	  return EXIT_FAILURE;
-	} else
-	  {
-	    std::string cmd= "cp "; cmd+= folder + "/kernel.dv "; cmd+= folder + "/kernel1.dv "; 
-	    system(cmd.c_str());
-	    std::ofstream kernelvecStream(folder + "/kernel.dv");
-	    writeDV(kernelvecStream,Y);
-	    std::cerr<<"[SUCCESS] A * kernelvec is a vector from the kernel of A"<<std::endl;
-	    std::cerr<<"[INFO] Saving the result in kernel.dv"<<std::endl;
-	  }
+    for(Size_t i=0; (!kernelfound) && (i< retries); ++i) {
+
+
+      std::clog << "[FCHK] attempt " << (i+1) <<  " failed : the computed vector is not from the kernel..." << std::endl;
+      std::clog << "[FCHK] trying to apply A another time on the vector... "<<std::endl;
+
+      std::string cmd= "cp "; cmd+= folder + "/kernel.dv "; cmd+= folder + "/try" + std::to_string(i) + "Z.dv";
+      system(cmd.c_str());
+      writeDV(kernelvecStream,Y);
+
+      PAR_BLOCK { spmv(p, Z, A, Y); }
+      kernelfound = isNullVector(p,Z);
+
+      if (kernelfound) break;
+
+      std::clog<<"[FCHK] trying to apply A another time on the vector... "<<std::endl;
+      cmd= "cp "; cmd+= folder + "/kernel.dv "; cmd+= folder + "/try" + std::to_string(i) + "Y.dv";
+      system(cmd.c_str());
+      writeDV(kernelvecStream,Z);
+
+      PAR_BLOCK { spmv(p, Y, A, Z); }
+      kernelfound = isNullVector(p,Y);
+
+    }
+
+    if (kernelfound) {
+      std::cerr<<"[SUCCESS] A * kernelvec is a vector from the kernel of A"<<std::endl;
+      std::cerr<<"[INFO] Saving the result in kernel.dv"<<std::endl;
+    } else {
+      std::cerr<<"[ERROR] still not a vector from the Kernel. Sorry. Try again."<<std::endl;
     }
 
     std::cerr << "/!\\ ALL IS OK! a non-null kernel vector has been found !" << std::endl;
 
     return EXIT_SUCCESS;
 }
-
