1dd36cd6a5ad9e643dd2ba774399080a86eb6060
Clément Pernet <clement.pernet@gmail.com>
2021-12-02 10:18:59 +0100

    minor edits required by review

--- examples/nullspacebasis.C
+++ examples/nullspacebasis.C
@@ -37,33 +37,31 @@ using namespace LinBox;
 
 int main (int argc, char **argv)
 {
-	if ( argc <  3 || argc > 4) {
-		std::cerr << "Usage to get a random null space basis over GF(p,k):  <matrix-file-in-SMS-format> p [k]" << std::endl;
-		return -1;
-	}
-
-	std::ifstream input (argv[1]);
-	if (!input) { std::cerr << "Error opening matrix file " << argv[1] << std::endl; return -1; }
-
+    if ( argc <  3 || argc > 4) {
+        std::cerr << "Usage to get a random null space basis over GF(p,k):  <matrix-file-in-SMS-format> p [k]" << std::endl;
+        return -1;
+    }
+    
+    std::ifstream input (argv[1]);
+    if (!input) { std::cerr << "Error opening matrix file " << argv[1] << std::endl; return -1; }
+    
 	//typedef Givaro::Modular<int> Field;
-	typedef Givaro::GFqDom<int64_t> Field;
-	Field F(atoi(argv[2]),argc>3?atoi(argv[3]):1);
-	SparseMatrix<Field, SparseMatrixFormat::SparseSeq > B (F);
-	B.read (input);
-	std::cout << "B is " << B.rowdim() << " by " << B.coldim() << std::endl;
-
+    typedef Givaro::GFqDom<int64_t> Field;
+    Field F(atoi(argv[2]),argc>3?atoi(argv[3]):1);
+    SparseMatrix<Field, SparseMatrixFormat::SparseSeq > B (F);
+    B.read (input);
+    std::cout << "B is " << B.rowdim() << " by " << B.coldim() << std::endl;
+    
     DenseMatrix<Field> NullSpace(F,B.coldim(),B.coldim());
     GaussDomain<Field> GD(F);
-
+    
     GD.nullspacebasisin(NullSpace, B);
-
+    
     NullSpace.write( std::cerr << "X:=", Tag::FileFormat::Maple ) << ';' << std::endl;
 
     std::cerr << "NullsSpace dimensions:" << NullSpace.rowdim() << 'x' << NullSpace.coldim() << std::endl;
 
     return 0;
-
-
 }
 
 // Local Variables:
