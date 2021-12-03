d69bb646712fd14746606b75d5e527aa4ebb5179
Clément Pernet <clement.pernet@gmail.com>
2021-12-03 12:35:03 +0100

    linbox/matrix/densematrix/blas-matrix.h

--- linbox/vector/blas-vector.h
+++ linbox/vector/blas-vector.h
@@ -241,7 +241,8 @@ namespace LinBox {
         Element_ptr      getPointer()       { return _ptr; }
         ConstElement_ptr getConstPointer() const { return _ptr;}
 
-		const Storage &getRep() const { return _rep ; }
+        Storage &refRep() { return _rep ; }
+        const Storage &getRep() const { return _rep ; }
 
         /** Get the increment in the vector
          * @return the inc value of the subvector
